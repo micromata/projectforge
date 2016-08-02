d6a924e2d353943e52f96006076102dda546f714
Christian Tausendfreund <c.tausendfreund@micromata.de>
2016-08-02 14:39:14 +0200

    PROJECTFORGE-2226 add functionality to create the tabs in employee edit page according to the attrschema.xml

--- projectforge-wicket/src/main/java/org/projectforge/web/wicket/components/TabPanel.java
+++ projectforge-wicket/src/main/java/org/projectforge/web/wicket/components/TabPanel.java
@@ -1,5 +1,9 @@
 package org.projectforge.web.wicket.components;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
 import org.apache.wicket.behavior.AttributeAppender;
 import org.apache.wicket.markup.html.link.ExternalLink;
 import org.apache.wicket.markup.html.list.AbstractItem;
@@ -9,25 +13,38 @@ import org.projectforge.web.wicket.bootstrap.GridBuilder;
 
 public class TabPanel extends Panel
 {
-  private final RepeatingView tabs;
+  private final RepeatingView tabs = new RepeatingView("tabs");
 
-  private final RepeatingView tabPanes;
+  private final RepeatingView tabPanes = new RepeatingView("tabPanes");
+
+  private final Map<String, GridBuilder> tabContainerCache = new HashMap<>();
+
+  private GridBuilder defaultTabContainer;
 
   public TabPanel(String id)
   {
     super(id);
-
-    tabs = new RepeatingView("tabs");
     add(tabs);
-
-    tabPanes = new RepeatingView("tabPanes");
     add(tabPanes);
   }
 
-  public GridBuilder newTab(final String i18nKey, final boolean active)
+  public GridBuilder getOrCreateTab(final String i18nKey)
   {
+    return getOrCreateTab(i18nKey, false);
+  }
+
+  public GridBuilder getOrCreateTab(final String i18nKey, final boolean activeAndDefault)
+  {
+    if (StringUtils.isEmpty(i18nKey)) {
+      return defaultTabContainer;
+    }
+
+    if (tabContainerCache.containsKey(i18nKey)) {
+      return tabContainerCache.get(i18nKey);
+    }
+
     final String markupId = "tab_" + i18nKey.replace('.', '_');
-    
+
     final AbstractItem tab = new AbstractItem(tabs.newChildId());
     tab.add(new ExternalLink("tabLink", "#" + markupId, getString(i18nKey)));
     tabs.add(tab);
@@ -36,11 +53,15 @@ public class TabPanel extends Panel
     tabPane.add(AttributeAppender.replace("id", markupId));
     tabPanes.add(tabPane);
 
-    if (active) {
+    final GridBuilder tabContainer = new GridBuilder(tabPane, "tabContainer");
+    tabContainerCache.put(i18nKey, tabContainer);
+
+    if (activeAndDefault) {
       tab.add(AttributeAppender.append("class", "active"));
       tabPane.add(AttributeAppender.append("class", "active"));
+      defaultTabContainer = tabContainer;
     }
 
-    return new GridBuilder(tabPane, "tabContainer");
+    return tabContainer;
   }
 }
