bb5ee66bec033bcc52d3be4c43f56007a804efd0
Fabian Desel <f.desel@micromata.de>
2016-08-22 14:36:10 +0200

    PROJECTFORGE-2249 : PROJECTFORGE-2250 : Work in Progress

--- plugins/org.projectforge.plugins.extendemployeedata/src/main/java/org/projectforge/plugins/eed/wicket/EmployeeGeneralValueConfigurationForm.java
+++ plugins/org.projectforge.plugins.extendemployeedata/src/main/java/org/projectforge/plugins/eed/wicket/EmployeeGeneralValueConfigurationForm.java
@@ -5,60 +5,27 @@ import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.Model;
 import org.projectforge.framework.persistence.api.BaseSearchFilter;
+import org.projectforge.plugins.eed.EmployeeGeneralValueDO;
+import org.projectforge.web.wicket.AbstractEditForm;
 import org.projectforge.web.wicket.AbstractListForm;
 import org.projectforge.web.wicket.WicketUtils;
 import org.projectforge.web.wicket.components.SingleButtonPanel;
 
-public class EmployeeGeneralValueConfigurationForm extends AbstractListForm<BaseSearchFilter,EmployeeGeneralValueConfigurationPage>
+public class EmployeeGeneralValueConfigurationForm
+    extends AbstractEditForm<EmployeeGeneralValueDO, EmployeeGeneralValueConfigurationPage>
 {
-  private static final org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(EmployeeGeneralValueConfigurationForm.class);
+  private static final org.apache.log4j.Logger log = org.apache.log4j.Logger
+      .getLogger(EmployeeGeneralValueConfigurationForm.class);
 
-  public EmployeeGeneralValueConfigurationForm(EmployeeGeneralValueConfigurationPage parentPage)
+  public EmployeeGeneralValueConfigurationForm(EmployeeGeneralValueConfigurationPage parentPage,
+      EmployeeGeneralValueDO data)
   {
-    super(parentPage);
-  }
-
-  @Override
-  protected BaseSearchFilter newSearchFilterInstance()
-  {
-    return new BaseSearchFilter();
+    super(parentPage, data);
   }
 
   @Override
   protected Logger getLogger()
   {
     return log;
-  }
-
-  @Override
-  protected void init()
-  {
-    super.init();
-
-    //Top Buttons
-    //Disable default buttons
-    resetButtonPanel.setVisible(false);
-    searchButtonPanel.setVisible(false);
-
-    // Customized Filter
-    remove(gridBuilder.getMainContainer());
-    gridBuilder = newGridBuilder(this, "filter");
-  }
-
-  public Panel getSaveButtonPanel(String id)
-  {
-    //Bottom Buttons
-    final Button saveButton = new Button(SingleButtonPanel.WICKET_ID, new Model<>("save"))
-    {
-      @Override
-      public final void onSubmit()
-      {
-        parentPage.saveList();
-      }
-    };
-    WicketUtils.addTooltip(saveButton, getString("save"));
-    final SingleButtonPanel saveButtonPanel = new SingleButtonPanel(id, saveButton,
-        getString("save"), SingleButtonPanel.DEFAULT_SUBMIT);
-    return saveButtonPanel;
   }
 }
--- plugins/org.projectforge.plugins.extendemployeedata/src/main/java/org/projectforge/plugins/eed/wicket/EmployeeGeneralValueConfigurationPage.java
+++ plugins/org.projectforge.plugins.extendemployeedata/src/main/java/org/projectforge/plugins/eed/wicket/EmployeeGeneralValueConfigurationPage.java
@@ -1,5 +1,6 @@
 package org.projectforge.plugins.eed.wicket;
 
+import org.apache.log4j.Logger;
 import org.apache.wicket.extensions.markup.html.repeater.data.sort.SortOrder;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
 import org.apache.wicket.markup.html.WebPage;
@@ -9,23 +10,21 @@ import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.projectforge.business.user.I18nHelper;
 import org.projectforge.plugins.eed.EmployeeGeneralValueDO;
 import org.projectforge.plugins.eed.EmployeeGeneralValueDao;
-import org.projectforge.web.wicket.AbstractListPage;
-import org.projectforge.web.wicket.CellItemListener;
-import org.projectforge.web.wicket.CellItemListenerPropertyColumn;
-import org.projectforge.web.wicket.IListPageColumnsCreator;
-import org.projectforge.web.wicket.ListPage;
+import org.projectforge.plugins.eed.service.EmployeeGeneralValueService;
+import org.projectforge.web.wicket.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@ListPage(editPage = EmployeeGeneralValueEditPage.class)
-public class EmployeeGeneralValueConfigurationPage extends AbstractListPage<EmployeeGeneralValueConfigurationForm, EmployeeGeneralValueDao,EmployeeGeneralValueDO> implements
-    IListPageColumnsCreator<EmployeeGeneralValueDO>
+public class EmployeeGeneralValueConfigurationPage extends AbstractEditPage<EmployeeGeneralValueDO,EmployeeGeneralValueConfigurationForm,EmployeeGeneralValueDao>
 {
   private static final org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(EmployeeGeneralValueConfigurationPage.class);
 
   @SpringBean
   private EmployeeGeneralValueDao employeeGeneralValueDao;
+
+  @SpringBean
+  private EmployeeGeneralValueService employeeGeneralValueService;
 
   private List<EmployeeGeneralValueDO> dataList;
 
@@ -33,74 +32,23 @@ public class EmployeeGeneralValueConfigurationPage extends AbstractListPage<Empl
   public EmployeeGeneralValueConfigurationPage(PageParameters parameters)
   {
     super(parameters, "plugins.eed.config");
+    EmployeeGeneralValueDO valueDO = employeeGeneralValueService.getValueDO();
+    init(valueDO);
   }
 
   @Override
-  public List<IColumn<EmployeeGeneralValueDO, String>> createColumns(final WebPage returnToPage, final boolean sortable)
-  {
-    final List<IColumn<EmployeeGeneralValueDO, String>> columns = new ArrayList<>();
-
-    final CellItemListener<EmployeeGeneralValueDO> cellItemListener = (CellItemListener<EmployeeGeneralValueDO>) (item, componentId,
-        rowModel) -> {
-      final EmployeeGeneralValueDO configValue = rowModel.getObject();
-      appendCssClasses(item, configValue.getId(), configValue.isDeleted());
-    };
-
-    columns.add(new CellItemListenerPropertyColumn<>(new ResourceModel("key"),
-        getSortable("key", sortable),
-        "key", cellItemListener));
-
-    /*columns.add(new CellItemListenerPropertyColumn<>(new ResourceModel("value"),
-        getSortable("value", sortable),
-        "value", cellItemListener));*/
-
-    columns.add(new StringValueColumn(new ResourceModel("value"),"value"));
-
-    return columns;
+  protected EmployeeGeneralValueConfigurationForm newEditForm(AbstractEditPage<?, ?, ?> parentPage, EmployeeGeneralValueDO data) {
+    return new EmployeeGeneralValueConfigurationForm(this, data);
   }
 
   @Override
-  protected void init()
-  {
-    final List<IColumn<EmployeeGeneralValueDO, String>> columns = createColumns(this, true);
-    dataTable = createDataTable(columns, "key", SortOrder.ASCENDING);
-    form.add(dataTable);
-
-    // remove add and reindex buttons from context menu
-    //contentMenuBarPanel = new MenuBarPanel("menuBar");
-
+  protected Logger getLogger() {
+    return log;
   }
 
   @Override
   public EmployeeGeneralValueDao getBaseDao()
   {
     return employeeGeneralValueDao;
-  }
-
-  @Override
-  protected void addBottomPanel(final String id)
-  {
-    form.add(form.getSaveButtonPanel(id));
-  }
-
-  @Override
-  protected EmployeeGeneralValueConfigurationForm newListForm(AbstractListPage<?, ?, ?> parentPage)
-  {
-    return new EmployeeGeneralValueConfigurationForm(this);
-  }
-
-  public void saveList()
-  {
-    for (EmployeeGeneralValueDO e : this.dataList) {
-      employeeGeneralValueDao.update(e);
-    }
-    info(I18nHelper.getLocalizedString("plugins.eed.listcare.savesucces"));
-  }
-
-  @Override
-  public List<EmployeeGeneralValueDO> getList()
-  {
-    this.dataList = super.getList();
-    return this.dataList;
   }
 }
