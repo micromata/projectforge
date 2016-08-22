f562ce4b70976afa65965611c047a511c0ca48fc
Marcel Hesse <m.hesse@micromata.de>
2016-08-22 14:43:26 +0200

    Removed no longer needed exception class

--- projectforge-business/src/main/java/org/projectforge/business/timesheet/NoKost2IdException.java
+++
@@ -1,9 +0,0 @@
-package org.projectforge.business.timesheet;
-
-/**
- * This class is a stub by intention
- */
-public class NoKost2IdException extends RuntimeException
-{
-  // this class is just used as marker for not found cost2 ids
-}
