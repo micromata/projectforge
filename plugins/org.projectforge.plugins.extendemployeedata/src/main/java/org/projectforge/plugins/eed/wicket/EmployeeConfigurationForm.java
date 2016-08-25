cb1119aba230b209d7236e8e524e0d0c5372c75f
Fabian Desel <f.desel@micromata.de>
2016-08-25 16:40:16 +0200

    PROJECTFORGE-2249:PROJECTFORGE-2255: -improve Migration and add Save Button

--- plugins/org.projectforge.plugins.extendemployeedata/src/main/java/org/projectforge/plugins/eed/wicket/EmployeeConfigurationForm.java
+++ plugins/org.projectforge.plugins.extendemployeedata/src/main/java/org/projectforge/plugins/eed/wicket/EmployeeConfigurationForm.java
@@ -3,13 +3,19 @@ package org.projectforge.plugins.eed.wicket;
 import java.util.function.Function;
 
 import org.apache.log4j.Logger;
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.projectforge.framework.i18n.UserException;
 import org.projectforge.framework.persistence.attr.impl.GuiAttrSchemaService;
 import org.projectforge.plugins.eed.EmployeeConfigurationDO;
 import org.projectforge.plugins.eed.EmployeeConfigurationTimedDO;
 import org.projectforge.plugins.eed.service.EmployeeConfigurationService;
 import org.projectforge.web.wicket.AbstractEditForm;
+import org.projectforge.web.wicket.WicketUtils;
 import org.projectforge.web.wicket.bootstrap.GridSize;
+import org.projectforge.web.wicket.components.SingleButtonPanel;
 import org.projectforge.web.wicket.flowlayout.DivPanel;
 
 import de.micromata.genome.db.jpa.tabattr.api.AttrGroup;
@@ -34,12 +40,25 @@ public class EmployeeConfigurationForm extends AbstractEditForm<EmployeeConfigur
   {
     super.init();
 
+    showUpdateAndStayButton();
+
     // AttrPanels
     gridBuilder.newSplitPanel(GridSize.COL100, true); // set hasSubSplitPanel to true to remove borders from this split panel
     final DivPanel divPanel = gridBuilder.getPanel();
     final Function<AttrGroup, EmployeeConfigurationTimedDO> addNewEntryFunction =
         group -> employeeConfigurationService.addNewTimeAttributeRow(data, group.getName());
     attrSchemaService.createTimedAttrPanels(divPanel, data, parentPage, addNewEntryFunction);
+  }
+
+  @Override
+  protected void updateButtonVisibility()
+  {
+    createButtonPanel.setVisible(false);
+    deleteButtonPanel.setVisible(false);
+    markAsDeletedButtonPanel.setVisible(false);
+    undeleteButtonPanel.setVisible(false);
+    updateAndNextButtonPanel.setVisible(false);
+    updateButton.setVisible(false);
   }
 
   @Override
