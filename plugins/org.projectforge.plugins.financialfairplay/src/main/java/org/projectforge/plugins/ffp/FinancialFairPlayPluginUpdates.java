8a788d11352832a40247df5a940edaa2151ee902
Florian Blumenstein <f.blumenstein@micromata.de>
2016-09-06 17:37:38 +0200

    Initial ffp plugin commit

---
+++ plugins/org.projectforge.plugins.financialfairplay/src/main/java/org/projectforge/plugins/ffp/FinancialFairPlayPluginUpdates.java
@@ -0,0 +1,44 @@
+package org.projectforge.plugins.ffp;
+
+import org.projectforge.business.fibu.EmployeeTimedAttrDataDO;
+import org.projectforge.business.fibu.EmployeeTimedAttrWithDataDO;
+import org.projectforge.continuousdb.UpdateEntry;
+import org.projectforge.continuousdb.UpdateEntryImpl;
+import org.projectforge.continuousdb.UpdatePreCheckStatus;
+import org.projectforge.continuousdb.UpdateRunningStatus;
+import org.projectforge.framework.configuration.ApplicationContextProvider;
+import org.projectforge.framework.persistence.database.InitDatabaseDao;
+import org.projectforge.framework.persistence.database.MyDatabaseUpdateService;
+
+public class FinancialFairPlayPluginUpdates
+{
+  static MyDatabaseUpdateService dao;
+
+  @SuppressWarnings("serial")
+  public static UpdateEntry getInitializationUpdateEntry()
+  {
+    return new UpdateEntryImpl(FinancialFairPlayPlugin.ID, "6.3", "2016-09-06", "Adds T_PLUGIN_FINANCIAL_FAIR_PLAY* Tables")
+    {
+
+      @Override
+      public UpdatePreCheckStatus runPreCheck()
+      {
+        // Does the data-base table already exist?
+        if (dao.doEntitiesExist(EmployeeTimedAttrWithDataDO.class, EmployeeTimedAttrDataDO.class)) {
+          return UpdatePreCheckStatus.ALREADY_UPDATED;
+        } else {
+          return UpdatePreCheckStatus.READY_FOR_UPDATE;
+        }
+      }
+
+      @Override
+      public UpdateRunningStatus runUpdate()
+      {
+        InitDatabaseDao initDatabaseDao = ApplicationContextProvider.getApplicationContext().getBean(InitDatabaseDao.class);
+        // Updating the schema
+        initDatabaseDao.updateSchema();
+        return UpdateRunningStatus.DONE;
+      }
+    };
+  }
+}
