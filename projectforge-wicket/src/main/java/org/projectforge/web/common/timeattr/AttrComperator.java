a47b519ad42aeae825ca8280154b6ddaaf708699
Christian Tausendfreund <c.tausendfreund@micromata.de>
2016-09-27 16:46:18 +0200

    PROJECTFORGE-2446 sorting now also works with null values

--- projectforge-wicket/src/main/java/org/projectforge/web/common/timeattr/AttrComperator.java
+++ projectforge-wicket/src/main/java/org/projectforge/web/common/timeattr/AttrComperator.java
@@ -23,23 +23,23 @@ public class AttrComperator<PK extends Serializable, T extends TimeableAttrRow<P
   private final GuiAttrSchemaService guiAttrSchemaService = (GuiAttrSchemaService) ApplicationContextProvider.getApplicationContext()
       .getBean("guiAttrSchemaService");
 
-  private final String sortProperty;
+  private final String groupName;
+
+  private final String descName;
 
   private final boolean ascending;
 
   public AttrComperator(final String sortProperty, final boolean ascending)
   {
-    this.sortProperty = sortProperty;
+    final String[] strings = sortProperty.split(":");
+    this.groupName = strings[1];
+    this.descName = strings[2];
     this.ascending = ascending;
   }
 
   @Override
   public int compare(U o1, U o2)
   {
-    final String[] strings = sortProperty.split(":");
-    final String groupName = strings[1];
-    final String descName = strings[2];
-
     final Date now = new Date();
     final Optional<IModel<String>> stringValue1 = guiAttrSchemaService.getStringAttribute(o1, now, groupName, descName);
     final Optional<IModel<String>> stringValue2 = guiAttrSchemaService.getStringAttribute(o2, now, groupName, descName);
@@ -48,6 +48,14 @@ public class AttrComperator<PK extends Serializable, T extends TimeableAttrRow<P
       final String sv1 = stringValue1.get().getObject();
       final String sv2 = stringValue2.get().getObject();
       return ascending ? sv1.compareTo(sv2) : sv2.compareTo(sv1);
+    }
+
+    if (stringValue1.isPresent()) {
+      return ascending ? 1 : -1;
+    }
+
+    if (stringValue2.isPresent()) {
+      return ascending ? -1 : 1;
     }
 
     return 0;
