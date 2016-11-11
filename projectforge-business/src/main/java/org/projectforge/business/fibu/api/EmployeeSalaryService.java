e567a67433c8e422bba34e8d9c0de7e217058fbe
Florian Blumenstein <f.blumenstein@micromata.de>
2016-11-11 17:47:44 +0100

    PROJECTFORGE-1625 Add salary import to eed plugin

--- projectforge-business/src/main/java/org/projectforge/business/fibu/api/EmployeeSalaryService.java
+++ projectforge-business/src/main/java/org/projectforge/business/fibu/api/EmployeeSalaryService.java
@@ -1,5 +1,7 @@
 package org.projectforge.business.fibu.api;
 
+import java.util.Calendar;
+
 import org.projectforge.business.fibu.EmployeeDO;
 import org.projectforge.business.fibu.EmployeeSalaryDO;
 
@@ -7,5 +9,11 @@ public interface EmployeeSalaryService
 {
 
   EmployeeSalaryDO getLatestSalaryForEmployee(EmployeeDO employee);
+
+  EmployeeSalaryDO getEmployeeSalaryByDate(EmployeeDO employee, Calendar selectedDate);
+
+  void saveOrUpdate(EmployeeSalaryDO employeeSalaryDO);
+
+  EmployeeSalaryDO selectByPk(Integer id);
 
 }
