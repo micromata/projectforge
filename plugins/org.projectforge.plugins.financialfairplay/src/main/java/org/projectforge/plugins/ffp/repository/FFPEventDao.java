97061abb78d752ec158dfdb37edce0ebc97e374a
Florian Blumenstein <f.blumenstein@micromata.de>
2016-12-26 18:37:01 +0100

    Add new dept monito page

--- plugins/org.projectforge.plugins.financialfairplay/src/main/java/org/projectforge/plugins/ffp/repository/FFPEventDao.java
+++ plugins/org.projectforge.plugins.financialfairplay/src/main/java/org/projectforge/plugins/ffp/repository/FFPEventDao.java
@@ -1,19 +1,28 @@
 package org.projectforge.plugins.ffp.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.projectforge.business.fibu.EmployeeDO;
 import org.projectforge.framework.persistence.api.BaseDao;
+import org.projectforge.framework.persistence.jpa.PfEmgrFactory;
 import org.projectforge.plugins.ffp.FinancialFairPlayPluginUserRightId;
+import org.projectforge.plugins.ffp.model.FFPDebtDO;
 import org.projectforge.plugins.ffp.model.FFPEventDO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 /**
  * Access to ffp events.
- * 
- * @author Florian Blumenstein
  *
+ * @author Florian Blumenstein
  */
 @Repository
 public class FFPEventDao extends BaseDao<FFPEventDO>
 {
+  @Autowired
+  private PfEmgrFactory emgrFactory;
+
   public FFPEventDao()
   {
     super(FFPEventDO.class);
@@ -26,4 +35,8 @@ public class FFPEventDao extends BaseDao<FFPEventDO>
     return new FFPEventDO();
   }
 
+  public List<FFPDebtDO> getDeptList(EmployeeDO employee)
+  {
+    return new ArrayList<>();
+  }
 }
