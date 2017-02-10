f14d035e14524956f313ffa0d775aa6bfb07c6b2
Christian Tausendfreund <c.tausendfreund@micromata.de>
2017-02-10 10:01:10 +0100

    PROJECTFORGE-2890 add migration for new table column

--- projectforge-business/src/main/java/org/projectforge/framework/persistence/database/DatabaseCoreUpdates.java
+++ projectforge-business/src/main/java/org/projectforge/framework/persistence/database/DatabaseCoreUpdates.java
@@ -113,6 +113,33 @@ public class DatabaseCoreUpdates
     final List<UpdateEntry> list = new ArrayList<>();
 
     ////////////////////////////////////////////////////////////////////
+    // 6.8.0
+    // /////////////////////////////////////////////////////////////////
+    list.add(new UpdateEntryImpl(CORE_REGION_ID, "6.8.0", "2017-02-15", "Add possibility to create applications for leave of a half day.")
+    {
+      @Override
+      public UpdatePreCheckStatus runPreCheck()
+      {
+        log.info("Running pre-check for ProjectForge version 6.8.0");
+        if (databaseUpdateService.doesTableAttributeExist("t_employee_vacation", "is_half_day") == false) {
+          return UpdatePreCheckStatus.READY_FOR_UPDATE;
+        }
+        return UpdatePreCheckStatus.ALREADY_UPDATED;
+      }
+
+      @Override
+      public UpdateRunningStatus runUpdate()
+      {
+        if (databaseUpdateService.doesTableAttributeExist("t_employee_vacation", "is_half_day") == false) {
+          //Updating the schema
+          initDatabaseDao.updateSchema();
+        }
+        return UpdateRunningStatus.DONE;
+      }
+
+    });
+
+    ////////////////////////////////////////////////////////////////////
     // 6.7.0
     // /////////////////////////////////////////////////////////////////
     list.add(new UpdateEntryImpl(CORE_REGION_ID, "6.7.0", "2017-01-11",
