6c2da46fb5729f29f9455a6a2053d4129647931f
Christian Tausendfreund <c.tausendfreund@micromata.de>
2017-04-12 11:35:46 +0200

    PROJECTFORGE-3048 add unit test for ConfigurationService

---
+++ projectforge-business/src/test/java/org/projectforge/business/configuration/ConfigurationServiceTest.java
@@ -0,0 +1,36 @@
+package org.projectforge.business.configuration;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+import org.projectforge.framework.configuration.ConfigurationDao;
+import org.projectforge.framework.configuration.ConfigurationParam;
+import org.projectforge.framework.configuration.entities.ConfigurationDO;
+import org.projectforge.test.AbstractTestBase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.annotations.Test;
+
+public class ConfigurationServiceTest extends AbstractTestBase
+{
+  @Autowired
+  private ConfigurationService configurationService;
+
+  @Autowired
+  private ConfigurationDao configurationDao;
+
+  @Test
+  public void testGetMinPasswordLength()
+  {
+    // default
+    assertEquals(10, configurationService.getMinPasswordLength());
+
+    final ConfigurationDO minPwLenEntry = configurationDao.getEntry(ConfigurationParam.MIN_PASSWORD_LENGTH);
+    minPwLenEntry.setIntValue(16);
+    configurationDao.internalUpdate(minPwLenEntry);
+    assertEquals(16, configurationService.getMinPasswordLength());
+
+    // null -> use default
+    minPwLenEntry.setIntValue(null);
+    configurationDao.internalUpdate(minPwLenEntry);
+    assertEquals(10, configurationService.getMinPasswordLength());
+  }
+}
