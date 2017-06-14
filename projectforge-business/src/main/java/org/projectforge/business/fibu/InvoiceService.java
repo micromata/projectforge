9f6e82ccebe3f46d37c08c225b3dac17f4c4877f
Florian Blumenstein <f.blumenstein@micromata.de>
2017-06-14 10:12:16 +0200

    PROJECTFORGE-1986 Some code and template updates

--- projectforge-business/src/main/java/org/projectforge/business/fibu/InvoiceService.java
+++ projectforge-business/src/main/java/org/projectforge/business/fibu/InvoiceService.java
@@ -4,10 +4,13 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.xml.namespace.QName;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
@@ -26,6 +29,8 @@ import org.springframework.stereotype.Service;
 
 /**
  * Created by blumenstein on 08.05.17.
+ * <p>
+ * Copy some code from https://stackoverflow.com/questions/22268898/replacing-a-text-in-apache-poi-xwpf
  */
 @Service
 public class InvoiceService
@@ -51,16 +56,25 @@ public class InvoiceService
         invoiceTemplate = applicationContext.getResource("file://" + resourceDir + "/officeTemplates/" + customInvoiceTemplateName);
       }
       if (invoiceTemplate == null || invoiceTemplate.exists() == false) {
-        invoiceTemplate = applicationContext.getResource("classpath:officeTemplates/invoiceTemplate.docx");
+        invoiceTemplate = applicationContext.getResource("classpath:officeTemplates/InvoiceTemplate.docx");
       }
       XWPFDocument templateDocument = readWordFile(invoiceTemplate.getInputStream());
-      replaceTextInDoc(templateDocument, "Rechnungsadresse", data.getCustomerAddress());
-      replaceTextInDoc(templateDocument, "Typ", data.getTyp().toString());
-      replaceTextInDoc(templateDocument, "Kundenreferenz", data.getCustomerref1());
-      replaceTextInDoc(templateDocument, "Kundenreferenz2", data.getCustomerref2());
-      replaceTextInDoc(templateDocument, "Auftragsnummer", data.getPositionen().get(0).getAuftragsPosition().getAuftrag().getNummer().toString());
-      replaceTextInDoc(templateDocument, "Nummer", data.getNummer().toString());
-      replaceTextInDoc(templateDocument, "Rechnungsdatum", data.getDatum().toString());
+      Map<String, String> map = new HashMap<>();
+      map.put("{Rechnungsadresse}", data.getCustomerAddress());
+      map.put("{Typ}", data.getTyp().toString());
+      map.put("{Kundenreferenz}", data.getCustomerref1());
+      map.put("{Kundenreferenz2}", data.getCustomerref2());
+      map.put("{Auftragsnummer}", data.getPositionen().get(0).getAuftragsPosition().getAuftrag().getNummer().toString());
+      map.put("{Nummer}", data.getNummer().toString());
+      map.put("{Rechnungsdatum}", data.getDatum().toString());
+      replace(templateDocument, map);
+      //      replaceTextInDoc(templateDocument, "Rechnungsadresse", data.getCustomerAddress());
+      //      replaceTextInDoc(templateDocument, "Typ", data.getTyp().toString());
+      //      replaceTextInDoc(templateDocument, "Kundenreferenz", data.getCustomerref1());
+      //      replaceTextInDoc(templateDocument, "Kundenreferenz2", data.getCustomerref2());
+      //      replaceTextInDoc(templateDocument, "Auftragsnummer", data.getPositionen().get(0).getAuftragsPosition().getAuftrag().getNummer().toString());
+      //      replaceTextInDoc(templateDocument, "Nummer", data.getNummer().toString());
+      //      replaceTextInDoc(templateDocument, "Rechnungsdatum", data.getDatum().toString());
       //Skonto
       if (data.getDiscountPercent() != null) {
         String skontoText =
@@ -98,6 +112,112 @@ public class InvoiceService
     return result;
   }
 
+  public void replace(XWPFDocument document, Map<String, String> map)
+  {
+    List<XWPFParagraph> paragraphs = document.getParagraphs();
+    for (XWPFParagraph paragraph : paragraphs) {
+      if (StringUtils.isEmpty(paragraph.getText()) == false) {
+        replace(paragraph, map);
+      }
+      if (checkForTextBox(paragraph)) {
+        replaceInTextBox(paragraph, map);
+      }
+    }
+  }
+
+  private boolean checkForTextBox(final XWPFParagraph paragraph)
+  {
+    XmlObject[] textBoxObjects = paragraph.getCTP().selectPath("declare namespace w='http://schemas.openxmlformats.org/wordprocessingml/2006/main' "
+        + "declare namespace wps='http://schemas.microsoft.com/office/word/2010/wordprocessingShape' .//*/wps:txbx/w:txbxContent");
+    return textBoxObjects.length > 0;
+  }
+
+  private void replaceInTextBox(final XWPFParagraph paragraph, final Map<String, String> map)
+  {
+    for (Map.Entry<String, String> entry : map.entrySet()) {
+      String searchText = entry.getKey();
+      //Text-Box
+      XmlObject[] textBoxObjects = paragraph.getCTP().selectPath("declare namespace w='http://schemas.openxmlformats.org/wordprocessingml/2006/main' "
+          + "declare namespace wps='http://schemas.microsoft.com/office/word/2010/wordprocessingShape' .//*/wps:txbx/w:txbxContent");
+      for (int i = 0; i < textBoxObjects.length; i++) {
+        XWPFParagraph embeddedPara = null;
+        try {
+          XmlObject[] paraObjects = textBoxObjects[i].selectChildren(new QName("http://schemas.openxmlformats.org/wordprocessingml/2006/main", "p"));
+          for (int j = 0; j < paraObjects.length; j++) {
+            XWPFParagraph textBoxParagraph = new XWPFParagraph(CTP.Factory.parse(paraObjects[j].xmlText()), paragraph.getBody());
+            if (StringUtils.contains(textBoxParagraph.getText(), entry.getKey())) {
+              replace(textBoxParagraph, entry.getKey(),
+                  entry.getValue() != null ? entry.getValue() : "");
+              textBoxParagraph.
+            }
+          }
+        } catch (XmlException e) {
+          //handle
+        }
+      }
+    }
+  }
+
+  public void replace(XWPFDocument document, String searchText, String replacement)
+  {
+    List<XWPFParagraph> paragraphs = document.getParagraphs();
+    for (XWPFParagraph paragraph : paragraphs) {
+      if (StringUtils.isEmpty(paragraph.getText()) == false && StringUtils.contains(paragraph.getText(), searchText)) {
+        replace(paragraph, searchText, replacement);
+      }
+    }
+  }
+
+  private void replace(XWPFParagraph paragraph, Map<String, String> map)
+  {
+    for (Map.Entry<String, String> entry : map.entrySet()) {
+      String searchText = entry.getKey();
+      if (StringUtils.isEmpty(paragraph.getText()) == false && StringUtils.contains(paragraph.getText(), searchText)) {
+        replace(paragraph, searchText, entry.getValue() != null ? entry.getValue() : "");
+      }
+    }
+  }
+
+  public void replace(XWPFParagraph paragraph, String searchText, String replacement)
+  {
+    boolean found = true;
+    while (found) {
+      found = false;
+      int pos = paragraph.getText().indexOf(searchText);
+      if (pos >= 0) {
+        found = true;
+        Map<Integer, XWPFRun> posToRuns = getPosToRuns(paragraph);
+        XWPFRun run = posToRuns.get(pos);
+        XWPFRun lastRun = posToRuns.get(pos + searchText.length() - 1);
+        int runNum = paragraph.getRuns().indexOf(run);
+        int lastRunNum = paragraph.getRuns().indexOf(lastRun);
+        if (replacement.contains("\r\n")) {
+          replacement = replacement.replace("\r", "");
+        }
+        run.setText(replacement, 0);
+        for (int i = lastRunNum; i > runNum; i--) {
+          paragraph.removeRun(i);
+        }
+      }
+    }
+  }
+
+  private Map<Integer, XWPFRun> getPosToRuns(XWPFParagraph paragraph)
+  {
+    int pos = 0;
+    Map<Integer, XWPFRun> map = new HashMap<Integer, XWPFRun>(10);
+    for (XWPFRun run : paragraph.getRuns()) {
+      String runText = run.text();
+      if (runText != null) {
+        for (int i = 0; i < runText.length(); i++) {
+          map.put(pos + i, run);
+        }
+        pos += runText.length();
+      }
+    }
+    return (map);
+  }
+
   private void generateTableText(XWPFDocument docx, RechnungsPositionDO pos, RechnungDO data)
   {
     replaceTextInTable(docx, "id", String.valueOf(pos.getNumber()));
@@ -130,6 +250,8 @@ public class InvoiceService
   {
     //Zeilen
     for (XWPFParagraph p : docx.getParagraphs()) {
+      String pText = p.getText();
+      log.info("Found text in word document (Paragraph): " + pText);
       searchInRuns(p.getRuns(), text, replaceText);
     }
     //Text-Box
@@ -156,6 +278,7 @@ public class InvoiceService
     if (runs != null) {
       for (XWPFRun r : runs) {
         String tmp_text = r.getText(0);
+        log.info("Found text in word document (Run): " + tmp_text);
         if (tmp_text != null && tmp_text.contains("{" + text + "}")) {
           if (replaceText == null) {
             tmp_text = tmp_text.replace("{" + text + "}", "");
