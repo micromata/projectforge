4118eb20da824fdf34448d090ac41e8b62e8d922
Florian Blumenstein <f.blumenstein@micromata.de>
2017-08-11 10:11:31 +0200

    Fix address image data migration

--- projectforge-business/src/main/java/org/projectforge/framework/persistence/database/DatabaseCoreUpdates.java
+++ projectforge-business/src/main/java/org/projectforge/framework/persistence/database/DatabaseCoreUpdates.java
@@ -225,8 +225,10 @@ public class DatabaseCoreUpdates
 
       private boolean isImageDataPreviewMissing()
       {
-        return databaseUpdateService.doesTableAttributeExist("t_address", "image_data_preview") == false
-            || databaseUpdateService.query("select pk from t_address where image_data_preview is not null LIMIT 1").size() < 1;
+        return databaseUpdateService.doesTableAttributeExist("t_address", "image_data_preview") == false ||
+            ((int) databaseUpdateService.query("select count(*) from t_address where imagedata is not null AND imagedata != ''").get(0).getEntry(0).getValue())
+                > 0
+                && databaseUpdateService.query("select pk from t_address where imagedata is not null AND image_data_preview is not null LIMIT 1").size() < 1;
       }
 
       private boolean oldUniqueConstraint()
