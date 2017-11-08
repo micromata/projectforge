ffe00213cc9082a2107ffe01d4165277e2056dab
Florian Blumenstein <f.blumenstein@micromata.de>
2017-11-08 15:41:53 +0100

    Finalize PF docker

--- projectforge-docker/database/StartDatabase.sh
+++ projectforge-docker/database/StartDatabase.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
+
 docker info > /dev/null || exit 1
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 [host port]"
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 [database type] [host port]"
     exit
 fi
 
+# Create the projectforge network, if it is not already created
+projectforge_network_created=$( docker network ls -f name=projectforge_network -q )
+if [ -z $projectforge_network_created ]
+then
+    docker network create projectforge_network
+fi
+
 hostPort="$1"
+databaseType="$2"
 
-echo 'HOST_PORT='$hostPort > .env
+if [ "$databaseType" == "postgres" ]; then
+  echo "Using postgres db"
+  cd postgres
+  echo 'HOST_PORT='$hostPort > .env
+  docker-compose up -d
+fi
 
-docker-compose up -d
