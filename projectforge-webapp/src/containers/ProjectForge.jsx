f63bd35ac6da9f40a98b6784f45aaa3612e26ef1
Fin Reinhard <fin.reinhard@icloud.com>
2019-03-01 14:26:14 +0100

    adding TopBar
    enabled eslint with airbnb styles

--- projectforge-webapp/src/containers/ProjectForge.jsx
+++ projectforge-webapp/src/containers/ProjectForge.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
+import TopBar from '../components/base/topbar/View';
 
-function ProjectForge({loggedIn}) {
-    if (!loggedIn) {
-
-    }
+function ProjectForge() {
+    return (
+        <React.Fragment>
+            <TopBar />
+        </React.Fragment>
+    );
 }
 
 const mapStateToProps = state => ({
-    loggedIn: state.authentication.user !== null
+    loggedIn: state.authentication.user !== null,
 });
 
 export default connect(mapStateToProps)(ProjectForge);
