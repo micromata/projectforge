edac533e31f2f801d9035011bd12c86905343314
Fin Reinhard <fin.reinhard@icloud.com>
2019-03-02 23:35:07 +0100

    finished base page

--- projectforge-webapp/src/index.js
+++ projectforge-webapp/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
-import {Provider} from 'react-redux';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/style/projectforge.scss';
 import reducer from './reducers';
 import ProjectForge from './containers/ProjectForge';
 import * as serviceWorker from './serviceWorker';
@@ -9,10 +11,11 @@ import * as serviceWorker from './serviceWorker';
 const store = createStore(reducer);
 
 ReactDOM.render(
+    /* eslint-disable-next-line */
     <Provider store={store}>
         <ProjectForge />
     </Provider>,
-    document.getElementById('root')
+    document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
