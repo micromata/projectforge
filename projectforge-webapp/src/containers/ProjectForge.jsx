2648303ad54b74e6d88de60c530d8618b8c9f291
Fin Reinhard <fin.reinhard@icloud.com>
2019-03-02 23:35:07 +0100

    finished base page

--- projectforge-webapp/src/containers/ProjectForge.jsx
+++ projectforge-webapp/src/containers/ProjectForge.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import TopBar from '../components/base/topbar/View';
+import PropTypes from 'prop-types';
+import TopBar from '../components/base/topbar';
+import Footer from '../components/base/footer';
 
-function ProjectForge() {
+function ProjectForge({ loggedIn }) {
+    let content;
+
+    if (loggedIn) {
+        content = 'Logged In';
+    } else {
+        content = 'Login Screen';
+    }
+
     return (
         <React.Fragment>
             <TopBar />
+            {content}
+            <Footer version="Version 6.25-SNAPSHOT, 2019-02-26" updateAvailable />
         </React.Fragment>
     );
 }
+
+ProjectForge.propTypes = {
+    loggedIn: PropTypes.bool,
+};
+
+ProjectForge.defaultProps = {
+    loggedIn: false,
+};
 
 const mapStateToProps = state => ({
     loggedIn: state.authentication.user !== null,
