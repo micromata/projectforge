dcbcb3a3422e34661f88029fcb63ede23e1ee279
Fin Reinhard <fin.reinhard@icloud.com>
2019-03-08 11:19:19 +0100

    applying eslint to redux files

--- projectforge-webapp/src/reducers/index.js
+++ projectforge-webapp/src/reducers/index.js
@@ -1,7 +1,7 @@
-import {combineReducers} from 'redux';
+import { combineReducers } from 'redux';
 import authentication from './authentication';
 
 
 export default combineReducers({
-    authentication
+    authentication,
 });
