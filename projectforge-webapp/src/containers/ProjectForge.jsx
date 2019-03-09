e8baaeb006e0e8ce4e768c5e71c6dee53b550128
Fin Reinhard <fin.reinhard@icloud.com>
2019-03-09 16:36:54 +0100

    adding Session Management

--- projectforge-webapp/src/containers/ProjectForge.jsx
+++ projectforge-webapp/src/containers/ProjectForge.jsx
@@ -1,38 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import TopBar from '../components/base/topbar';
 import Footer from '../components/base/footer';
 import LoginView from '../components/authentication/LoginView';
-import { loginUser } from '../actions';
+import { loadSessionIfAvailable, loginUser } from '../actions';
 
-function ProjectForge({ loggedIn, loginUser: login }) {
-    let content;
+class ProjectForge extends Component {
+    componentDidMount() {
+        const { loadSessionIfAvailable: loadSession } = this.props;
 
-    if (loggedIn) {
-        content = 'Logged In';
-    } else {
-        content = (
-            <LoginView
-                // TODO: EXAMPLE DATA, REPLACE WITH REAL DATA FROM REST API
-                motd="Please try user demo with password demo123. Have a lot of fun!"
-                login={login}
-            />
-        );
+        loadSession();
     }
 
-    return (
-        <React.Fragment>
-            <TopBar />
-            {content}
-            <Footer version="Version 6.25-SNAPSHOT, 2019-02-26" updateAvailable />
-        </React.Fragment>
-    );
+
+    render() {
+        const { loggedIn, loginUser: login } = this.props;
+        let content;
+
+        if (loggedIn) {
+            content = 'Logged In';
+        } else {
+            content = (
+                <LoginView
+                    // TODO: EXAMPLE DATA, REPLACE WITH REAL DATA FROM REST API
+                    motd="Please try user demo with password demo123. Have a lot of fun!"
+                    login={login}
+                />
+            );
+        }
+
+        return (
+            <React.Fragment>
+                <TopBar />
+                {content}
+                <Footer version="Version 6.25-SNAPSHOT, 2019-02-26" updateAvailable />
+            </React.Fragment>
+        );
+    }
 }
 
 ProjectForge.propTypes = {
     loginUser: PropTypes.func.isRequired,
     loggedIn: PropTypes.bool.isRequired,
+    loadSessionIfAvailable: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
@@ -41,6 +52,7 @@ const mapStateToProps = state => ({
 
 const actions = {
     loginUser,
+    loadSessionIfAvailable,
 };
 
 export default connect(mapStateToProps, actions)(ProjectForge);
--- projectforge-webapp/src/utilities/rest.js
+++ projectforge-webapp/src/utilities/rest.js
@@ -5,11 +5,30 @@ const createQueryParams = params => Object.keys(params)
     .map(key => `${key}=${encodeURI(params[key])}`)
     .join('&');
 
-/* eslint-disable-next-line import/prefer-default-export */
 export const getServiceURL = (serviceURL, params) => {
     if (params) {
         return `${baseURL}/${serviceURL}?${createQueryParams(params)}`;
     }
 
     return `${baseURL}/${serviceURL}`;
+};
+
+// ATTENTION: Only for the Login Process.
+export const getLoginHeaders = (username, password) => ({
+    'Authentication-Username': username,
+    'Authentication-Password': password,
+});
+
+// ATTENTION: For everything except the Login Process.
+export const getAuthenticationHeaders = (userId, token) => ({
+    'Authentication-User-Id': userId,
+    'Authentication-Token': token,
+});
+
+export const handleHTTPErrors = (response) => {
+    if (!response.ok) {
+        throw Error(response.statusText);
+    }
+
+    return response;
 };
