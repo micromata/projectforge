fe57e6d78bcbba845fb4b4efe1fc9a87836aebcc
Fin Reinhard <fin.reinhard@icloud.com>
2019-03-10 20:48:17 +0100

    add comments for the categories dropdown algorithm

--- projectforge-webapp/src/components/base/navigation/categories-dropdown/index.jsx
+++ projectforge-webapp/src/components/base/navigation/categories-dropdown/index.jsx
@@ -16,18 +16,26 @@ function countColumnSize(column) {
 }
 
 function CategoriesDropdown({ categories }) {
+    // Creating an array of columns for the categories.
     const columns = [
         [], [], [], [],
     ];
 
+    // Add the 'General' category to the first column, so it will always be first.
     columns[0].push(categories[0]);
 
+    // Summarized: Balancing the categories to get less white space.
     categories
+    // Remove the first ('general') category.
         .slice(1)
+        // Sort the categories by their items size.
         .sort((categoryA, categoryB) => categoryB.items.length - categoryA.items.length)
+        // forEach through all categories.
         .forEach(category => columns
+        // Compare all columns and get the smallest one.
             .reduce((columnA, columnB) => (countColumnSize(columnA) < countColumnSize(columnB)
                 ? columnA : columnB))
+            // Add the category to the smallest column.
             .push(category));
 
     // TODO: ADD STYLE FOR DROPDOWN MENU
