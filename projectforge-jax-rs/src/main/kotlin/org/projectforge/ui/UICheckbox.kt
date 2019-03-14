6b4781a45451d9a4d6688bf8591c5a4fe4c14a22
Kai Reinhard <K.Reinhard@micromata.de>
2019-03-14 13:32:29 +0100

    Builder replaced by UI classes.

---
+++ projectforge-jax-rs/src/main/kotlin/org/projectforge/ui/UICheckbox.kt
@@ -0,0 +1,5 @@
+package org.projectforge.ui
+
+import com.google.gson.annotations.SerializedName
+
+data class UICheckbox(val id: String) : UIElement(UIElementType.CHECKBOX)
---
+++ projectforge-jax-rs/src/main/kotlin/org/projectforge/ui/UICol.kt
@@ -0,0 +1,9 @@
+package org.projectforge.ui
+
+data class UICol(val length: Int,
+                 val content: MutableList<UIElement> = mutableListOf()) : UIElement(UIElementType.COL) {
+    fun add(element: UIElement): UICol {
+        content?.add(element)
+        return this
+    }
+}
