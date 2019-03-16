cc16423472cbd064ed5b98d72e6dc01b209a142c
Kai Reinhard <K.Reinhard@micromata.de>
2019-03-16 09:43:06 +0100

    Kotlin is fun...

--- projectforge-jax-rs/src/main/kotlin/org/projectforge/ui/UIGroup.kt
+++ projectforge-jax-rs/src/main/kotlin/org/projectforge/ui/UIGroup.kt
@@ -1,38 +1,48 @@
 package org.projectforge.ui
 
+import org.projectforge.rest.ui.LayoutUtils
+
 /**
  * A group represents a group of UI elements, such as a pair of a Label and an Input field.
  */
 data class UIGroup(val content: MutableList<UIElement> = mutableListOf()) : UIElement(UIElementType.GROUP) {
+    @Transient
+    private val log = org.slf4j.LoggerFactory.getLogger(UIGroup::class.java)
+
     fun add(element: UIElement): UIGroup {
         content.add(element)
         return this
     }
 
     /**
-     * Adds a UILabel and the given UIInput. UILabel.labelFor is set with id of given input.
+     * Convenient method:
+     * Adds a UILabel and the given input. UILabel.labelFor is set with id of given input.
+     * @param label The value of the label to add as UILabel.
+     * @param input The element (of type UIInput, UITextArea or UISelect).
      */
-    fun add(label: String, input: UIInput): UIGroup {
-        content.add(UILabel(label, input.id, reference = input))
+    fun add(label: String, input: UIElement): UIGroup {
+        return add(UILabel(label), input)
+    }
+
+    /**
+     * Convenient method:
+     * Adds a UILabel and the given input. UILabel.labelFor is set with id of given input.
+     * @param label The label to add.
+     * @param input The element (of type UIInput, UITextArea or UISelect).
+     */
+    fun add(label: UILabel, input: UIElement): UIGroup {
+        label.reference = input
+        label.labelFor =
+                when (input) {
+                    is UIInput -> input.id
+                    is UITextarea -> input.id
+                    is UISelect -> input.id
+                    else -> null
+                }
+        if (label.labelFor == null)
+            log.error("Unsupported element for method add(UILabel, UIElement): '${input}'. Supported elements are UIInput, UISelect and UITextArea")
+        content.add(label)
         content.add(input)
-        return this;
-    }
-
-    /**
-     * Adds a UILabel and the given UISelect. UILabel.labelFor is set with id of given input.
-     */
-    fun add(label: String, textArea: UITextarea): UIGroup {
-        content.add(UILabel(label, textArea.id, reference = textArea))
-        content.add(textArea)
-        return this;
-    }
-
-    /**
-     * Adds a UILabel and the given UISelect. UILabel.labelFor is set with id of given input.
-     */
-    fun add(label: String, select: UISelect): UIGroup {
-        content.add(UILabel(label, select.id, reference = select))
-        content.add(select)
         return this;
     }
 }
--- projectforge-jax-rs/src/main/kotlin/org/projectforge/ui/UILabel.kt
+++ projectforge-jax-rs/src/main/kotlin/org/projectforge/ui/UILabel.kt
@@ -4,5 +4,5 @@ import com.google.gson.annotations.SerializedName
 
 data class UILabel(var value: String,
                    @SerializedName("for")
-                   val labelFor:String? = null,
+                   var labelFor:String? = null,
                    @Transient var reference : UIElement? = null) : UIElement(UIElementType.LABEL)
