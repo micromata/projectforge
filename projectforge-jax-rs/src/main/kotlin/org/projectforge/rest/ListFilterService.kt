8a8df01ef38cb1126a4f558073501d0db51a0c15
Kai Reinhard <K.Reinhard@micromata.de>
2019-03-18 17:20:16 +0100

    /rs/*/initial-list returns now filter, list and layout.

---
+++ projectforge-jax-rs/src/main/kotlin/org/projectforge/rest/ListFilterService.kt
@@ -0,0 +1,41 @@
+package org.projectforge.rest
+
+import org.projectforge.framework.persistence.api.BaseSearchFilter
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import javax.servlet.http.HttpSession
+
+/**
+ * For persisting list filters.
+ */
+@Service
+class ListFilterService {
+    private val log = org.slf4j.LoggerFactory.getLogger(ListFilterService::class.java)
+
+    @Autowired
+    private val userPreferencesService: UserPreferencesService? = null
+
+    fun getSearchFilter(session: HttpSession, filterClazz: Class<out BaseSearchFilter>): BaseSearchFilter {
+        val filter = userPreferencesService!!.getEntry(session, filterClazz.name + ":Filter")
+        if (filter != null) {
+            if (filter!!.javaClass == filterClazz) {
+                try {
+                    return filter as BaseSearchFilter
+                } catch (ex: ClassCastException) {
+                    // No output needed, info message follows:
+                }
+                // Probably a new software release results in an incompability of old and new filter format.
+                log.info(
+                        "Could not restore filter from user prefs: (old) filter type "
+                                + filter!!.javaClass.getName()
+                                + " is not assignable to (new) filter type "
+                                + filterClazz.javaClass.getName()
+                                + " (OK, probably new software release).")
+            }
+        }
+        val result = filterClazz.newInstance()
+        result.reset()
+        userPreferencesService!!.putEntry(session, filterClazz.name + ":Filter", result, true)
+        return result
+    }
+}
