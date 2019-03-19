2bb22bfce5dec97a3f8f68775c8a410672182503
Fin Reinhard <fin.reinhard@icloud.com>
2019-03-19 14:29:11 +0100

    add search action

--- projectforge-webapp/src/actions/listPage.js
+++ projectforge-webapp/src/actions/listPage.js
@@ -9,6 +9,9 @@ export const LIST_PAGE_FILTER_SET = 'LIST_PAGE_FILTER_SET';
 export const LIST_PAGE_FILTER_RESET_BEGIN = 'LIST_PAGE_FILTER_RESET_BEGIN';
 export const LIST_PAGE_FILTER_RESET_SUCCESS = 'LIST_PAGE_FILTER_RESET_SUCCESS';
 
+export const LIST_PAGE_DATA_UPDATE_BEGIN = 'LIST_PAGE_DATA_UPDATE_BEGIN';
+export const LIST_PAGE_DATA_UPDATE_SUCCESS = 'LIST_PAGE_DATA_UPDATE_SUCCESS';
+
 export const loadBegin = () => ({ type: LIST_PAGE_LOAD_BEGIN });
 
 export const loadSuccess = (filter, ui, data) => ({
@@ -40,10 +43,17 @@ export const filterResetSuccess = filter => ({
     payload: { filter },
 });
 
+export const dataUpdateBegin = () => ({ type: LIST_PAGE_DATA_UPDATE_BEGIN });
+
+export const dataUpdateSuccess = data => ({
+    type: LIST_PAGE_DATA_UPDATE_SUCCESS,
+    payload: { data },
+});
+
 export const loadList = () => (dispatch) => {
     dispatch(loadBegin());
 
-    fetch(
+    return fetch(
         getServiceURL('books/initial-list'),
         {
             method: 'GET',
@@ -63,7 +73,7 @@ export const setFilter = (id, newValue) => (dispatch) => {
 export const resetFilter = () => (dispatch) => {
     dispatch(filterResetBegin());
 
-    fetch(
+    return fetch(
         getServiceURL('books/filterReset'),
         {
             method: 'GET',
@@ -73,5 +83,25 @@ export const resetFilter = () => (dispatch) => {
         .then(handleHTTPErrors)
         .then(response => response.json())
         .then(json => dispatch(filterResetSuccess(json)))
+        .catch(error => dispatch(loadFailure(error.message)));
+};
+
+export const updateData = () => (dispatch, getState) => {
+    dispatch(dataUpdateBegin());
+
+    return fetch(
+        getServiceURL('books/list'),
+        {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(getState().listPage.filter),
+        },
+    )
+        .then(handleHTTPErrors)
+        .then(response => response.json())
+        .then(json => dispatch(dataUpdateSuccess(json)))
         .catch(error => dispatch(loadFailure(error.message)));
 };
