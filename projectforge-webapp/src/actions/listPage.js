9554c0bd683c7fe0b1bce16088d68b236bab36dc
Fin Reinhard <fin.reinhard@icloud.com>
2019-03-19 14:07:29 +0100

    add Reset Filter action

--- projectforge-webapp/src/actions/listPage.js
+++ projectforge-webapp/src/actions/listPage.js
@@ -6,6 +6,9 @@ export const LIST_PAGE_LOAD_FAILURE = 'LIST_PAGE_LOAD_FAILURE';
 
 export const LIST_PAGE_FILTER_SET = 'LIST_PAGE_FILTER_SET';
 
+export const LIST_PAGE_FILTER_RESET_BEGIN = 'LIST_PAGE_FILTER_RESET_BEGIN';
+export const LIST_PAGE_FILTER_RESET_SUCCESS = 'LIST_PAGE_FILTER_RESET_SUCCESS';
+
 export const loadBegin = () => ({ type: LIST_PAGE_LOAD_BEGIN });
 
 export const loadSuccess = (filter, ui, data) => ({
@@ -30,6 +33,13 @@ export const filterSet = (id, newValue) => ({
     },
 });
 
+export const filterResetBegin = () => ({ type: LIST_PAGE_FILTER_RESET_BEGIN });
+
+export const filterResetSuccess = filter => ({
+    type: LIST_PAGE_FILTER_RESET_SUCCESS,
+    payload: { filter },
+});
+
 export const loadList = () => (dispatch) => {
     dispatch(loadBegin());
 
@@ -48,4 +58,20 @@ export const loadList = () => (dispatch) => {
 
 export const setFilter = (id, newValue) => (dispatch) => {
     dispatch(filterSet(id, newValue));
+};
+
+export const resetFilter = () => (dispatch) => {
+    dispatch(filterResetBegin());
+
+    fetch(
+        getServiceURL('books/filterReset'),
+        {
+            method: 'GET',
+            credentials: 'include',
+        },
+    )
+        .then(handleHTTPErrors)
+        .then(response => response.json())
+        .then(json => dispatch(filterResetSuccess(json)))
+        .catch(error => dispatch(loadFailure(error.message)));
 };
