d73455863da3ec04d9b8415916c735eaaab6fbe6
Kai Reinhard <K.Reinhard@micromata.de>
2019-03-21 08:22:29 +0100

    Auto layout and i18n...

--- projectforge-jax-rs/src/main/kotlin/org/projectforge/ui/UIButton.kt
+++ projectforge-jax-rs/src/main/kotlin/org/projectforge/ui/UIButton.kt
@@ -1,3 +1,7 @@
 package org.projectforge.ui
 
-data class UIButton(val id : String, var title : String, val style : UIButtonStyle? = null) : UIElement(UIElementType.BUTTON)
+data class UIButton(val id : String,
+                    /** May be null for standard buttons. For standard buttons the title will be set dependent on the id. */
+                    var title : String? = null,
+                    val style : UIButtonStyle? = null)
+    : UIElement(UIElementType.BUTTON)
--- projectforge-jax-rs/src/main/kotlin/org/projectforge/ui/UICheckbox.kt
+++ projectforge-jax-rs/src/main/kotlin/org/projectforge/ui/UICheckbox.kt
@@ -1,4 +1,5 @@
 package org.projectforge.ui
 
 data class UICheckbox(val id: String,
-                      val label: String? = null) : UIElement(UIElementType.CHECKBOX)
+                      var label: String? = null,
+                      var tooltip: String? = null) : UIElement(UIElementType.CHECKBOX)
