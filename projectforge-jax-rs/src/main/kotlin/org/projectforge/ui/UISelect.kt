f1f02bc9d05460689a7700001a77a43630388890
Kai Reinhard <K.Reinhard@micromata.de>
2019-03-21 19:33:35 +0100

    Labels as part also of UISelect and UITextarea.

--- projectforge-jax-rs/src/main/kotlin/org/projectforge/ui/UISelect.kt
+++ projectforge-jax-rs/src/main/kotlin/org/projectforge/ui/UISelect.kt
@@ -1,13 +1,16 @@
 package org.projectforge.ui
 
-import org.projectforge.common.i18n.I18nEnum
+import com.google.gson.annotations.SerializedName
 
 data class UISelect(val id: String,
                     val values: MutableList<UISelectValue> = mutableListOf(),
                     val required: Boolean? = null,
                     @Transient
-                    val i18nEnum: Class<out Enum<*>>? = null)
-    : UIElement(UIElementType.SELECT) {
+                    val i18nEnum: Class<out Enum<*>>? = null,
+                    override var label: String? = null,
+                    @SerializedName("additional-label")
+                    override var additionalLabel: String? = null)
+    : UIElement(UIElementType.SELECT), UILabelledElement {
 
     fun add(selectValue: UISelectValue): UISelect {
         values.add(selectValue)
