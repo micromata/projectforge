d7bacc83518961397ae103879e5a33b42662d79b
Kai Reinhard <K.Reinhard@micromata.de>
2019-03-23 07:48:17 +0100

    Auto-layout: refactoring...

--- projectforge-jax-rs/src/main/kotlin/org/projectforge/ui/UIGroup.kt
+++ projectforge-jax-rs/src/main/kotlin/org/projectforge/ui/UIGroup.kt
@@ -25,7 +25,7 @@ data class UIGroup(val content: MutableList<UIElement> = mutableListOf()) : UIEl
             is UILabelledElement -> element.label
             else -> "??? ${id} ???"
         }
-        return add(UILabel(label = label), element)
+        return add(UILabel(label = label, protectLabel = true), element)
     }
 
     /**
