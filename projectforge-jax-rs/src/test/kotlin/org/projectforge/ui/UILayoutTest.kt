df2779ebb01efffd552a0108968c861e4554da53
Kai Reinhard <K.Reinhard@micromata.de>
2019-03-24 19:30:43 +0100

    Json validation...

--- projectforge-jax-rs/src/test/kotlin/org/projectforge/ui/UILayoutTest.kt
+++ projectforge-jax-rs/src/test/kotlin/org/projectforge/ui/UILayoutTest.kt
@@ -1,16 +1,13 @@
 package org.projectforge.ui
 
 import com.google.gson.GsonBuilder
-import de.micromata.genome.db.jpa.history.api.HistoryService
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.projectforge.business.book.BookDO
 import org.projectforge.rest.BookRest
-import org.projectforge.rest.core.ListFilterService
 import org.projectforge.rest.json.JsonValidator
 import org.projectforge.test.AbstractTestBase
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.stereotype.Component
 
 class UILayoutTest : AbstractTestBase() {
     @Autowired
@@ -41,5 +38,32 @@ class UILayoutTest : AbstractTestBase() {
         assertEquals("cancel", jsonValidator.get("actions[0].id"))
         assertEquals("undelete", jsonValidator.get("actions[1].id"))
         assertEquals(2, jsonValidator.getList("actions")?.size)
+    }
+
+    @Test
+    fun testEditBookLayout() {
+        val gson = GsonBuilder().create()
+
+        val book = BookDO()
+        var jsonString = gson.toJson(bookRest.createEditLayout(book))
+        var jsonValidator = JsonValidator(jsonString)
+        assertEquals("???book.title.add???", jsonValidator.get("title")) // translations not available in test.
+        assertField(jsonValidator.getMap("layout[0]"), "title", 255.0, "string", "??? book.title ???", type = "input", key = "el-1")
+        assertEquals(true, jsonValidator.getBoolean("layout[0].focus"))
+        assertField(jsonValidator.getMap("layout[1]"), "authors", 1000.0, null, "??? book.authors ???", type = "textarea", key = "el-2")
+        assertNull(jsonValidator.getBoolean("layout[1].focus"))
+    }
+
+    private fun assertField(element: Map<String, *>?, id: String, maxLength: Double, dataType: String?, label: String, type: String, key: String) {
+        assertNotNull(element)
+        if (element == null) return // Only for compiler: shouldn't occur due to previous assertNotNull statement.
+        assertEquals(id, element.get("id"))
+        assertEquals(maxLength, element.get("max-length"))
+        if (dataType != null)
+            assertEquals(dataType, element.get("data-type"))
+        else
+            assertNull(dataType)
+        assertEquals(type, element.get("type"))
+        assertEquals(key, element.get("key"))
     }
 }
