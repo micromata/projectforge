ce07fe7150cd6ad1400253eab7d0a839410092cb
Kai Reinhard <K.Reinhard@micromata.de>
2019-03-25 07:51:30 +0100

    UIButton: default button is now supported.

--- projectforge-jax-rs/src/main/kotlin/org/projectforge/ui/UIButton.kt
+++ projectforge-jax-rs/src/main/kotlin/org/projectforge/ui/UIButton.kt
@@ -3,5 +3,9 @@ package org.projectforge.ui
 data class UIButton(val id : String,
                     /** May be null for standard buttons. For standard buttons the title will be set dependent on the id. */
                     var title : String? = null,
-                    val style : UIButtonStyle? = null)
+                    val style : UIButtonStyle? = null,
+                    /**
+                     * There should be one default button in every form, used if the user hits return.
+                     */
+                    val default : Boolean? = null)
     : UIElement(UIElementType.BUTTON)
