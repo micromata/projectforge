81c1523888523910aececdb8a994b4da8370471a
Fin Reinhard <fin.reinhard@icloud.com>
2019-03-26 23:11:29 +0100

    add DELETE Option to CORS Filter

--- projectforge-jax-rs/src/main/java/org/projectforge/rest/config/CORSFilter.java
+++ projectforge-jax-rs/src/main/java/org/projectforge/rest/config/CORSFilter.java
@@ -30,7 +30,7 @@ public class CORSFilter implements Filter {
     HttpServletRequest request = (HttpServletRequest) servletRequest;
     // Authorize (allow) all domains(the domain the request came from) to consume the content
     ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
-    ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST");
+    ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods", "GET, OPTIONS, HEAD, PUT, POST, DELETE");
     ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Credentials", "true");
     ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Headers", "Content-Type");
     HttpServletResponse resp = (HttpServletResponse) servletResponse;
