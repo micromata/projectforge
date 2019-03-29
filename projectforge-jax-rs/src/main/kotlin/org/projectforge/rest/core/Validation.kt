8493f065840c9a3a06c5e12fd77105e2d29d46e7
Kai Reinhard <K.Reinhard@micromata.de>
2019-03-29 15:28:25 +0100

    Rest: Validation tests.

--- projectforge-jax-rs/src/main/kotlin/org/projectforge/rest/core/Validation.kt
+++ projectforge-jax-rs/src/main/kotlin/org/projectforge/rest/core/Validation.kt
@@ -10,7 +10,15 @@ import org.projectforge.ui.ValidationError
 
 class Validation {
     companion object {
-        fun validateInteger(validationErrors: MutableList<ValidationError>, fieldId: String, value: String, minValue: Int?, maxValue: Int?) {
+        internal val MSG_INT_FORMAT_ERROR = "validation.error.format.integer"
+        internal val MSG_INT_OUT_OF_RANGE = "validation.error.range.integerOutOfRange"
+        internal val MSG_INT_TO_LOW = "validation.error.range.integerOutOfRange"
+        internal val MSG_INT_TO_HIGH = "validation.error.range.integerOutOfRange"
+        fun validateInteger(validationErrors: MutableList<ValidationError>,
+                            fieldId: String,
+                            value: String,
+                            minValue: Int? = null,
+                            maxValue: Int? = null) {
             if (value.isNullOrBlank())
                 return
             try {
@@ -18,13 +26,21 @@ class Validation {
                 if (minValue != null) {
                     if (maxValue != null) {
                         if (number < minValue || number > maxValue)
-                            validationErrors.add(ValidationError(translateMsg("validation.error.range.integerOutOfRange", minValue, maxValue), fieldId = fieldId))
+                            validationErrors.add(ValidationError(translateMsg(MSG_INT_OUT_OF_RANGE, minValue, maxValue),
+                                    fieldId = fieldId,
+                                    messageId = MSG_INT_OUT_OF_RANGE))
                     } else if (number < minValue)
-                        validationErrors.add(ValidationError(translateMsg("validation.error.range.integerToLow", minValue), fieldId = fieldId))
+                        validationErrors.add(ValidationError(translateMsg(MSG_INT_TO_LOW, minValue),
+                                fieldId = fieldId,
+                                messageId = MSG_INT_TO_LOW))
                 } else if (maxValue != null && number > maxValue)
-                    validationErrors.add(ValidationError(translateMsg("validation.error.range.integerToHigh", maxValue), fieldId = fieldId))
+                    validationErrors.add(ValidationError(translateMsg(MSG_INT_TO_HIGH, maxValue),
+                            fieldId = fieldId,
+                            messageId = MSG_INT_TO_HIGH))
             } catch (ex: NumberFormatException) {
-                validationErrors.add(ValidationError(translate("validation.error.format.integer"), fieldId = fieldId))
+                validationErrors.add(ValidationError(translate(MSG_INT_FORMAT_ERROR),
+                        fieldId = fieldId,
+                        messageId = MSG_INT_FORMAT_ERROR))
             }
         }
     }
--- projectforge-jax-rs/src/main/kotlin/org/projectforge/ui/ValidationError.kt
+++ projectforge-jax-rs/src/main/kotlin/org/projectforge/ui/ValidationError.kt
@@ -1,9 +1,20 @@
 package org.projectforge.ui
 
 import com.google.gson.annotations.SerializedName
+import org.projectforge.framework.i18n.translate
 
 data class ValidationError(var message: String? = null,
                            @SerializedName("field-id")
                            var fieldId: String? = null,
                            @SerializedName("message-id")
-                           var messageId: String? = null)
+                           var messageId: String? = null) {
+    companion object {
+        fun create(i18nKey : String, fieldId : String? = null) : ValidationError {
+            val validationError = ValidationError()
+            validationError.fieldId = fieldId
+            validationError.messageId = i18nKey
+            validationError.message = translate(i18nKey)
+            return validationError
+        }
+    }
+}
