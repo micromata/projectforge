c34a0127d3c577e3e3dc74cefe6c9ac4d63075ed
Kai Reinhard <K.Reinhard@micromata.de>
2019-03-31 18:19:37 +0200

    UIButtonStyle -> UIStyle.

--- projectforge-jax-rs/src/main/kotlin/org/projectforge/ui/UIStyle.kt
+++ projectforge-jax-rs/src/main/kotlin/org/projectforge/ui/UIStyle.kt
@@ -2,11 +2,17 @@ package org.projectforge.ui
 
 import com.google.gson.annotations.SerializedName
 
-enum class UIStyle {
+enum class UIButtonStyle {
     @SerializedName("danger")
     DANGER,
     @SerializedName("info")
     INFO,
+    @SerializedName("link")
+    LINK,
+    @SerializedName("primary")
+    PRIMARY,
+    @SerializedName("secondary")
+    SECONDARY,
     @SerializedName("success")
     SUCCESS,
     @SerializedName("warning")
