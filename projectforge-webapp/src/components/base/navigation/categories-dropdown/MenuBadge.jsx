65bee31eb78fd8b70db9544b1a4f4a2f5e15b8c5
Fin Reinhard <fin.reinhard@icloud.com>
2019-04-02 12:09:30 +0200

    add badge tooltip

--- projectforge-webapp/src/components/base/navigation/categories-dropdown/MenuBadge.jsx
+++ projectforge-webapp/src/components/base/navigation/categories-dropdown/MenuBadge.jsx
@@ -2,13 +2,36 @@ import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { colorPropType } from '../../../../utilities/propTypes';
+import revisedRandomId from '../../../../utilities/revisedRandomId';
+import { UncontrolledTooltip } from '../../../design';
 import style from '../Navigation.module.scss';
 
-function MenuBadge({ children, flying, color }) {
+function MenuBadge(
+    {
+        children,
+        color,
+        flying,
+        tooltip,
+    },
+) {
+    const id = `menu-badge-${revisedRandomId()}`;
+
     return (
-        <span className={classNames(style.badge, { [style.flying]: flying }, style[color])}>
-            {children}
-        </span>
+        <React.Fragment>
+            <span
+                className={classNames(style.badge, { [style.flying]: flying }, style[color])}
+                id={id}
+            >
+                {children}
+            </span>
+            {tooltip
+                ? (
+                    <UncontrolledTooltip target={id} placement="right">
+                        {tooltip}
+                    </UncontrolledTooltip>
+                )
+                : undefined}
+        </React.Fragment>
     );
 }
 
@@ -19,11 +42,13 @@ MenuBadge.propTypes = {
     ]).isRequired,
     color: colorPropType,
     flying: PropTypes.bool,
+    tooltip: PropTypes.string,
 };
 
 MenuBadge.defaultProps = {
     color: undefined,
     flying: false,
+    tooltip: undefined,
 };
 
 export default MenuBadge;
