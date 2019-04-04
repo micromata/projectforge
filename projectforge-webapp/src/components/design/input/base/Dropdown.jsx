02662f8a8309f3c3ee41a0bf33325985bdb3fb82
Fin Reinhard <fin.reinhard@icloud.com>
2019-04-04 22:59:12 +0200

    add dropdown to new input field

---
+++ projectforge-webapp/src/components/design/input/base/Dropdown.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import style from './Base.module.scss';
+
+function BaseDropdown({ children }) {
+    return (
+        <div className={style.dropdown}>
+            {children}
+        </div>
+    );
+}
+
+BaseDropdown.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
+export default BaseDropdown;
