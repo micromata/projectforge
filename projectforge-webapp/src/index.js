8cfba5eb12c7018cc64273c9d3a86785dbc78908
Kai Reinhard <K.Reinhard@micromata.de>
2019-04-06 13:11:20 +0200

    Big Calendar added.

--- projectforge-webapp/src/index.js
+++ projectforge-webapp/src/index.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-big-calendar/lib/css/react-big-calendar.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
