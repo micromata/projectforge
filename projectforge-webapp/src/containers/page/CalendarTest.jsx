b3e4f775cca732e742d6a1c5323d38a4ee4a5d62
Kai Reinhard <K.Reinhard@micromata.de>
2019-04-06 13:38:20 +0200

    BigCalendar...

---
+++ projectforge-webapp/src/containers/page/CalendarTest.jsx
@@ -0,0 +1,18 @@
+import React, { Component }  from 'react';
+import Calendar from 'react-big-calendar';
+import moment from 'moment';
+import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
+import 'react-big-calendar/lib/addons/dragAndDrop/styles.css';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+//Calendar.setLocalizer(Calendar.momentLocalizer(moment));
+
+//const DnDCalendar = withDragAndDrop(Calendar);
+
+function CalendarTestPage() {
+    return (
+        "let's rock!"
+    );
+}
+
+export default CalendarTestPage;
