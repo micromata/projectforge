d5a938b930bd89696fe3243b796fd540605f8687
Kai Reinhard <K.Reinhard@micromata.de>
2019-04-06 13:14:03 +0200

    react-big-calendar.css removed (should be included in calendar component only).

--- projectforge-webapp/src/index.js
+++ projectforge-webapp/src/index.js
@@ -1,5 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-big-calendar/lib/css/react-big-calendar.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
