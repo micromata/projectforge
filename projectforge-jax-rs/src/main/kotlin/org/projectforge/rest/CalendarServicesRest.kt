60d97ac84d77bdccda8c51a240e2ea7ec9ae9dd1
Kai Reinhard <K.Reinhard@micromata.de>
2019-04-08 07:50:27 +0200

    WIP: CalendarServicesRest

--- projectforge-jax-rs/src/main/kotlin/org/projectforge/rest/CalendarServicesRest.kt
+++ projectforge-jax-rs/src/main/kotlin/org/projectforge/rest/CalendarServicesRest.kt
@@ -1,6 +1,7 @@
 package org.projectforge.rest
 
 import org.projectforge.business.teamcal.filter.CalendarFilter
+import org.projectforge.business.teamcal.filter.ViewType
 import org.projectforge.business.timesheet.OrderDirection
 import org.projectforge.business.timesheet.TimesheetDao
 import org.projectforge.business.timesheet.TimesheetFilter
@@ -8,10 +9,8 @@ import org.projectforge.framework.persistence.user.api.ThreadLocalUserContext
 import org.projectforge.framework.time.DateHelper
 import org.projectforge.rest.core.RestHelper
 import org.projectforge.rest.core.RestUserPreferencesService
-import org.projectforge.web.rest.converter.PFUserDOConverter
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
-import java.time.LocalDate
 import java.util.*
 import javax.servlet.http.HttpServletRequest
 import javax.ws.rs.GET
@@ -29,6 +28,7 @@ import javax.ws.rs.core.Response
 @Path("calendar")
 class CalendarServicesRest() {
 
+    internal class CalendarData(val date : Date, val viewType : ViewType, val event : List<BigCalendarEvent>)
     internal class BigCalendarEvent(val id: Int, val title: String, val start: Date, val end: Date, val allDay: Boolean = false, val desc: String? = null)
 
     private val log = org.slf4j.LoggerFactory.getLogger(CalendarServicesRest::class.java)
@@ -44,6 +44,41 @@ class CalendarServicesRest() {
     private lateinit var restUserPreferencesService: RestUserPreferencesService
 
     private val restHelper = RestHelper()
+
+    @GET
+    @Path("initial")
+    @Produces(MediaType.APPLICATION_JSON)
+    fun getInitialCalendar(@Context request: HttpServletRequest): Response {
+///        val userId = requestedUserId ?: ThreadLocalUserContext.getUserId()
+        var filter: CalendarFilter? = restUserPreferencesService.getEntry(request, USERPREF_KEY) as CalendarFilter
+        if (filter == null) {
+            filter = CalendarFilter()
+            restUserPreferencesService.putEntry(request, USERPREF_KEY, filter, true)
+        }
+        val events = mutableListOf<BigCalendarEvent>()
+        if (filter.isShowTimesheets) {
+            val tsFilter = TimesheetFilter()
+            when (filter.viewType) {
+                ViewType.MONTH -> {
+                    tsFilter.startTime = filter.startDate.withDayOfMonth(1).toDate()
+                    tsFilter.stopTime = filter.startDate.dayOfMonth().withMaximumValue().toDate()
+                }
+                ViewType.BASIC_WEEK -> {
+                    tsFilter.startTime = filter.startDate.withDayOfWeek(1).toDate()
+                    tsFilter.stopTime = filter.startDate.dayOfWeek().withMaximumValue().toDate()
+                }
+                ViewType.BASIC_DAY -> {
+                    tsFilter.startTime = filter.startDate.toDate()
+                    tsFilter.stopTime = filter.startDate.plusDays(1).toDate()
+                }
+            }
+            val timesheets = timesheetDao.getList(tsFilter)
+            timesheets.forEach {
+                events.add(BigCalendarEvent(it.id, it.description, it.startTime, it.stopTime))
+            }
+        }
+        return restHelper.buildResponse(events)
+    }
 
     @GET
     @Path("eventList")
@@ -63,10 +98,10 @@ class CalendarServicesRest() {
         //tsFilter.stopTime = end.toDate()
         tsFilter.orderType = OrderDirection.ASC
         val timesheets = timesheetDao.getList(tsFilter)
-        val events = mutableListOf<BigCalendarEvent>()
+        val events = mutableListOf<CalendarServicesRest.BigCalendarEvent>()
 
         timesheets.forEach {
-            events.add(BigCalendarEvent(it.id, it.description, it.startTime, it.stopTime))
+            events.add(CalendarServicesRest.BigCalendarEvent(it.id, it.description, it.startTime, it.stopTime))
         }
 
         //timesheetDao.get
