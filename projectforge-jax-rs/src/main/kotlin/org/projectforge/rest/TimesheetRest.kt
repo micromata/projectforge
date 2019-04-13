c96c18fff848b9578e5846f3e4715e4e83b2e819
Kai Reinhard <K.Reinhard@micromata.de>
2019-04-13 16:29:48 +0200

    WIP: React migration.

--- projectforge-jax-rs/src/main/kotlin/org/projectforge/rest/TimesheetRest.kt
+++ projectforge-jax-rs/src/main/kotlin/org/projectforge/rest/TimesheetRest.kt
@@ -3,7 +3,11 @@ package org.projectforge.rest
 import org.projectforge.business.timesheet.TimesheetDO
 import org.projectforge.business.timesheet.TimesheetDao
 import org.projectforge.business.timesheet.TimesheetFilter
+import org.projectforge.common.DateFormatType
+import org.projectforge.framework.time.DateFormats
+import org.projectforge.framework.time.DateTimeFormatter
 import org.projectforge.rest.core.AbstractStandardRest
+import org.projectforge.rest.core.ResultSet
 import org.projectforge.ui.*
 import org.springframework.stereotype.Component
 import javax.ws.rs.Path
@@ -11,6 +15,16 @@ import javax.ws.rs.Path
 @Component
 @Path("timesheet")
 class TimesheetRest() : AbstractStandardRest<TimesheetDO, TimesheetDao, TimesheetFilter>(TimesheetDao::class.java, TimesheetFilter::class.java, "timesheet.title") {
+
+    private val dateTimeFormatter = DateTimeFormatter.instance();
+
+    /**
+     * For exporting list of addresses.
+     */
+    private class Timesheet(val timesheet: TimesheetDO,
+                            val weekOfYearShortLabel: String,
+                            val dayOfWeekLabel: String)
+
     /**
      * Initializes new timesheets for adding.
      */
@@ -22,19 +36,35 @@ class TimesheetRest() : AbstractStandardRest<TimesheetDO, TimesheetDao, Timeshee
     override fun validate(validationErrors: MutableList<ValidationError>, obj: TimesheetDO) {
     }
 
+
+    override fun processResultSetBeforeExport(resultSet: ResultSet<Any>) {
+        val list: List<TimesheetRest.Timesheet> = resultSet.resultSet.map { it ->
+            TimesheetRest.Timesheet(it as TimesheetDO,
+                    weekOfYearShortLabel = DateTimeFormatter.formatWeekOfYear(it.startTime),
+                    dayOfWeekLabel = dateTimeFormatter.getFormattedDate(it.startTime,
+                            DateFormats.getFormatString(DateFormatType.DAY_OF_WEEK_SHORT)))
+        }
+        resultSet.resultSet = list
+    }
+
     /**
      * LAYOUT List page
      */
     override fun createListLayout(): UILayout {
+        lc.idPrefix = "timesheet."
         val layout = super.createListLayout()
-                .add(UITable.UIResultSetTable() // Todo customer, project, KW, WT, Range, Duration
-                        .add(lc, "user", "task")
-                        .add(UITableColumn("kost2.project.customer", "fibu.kunde", formatter = Formatter.CUSTOMER))
-                        .add(UITableColumn("kost2.project", "fibu.projekt", formatter = Formatter.PROJECT))
-                        .add(UITableColumn("kost2", "fibu.kost2", formatter = Formatter.COST2))
-                        .add(lc,  "location", "description"))
-        layout.getTableColumnById("user").formatter = Formatter.USER
-        layout.getTableColumnById("task").formatter = Formatter.TASK_PATH
+                .add(UITable.UIResultSetTable() // Todo KW, WT, Range, Duration
+                        .add(lc, "user")
+                        .add(UITableColumn("timesheet.kost2.project.customer", "fibu.kunde", formatter = Formatter.CUSTOMER))
+                        .add(UITableColumn("timesheet.kost2.project", "fibu.projekt", formatter = Formatter.PROJECT))
+                        .add(lc, "task")
+                        .add(UITableColumn("timesheet.kost2", "fibu.kost2", formatter = Formatter.COST2))
+                        .add(UITableColumn("weekOfYearShortLabel", "calendar.weekOfYearShortLabel"))
+                        .add(UITableColumn("dayName", "calendar.dayOfWeekShortLabel", dataType = UIDataType.CUSTOMIZED))
+                        .add(UITableColumn("timePeriod", "timePeriod", dataType = UIDataType.CUSTOMIZED))
+                        .add(lc, "location", "description"))
+        layout.getTableColumnById("timesheet.user").formatter = Formatter.USER
+        layout.getTableColumnById("timesheet.task").formatter = Formatter.TASK_PATH
         LayoutUtils.addListFilterContainer(layout, "longFormat", "recursive",
                 filterClass = TimesheetFilter::class.java)
         return LayoutUtils.processListPage(layout)
