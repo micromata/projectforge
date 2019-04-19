f6c5282e62c05cc5a7c6573691c856ba972efe14
Kai Reinhard <k.reinhard@micromata.de>
2019-04-19 15:01:18 +0200

    ResponseAction introduced.

---
+++ projectforge-jax-rs/src/main/kotlin/org/projectforge/ui/ResponseAction.kt
@@ -0,0 +1,22 @@
+package org.projectforge.ui
+
+/**
+ * Given as response of a rest call to inform the client on how to proceed.
+ */
+class ResponseAction(val url: String?,
+                     val targetType: TargetType? = TargetType.REDIRECT)
+
+enum class TargetType {
+    /**
+     * The client should redirect to the given url. If no type is given, REDIRECT is used as default.
+     */
+    REDIRECT,
+    /**
+     * The client will receive a download file after calling the rest service with the given url.
+     */
+    DOWNLOAD,
+    /**
+     * The client calls the rest service with the given url and will receive a response.
+     */
+    RESTCALL
+}
