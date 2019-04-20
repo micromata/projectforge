4308a76150aa3c64d73eb12b6143b9afd9d2de6f
Kai Reinhard <k.reinhard@micromata.de>
2019-04-20 17:35:58 +0200

    TaskTreePanel...

--- projectforge-webapp/src/containers/page/TaskTreePage.jsx
+++ projectforge-webapp/src/containers/page/TaskTreePage.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import TaskTreePanel from '../panel/TaskTreePanel';
+import history from '../../utilities/history';
 
+class TaskTreePage extends React.Component {
+    static onTaskSelect(id) {
+        history.push(`/task/edit/${id}`);
+    }
 
-function TaskTreePage() {
-    return <TaskTreePanel />;
+    render() {
+        return <TaskTreePanel onTaskSelect={TaskTreePage.onTaskSelect} />;
+    }
 }
 
 export default (TaskTreePage);
