fb7893a7777156c38650c2048aa4e8c6e9743eec
Kai Reinhard <k.reinhard@micromata.de>
2019-04-22 09:42:35 +0200

    Serializer: much more Kotlin style.

--- projectforge-jax-rs/src/main/kotlin/org/projectforge/rest/json/Serializers.kt
+++ projectforge-jax-rs/src/main/kotlin/org/projectforge/rest/json/Serializers.kt
@@ -1,6 +1,8 @@
 package org.projectforge.rest.json
 
-import com.google.gson.*
+import com.google.gson.JsonElement
+import com.google.gson.JsonSerializationContext
+import com.google.gson.JsonSerializer
 import org.projectforge.business.fibu.KundeDao
 import org.projectforge.business.fibu.ProjektDao
 import org.projectforge.business.fibu.kost.Kost2DO
@@ -16,15 +18,13 @@ import java.lang.reflect.Type
  * Serialization for PFUserDO etc.
  */
 class PFUserDOSerializer : JsonSerializer<PFUserDO> {
+    private class User(val id: Int, val username: String?, val fullname: String?)
+
     @Synchronized
     override fun serialize(obj: PFUserDO?, type: Type, jsonSerializationContext: JsonSerializationContext): JsonElement? {
         if (obj == null) return null
-        val result = JsonObject()
-        result.add("id", JsonPrimitive(obj.id))
-        result.add("username", JsonPrimitive(obj.username))
-        result.add("fullname", JsonPrimitive(obj.fullname))
-        result.add("email", JsonPrimitive(obj.email))
-        return result
+        val user = User(obj.id, obj.username, obj.fullname)
+        return jsonSerializationContext.serialize(user)
     }
 }
 
@@ -35,9 +35,8 @@ class TaskDOSerializer : JsonSerializer<TaskDO> {
     @Synchronized
     override fun serialize(obj: TaskDO?, type: Type, jsonSerializationContext: JsonSerializationContext): JsonElement? {
         if (obj == null) return null
-        val task = TaskServicesRest.Task(obj.id, title=obj.title)
-        val result = jsonSerializationContext.serialize(task)
-        return result
+        val task = TaskServicesRest.Task(obj.id, title = obj.title)
+        return jsonSerializationContext.serialize(task)
     }
 }
 
@@ -45,56 +44,41 @@ class TaskDOSerializer : JsonSerializer<TaskDO> {
  * Serialization for Kost2DO etc.
  */
 class Kost2DOSerializer : JsonSerializer<Kost2DO> {
+    private class Kunde(val id: Int, val name: String?)
+    private class Projekt(val id: Int, val name: String?, var kunde: Kunde? = null)
+    private class Kost2(val id: Int, val description: String?, var projekt: Projekt? = null)
+
     @Synchronized
     override fun serialize(obj: Kost2DO?, type: Type, jsonSerializationContext: JsonSerializationContext): JsonElement? {
         if (obj == null) return null
-        val result = JsonObject()
-        result.add("id", JsonPrimitive(obj.id))
-        result.add("formattedNumber", JsonPrimitive(obj.formattedNumber))
-        if (obj.description != null)
-            result.add("description", JsonPrimitive(obj.description))
+        val kost2 = Kost2(obj.id, obj.description)
         if (obj.projekt != null) {
             val projektDao = Registry.instance.getEntry(ProjektDao::class.java)?.dao as ProjektDao
-            val projectDO = projektDao?.getById(obj.projektId)
-            if (projectDO != null) {
-                val project = JsonObject()
-                project.add("id", JsonPrimitive(projectDO.id))
-                project.add("name", JsonPrimitive(projectDO.name))
-                if (projectDO.kunde != null) {
-                    val customerDao = Registry.instance.getEntry(KundeDao::class.java)?.dao as KundeDao
-                    val customerDO = customerDao?.getById(projectDO.kundeId)
-                    if (customerDO != null) {
-                        val customer = JsonObject()
-                        customer.add("id", JsonPrimitive(customerDO.id))
-                        customer.add("name", JsonPrimitive(customerDO.name))
-                        project.add("customer", customer)
+            val projektDO = projektDao.getById(obj.projektId)
+            if (projektDO != null) {
+                val projekt = Projekt(projektDO.id, projektDO.name)
+                if (projektDO.kunde != null) {
+                    val kundeDao = Registry.instance.getEntry(KundeDao::class.java)?.dao as KundeDao
+                    val kundeDO = kundeDao.getById(projektDO.kundeId)
+                    if (kundeDO != null) {
+                        val kunde = Kunde(kundeDO.id, kundeDO.name)
+                        projekt.kunde = kunde
                     }
                 }
-                result.add("project", project)
+                kost2.projekt = projekt
             }
         }
-        return result
+        return jsonSerializationContext.serialize(kost2)
     }
 }
 
 class TenantDOSerializer : JsonSerializer<TenantDO> {
+    private class Tenant(val id: Int, val name: String?)
+
     @Synchronized
     override fun serialize(obj: TenantDO?, type: Type, jsonSerializationContext: JsonSerializationContext): JsonElement? {
         if (obj == null) return null
-        val result = JsonObject()
-        result.add("id", JsonPrimitive(obj.id))
-        result.add("name", JsonPrimitive(obj.name))
-        return result
-    }
-}
-
-private fun addProperty(result: JsonObject, property: String, value: Any?) {
-    if (value == null) {
-        return
-    }
-    when (value) {
-        is String -> result.add(property, JsonPrimitive(value))
-        is Int -> result.add(property, JsonPrimitive(value))
-        else -> throw IllegalArgumentException("Can't create gson primitive: '${value}'.")
+        val tenant = Tenant(obj.id, obj.name)
+        return jsonSerializationContext.serialize(tenant)
     }
 }
