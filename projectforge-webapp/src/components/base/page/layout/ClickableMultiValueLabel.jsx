76433aae98805457da0b708dd14c433e1619e8a7
Fin Reinhard <fin.reinhard@icloud.com>
2019-04-24 23:05:11 +0200

    ClickableMultiValueLabel now supports props

--- projectforge-webapp/src/components/base/page/layout/ClickableMultiValueLabel.jsx
+++ projectforge-webapp/src/components/base/page/layout/ClickableMultiValueLabel.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { components } from 'react-select';
 
+const handleClick = (onClick, props) => event => onClick(event, props);
+
 const ClickableMultiValueLabel = onClick => props => (
-    <div onClick={onClick} role="button" tabIndex={-1} onKeyPress={undefined}>
+    <div
+        onClick={handleClick(onClick, props)}
+        role="button"
+        tabIndex={-1}
+        onKeyPress={undefined}
+    >
         <components.MultiValueLabel {...props} />
     </div>
 );
