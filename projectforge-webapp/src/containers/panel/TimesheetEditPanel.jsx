ac531f35841a336903929e1b3326ef4675db5576
Kai Reinhard <k.reinhard@micromata.de>
2019-04-25 00:05:03 +0200

    TimesheetEditPanel is now a function (no class).

--- projectforge-webapp/src/containers/panel/TimesheetEditPanel.jsx
+++ projectforge-webapp/src/containers/panel/TimesheetEditPanel.jsx
@@ -3,30 +3,29 @@ import PropTypes from 'prop-types';
 
 import EditPage from '../page/edit/index';
 
-class TimesheetEditPanel extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        const { timesheetId, startDate, endDate } = this.props;
-        const match = {
-            params: {
-                category: 'timesheet',
-                id: timesheetId,
-            },
-        };
-        const search = startDate ? `start=${startDate}&end=${endDate}` : '';
-        const location = {
-            search,
-        };
-        return (
-            <EditPage
-                location={location}
-                match={match}
-            />
-        );
-    }
+function TimesheetEditPanel(
+    {
+        timesheetId,
+        startDate,
+        endDate,
+    },
+) {
+    const match = {
+        params: {
+            category: 'timesheet',
+            id: timesheetId,
+        },
+    };
+    const search = startDate ? `start=${startDate}&end=${endDate}` : '';
+    const location = {
+        search,
+    };
+    return (
+        <EditPage
+            location={location}
+            match={match}
+        />
+    );
 }
 
 TimesheetEditPanel.propTypes = {
