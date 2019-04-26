489cb96e7c4b639cac2ed57c5f3fdb0150e0d202
Kai Reinhard <k.reinhard@micromata.de>
2019-04-26 09:11:15 +0200

    Rest-API: don't fail on unknown properties for now (in the development state).

--- projectforge-jax-rs/src/main/java/org/projectforge/rest/config/ObjectMapperResolver.java
+++ projectforge-jax-rs/src/main/java/org/projectforge/rest/config/ObjectMapperResolver.java
@@ -1,19 +1,13 @@
 package org.projectforge.rest.config;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
-import org.apache.commons.lang3.StringUtils;
 
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.ext.ContextResolver;
 import javax.ws.rs.ext.Provider;
-import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Provider
@@ -28,7 +22,7 @@ public class ObjectMapperResolver implements ContextResolver<ObjectMapper> {
     module.addDeserializer(Date.class, new MyDateDeserializer());
     mapper.registerModule(module);
     // Deserialization should fail on unknown properties due to stronger validation checks of the rest api.
-    // mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
   }
 
   @Override
