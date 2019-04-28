b5fefbe71a41eb53b4ea045959b088daf35ec62e
Kai Reinhard <k.reinhard@micromata.de>
2019-04-28 17:58:10 +0200

    WIP: Magic filter: http://localhost:8080/rs/dev/magicDemoFilters

---
+++ projectforge-jax-rs/src/main/kotlin/org/projectforge/rest/DevelopmentRest.kt
@@ -0,0 +1,39 @@
+package org.projectforge.rest
+
+import org.projectforge.business.book.BookFilter
+import org.projectforge.rest.config.Rest
+import org.projectforge.rest.core.MagicFilter
+import org.projectforge.rest.core.MagicFilterEntry
+import org.projectforge.rest.dto.User
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("${Rest.URL}/dev")
+class DevelopmentRest() {
+
+    /**
+     * Lends the given book out by the logged-in user.
+     */
+    @GetMapping("magicDemoFilters")
+    fun magicFilter(): List<MagicFilter<BookFilter>> {
+        val list = mutableListOf<MagicFilter<BookFilter>>()
+
+        var filter = MagicFilter<BookFilter>(entries = mutableListOf())
+        filter.entries!!.add(MagicFilterEntry(search = "fin"))
+        filter.entries!!.add(MagicFilterEntry(search = "inhard", matchType = MagicFilterEntry.MatchType.ENDS_WITH))
+        filter.entries!!.add(MagicFilterEntry(search = "amb", matchType = MagicFilterEntry.MatchType.CONTAINS))
+        filter.entries!!.add(MagicFilterEntry(field = "modifiedByUser", value = User(2, username = "fin")))
+        filter.entries!!.add(MagicFilterEntry(field = "title", search = "java", matchType = MagicFilterEntry.MatchType.STARTS_WITH))
+        filter.entries!!.add(MagicFilterEntry(field = "yearOfPublishing", fromValue = 2010))
+        list.add(filter)
+
+        filter = MagicFilter<BookFilter>(entries = mutableListOf())
+        filter.entries!!.add(MagicFilterEntry(field = "modifiedInterval", fromValue = "2019-04-28'T'17:00:05.000Z"))
+        filter.entries!!.add(MagicFilterEntry(field = "type", values = mutableListOf("BOOK", "MAGAZINE")))
+        list.add(filter)
+
+        return list
+    }
+}
--- projectforge-jax-rs/src/main/kotlin/org/projectforge/rest/dto/User.kt
+++ projectforge-jax-rs/src/main/kotlin/org/projectforge/rest/dto/User.kt
@@ -2,12 +2,13 @@ package org.projectforge.rest.dto
 
 import org.projectforge.framework.persistence.user.entities.PFUserDO
 
-class User(var username: String? = null,
+class User(id : Int? = null,
+           var username: String? = null,
            var firstname: String? = null,
            var lastname: String? = null,
            var description: String? = null,
            var email: String? = null
-) : BaseObject<PFUserDO>() {
+) : BaseObject<PFUserDO>(id = id) {
 
     override fun copyFromMinimal(src: PFUserDO) {
         super.copyFromMinimal(src)
