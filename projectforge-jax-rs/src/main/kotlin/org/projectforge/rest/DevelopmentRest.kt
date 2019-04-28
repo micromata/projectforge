adc8f09a20070b4cbd220b1adcf41965788264f1
Kai Reinhard <k.reinhard@micromata.de>
2019-04-28 18:09:24 +0200

    WIP: Magic filter: http://localhost:8080/rs/dev/magicDemoFilters

--- projectforge-jax-rs/src/main/kotlin/org/projectforge/rest/DevelopmentRest.kt
+++ projectforge-jax-rs/src/main/kotlin/org/projectforge/rest/DevelopmentRest.kt
@@ -24,13 +24,21 @@ class DevelopmentRest() {
         filter.entries!!.add(MagicFilterEntry(search = "fin"))
         filter.entries!!.add(MagicFilterEntry(search = "inhard", matchType = MagicFilterEntry.MatchType.ENDS_WITH))
         filter.entries!!.add(MagicFilterEntry(search = "amb", matchType = MagicFilterEntry.MatchType.CONTAINS))
-        filter.entries!!.add(MagicFilterEntry(field = "modifiedByUser", value = User(2, username = "fin")))
+        filter.entries!!.add(MagicFilterEntry(field = "modifiedByUser", value = User(2)))
         filter.entries!!.add(MagicFilterEntry(field = "title", search = "java", matchType = MagicFilterEntry.MatchType.STARTS_WITH))
         filter.entries!!.add(MagicFilterEntry(field = "yearOfPublishing", fromValue = 2010))
         list.add(filter)
 
+        filter = MagicFilter<BookFilter>(BookFilter(), entries = mutableListOf())
+        filter.entries!!.add(MagicFilterEntry(search = "Mixed filter (magic and classic)"))
+        filter.searchFilter!!.isPresent = true
+        filter.searchFilter!!.searchString = "java"
+        list.add(filter)
+        
         filter = MagicFilter<BookFilter>(entries = mutableListOf())
-        filter.entries!!.add(MagicFilterEntry(field = "modifiedInterval", fromValue = "2019-04-28'T'17:00:05.000Z"))
+        filter.entries!!.add(MagicFilterEntry(field = "modifiedInterval",
+                fromValue = "2019-04-28'T'10:00:05.000Z",
+                toValue = "2019-04-28'T'17:00:05.000Z"))
         filter.entries!!.add(MagicFilterEntry(field = "type", values = mutableListOf("BOOK", "MAGAZINE")))
         list.add(filter)
 
