ce069f42d5173ca675a256b05607d3c0158c592f
Kai Reinhard <k.reinhard@micromata.de>
2019-05-02 06:25:53 +0200

    Rest url fixed.

--- projectforge-jax-rs/src/main/kotlin/org/projectforge/rest/task/TaskRest.kt
+++ projectforge-jax-rs/src/main/kotlin/org/projectforge/rest/task/TaskRest.kt
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("${Rest.URL}/taskCal")
+@RequestMapping("${Rest.URL}/task")
 class TaskRest()
     : AbstractStandardRest<TaskDO, TaskDao, TaskFilter>(TaskDao::class.java, TaskFilter::class.java, "task.title") {
 
