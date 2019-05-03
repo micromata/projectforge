e3861dde510a2aa4e4d9b574359bdbaf779bac5b
Kai Reinhard <k.reinhard@micromata.de>
2019-05-03 17:11:32 +0200

    Addressbooks...

--- projectforge-jax-rs/src/main/kotlin/org/projectforge/rest/dto/Addressbook.kt
+++ projectforge-jax-rs/src/main/kotlin/org/projectforge/rest/dto/Addressbook.kt
@@ -1,27 +1,49 @@
 package org.projectforge.rest.dto
 
 import org.projectforge.business.address.AddressbookDO
-import org.projectforge.framework.persistence.user.entities.PFUserDO
+import org.projectforge.common.StringHelper
 
 class Addressbook(var title: String? = null,
                   var owner: User? = null,
-                  var description: String? = null) : BaseObject<AddressbookDO>() {
-    override fun copyFromMinimal(src: AddressbookDO) {
-        super.copyFromMinimal(src)
-        title = src.title
-        val srcOwner = src.owner
-        if (srcOwner != null) {
-            owner = User()
-            owner?.copyFromMinimal(srcOwner)
-        }
+                  var description: String? = null,
+                  var fullAccessGroupIds: MutableList<Int>? = null,
+                  var fullAccessUserIds: MutableList<Int>? = null,
+                  var readonlyAccessGroupIds: MutableList<Int>? = null,
+                  var readonlyAccessUserIds: MutableList<Int>? = null,
+                  var minimalAccessGroupIds: MutableList<Int>? = null,
+                  var minimalAccessUserIds: MutableList<Int>? = null
+) : BaseObject<AddressbookDO>() {
+
+    // The user and group ids are stored as csv list of integers in the data base.
+    override fun copyFrom(src: AddressbookDO) {
+        super.copyFrom(src)
+        fullAccessGroupIds = toIntList(src.fullAccessGroupIds)
+        fullAccessUserIds = toIntList(src.fullAccessUserIds)
+        readonlyAccessGroupIds = toIntList(src.readonlyAccessGroupIds)
+        readonlyAccessUserIds = toIntList(src.readonlyAccessUserIds)
+        minimalAccessGroupIds = toIntList(src.minimalAccessGroupIds)
+        minimalAccessUserIds = toIntList(src.minimalAccessUserIds)
     }
 
+    // The user and group ids are stored as csv list of integers in the data base.
     override fun copyTo(dest: AddressbookDO) {
         super.copyTo(dest)
-        dest.title = title
-        if (owner != null) {
-            dest.owner = PFUserDO()
-            owner?.copyTo(dest.owner!!)
+        dest.fullAccessGroupIds = toString(fullAccessGroupIds)
+        dest.fullAccessUserIds = toString(fullAccessUserIds)
+        dest.readonlyAccessGroupIds = toString(readonlyAccessGroupIds)
+        dest.readonlyAccessUserIds = toString(readonlyAccessUserIds)
+        dest.minimalAccessGroupIds = toString(minimalAccessGroupIds)
+        dest.minimalAccessUserIds = toString(minimalAccessUserIds)
+    }
+
+    private fun toString(intList: List<Int>?): String? {
+        return intList?.joinToString { "$it" } ?: null
+    }
+
+    private fun toIntList(str: String?): MutableList<Int>? {
+        if (str.isNullOrBlank()) {
+            return null
         }
+        return StringHelper.splitToInts(str, ",", false).toMutableList()
     }
 }
---
+++ projectforge-jax-rs/src/test/kotlin/org/projectforge/rest/AddressbookTest.kt
@@ -0,0 +1,48 @@
+package org.projectforge.rest
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.projectforge.business.address.AddressbookDO
+import org.projectforge.rest.dto.Addressbook
+
+class AddressbookTest {
+
+    @Test
+    fun listToStringTest() {
+        var bookDO = AddressbookDO()
+        bookDO.fullAccessGroupIds = "1"
+        bookDO.fullAccessUserIds = "1, 2, 3"
+        bookDO.readonlyAccessGroupIds = ""
+        bookDO.readonlyAccessUserIds = "1, ,3"
+
+        val book = Addressbook()
+        book.copyFrom(bookDO)
+
+        checkIntList(book.fullAccessGroupIds, 1)
+        checkIntList(book.fullAccessUserIds, 1, 2, 3)
+        assertNull(book.readonlyAccessGroupIds)
+        checkIntList(book.readonlyAccessUserIds, 1, 3)
+        assertNull(book.minimalAccessGroupIds)
+        assertNull(book.minimalAccessUserIds)
+
+        bookDO = AddressbookDO()
+        book.copyTo(bookDO)
+        assertEquals("1", bookDO.fullAccessGroupIds)
+        assertEquals("1, 2, 3", bookDO.fullAccessUserIds)
+        assertNull( bookDO.readonlyAccessGroupIds)
+        assertEquals("1, 3", bookDO.readonlyAccessUserIds)
+        assertNull( bookDO.minimalAccessGroupIds)
+        assertNull( bookDO.minimalAccessUserIds)
+    }
+
+    private fun checkIntList(intList: List<Int>?, vararg expected: Int) {
+        assertNotNull(intList)
+        if (intList == null) {
+            return
+        }
+        assertEquals(expected.size, intList.size)
+        intList.forEachIndexed { index, element ->
+            assertEquals(expected[index], element)
+        }
+    }
+}
