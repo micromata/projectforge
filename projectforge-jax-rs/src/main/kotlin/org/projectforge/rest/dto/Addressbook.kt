1a05ea34596c5d7c0e950a60c54776780df4faef
Kai Reinhard <k.reinhard@micromata.de>
2019-05-03 18:49:05 +0200

    Addressbook finished...

--- projectforge-jax-rs/src/main/kotlin/org/projectforge/rest/dto/Addressbook.kt
+++ projectforge-jax-rs/src/main/kotlin/org/projectforge/rest/dto/Addressbook.kt
@@ -6,38 +6,41 @@ import org.projectforge.common.StringHelper
 class Addressbook(var title: String? = null,
                   var owner: User? = null,
                   var description: String? = null,
-                  var fullAccessGroupIds: MutableList<Int>? = null,
-                  var fullAccessUserIds: MutableList<Int>? = null,
-                  var readonlyAccessGroupIds: MutableList<Int>? = null,
-                  var readonlyAccessUserIds: MutableList<Int>? = null
+                  var fullAccessGroups: MutableList<Group>? = null,
+                  var fullAccessUsers: MutableList<User>? = null,
+                  var readonlyAccessGroups: MutableList<Group>? = null,
+                  var readonlyAccessUsers: MutableList<User>? = null
 ) : BaseObject<AddressbookDO>() {
 
     // The user and group ids are stored as csv list of integers in the data base.
     override fun copyFrom(src: AddressbookDO) {
         super.copyFrom(src)
-        fullAccessGroupIds = toIntList(src.fullAccessGroupIds)
-        fullAccessUserIds = toIntList(src.fullAccessUserIds)
-        readonlyAccessGroupIds = toIntList(src.readonlyAccessGroupIds)
-        readonlyAccessUserIds = toIntList(src.readonlyAccessUserIds)
+        fullAccessGroups = toGroupList(src.fullAccessGroupIds)
+        fullAccessUsers = toUserList(src.fullAccessUserIds)
+        readonlyAccessGroups = toGroupList(src.readonlyAccessGroupIds)
+        readonlyAccessUsers = toUserList(src.readonlyAccessUserIds)
     }
 
     // The user and group ids are stored as csv list of integers in the data base.
     override fun copyTo(dest: AddressbookDO) {
         super.copyTo(dest)
-        dest.fullAccessGroupIds = toString(fullAccessGroupIds)
-        dest.fullAccessUserIds = toString(fullAccessUserIds)
-        dest.readonlyAccessGroupIds = toString(readonlyAccessGroupIds)
-        dest.readonlyAccessUserIds = toString(readonlyAccessUserIds)
+        dest.fullAccessGroupIds = fullAccessGroups?.joinToString { "${it.id}" }
+        dest.fullAccessUserIds = fullAccessUsers?.joinToString { "${it.id}" }
+        dest.readonlyAccessGroupIds = readonlyAccessGroups?.joinToString { "${it.id}" }
+        dest.readonlyAccessUserIds = readonlyAccessUsers?.joinToString { "${it.id}" }
     }
 
-    private fun toString(intList: List<Int>?): String? {
-        return intList?.joinToString { "$it" } ?: null
+    private fun toUserList(str: String?): MutableList<User>? {
+        if (str.isNullOrBlank()) return null
+        val users = mutableListOf<User>()
+        StringHelper.splitToInts(str, ",", false).forEach { users.add(User(it, fullname = "Kai Reinhard")) }
+        return users
     }
 
-    private fun toIntList(str: String?): MutableList<Int>? {
-        if (str.isNullOrBlank()) {
-            return null
-        }
-        return StringHelper.splitToInts(str, ",", false).toMutableList()
+    private fun toGroupList(str: String?): MutableList<Group>? {
+        if (str.isNullOrBlank()) return null
+        val groups = mutableListOf<Group>()
+        StringHelper.splitToInts(str, ",", false).forEach { groups.add(Group(it, name = "Gruppe")) }
+        return groups
     }
 }
