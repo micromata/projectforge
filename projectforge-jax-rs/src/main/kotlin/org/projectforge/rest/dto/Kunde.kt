47c313fce4778a477c3f6f50bf9cd91c21c2f861
Kai Reinhard <k.reinhard@micromata.de>
2019-05-08 11:19:43 +0200

    Kunde: nummer fixed.

--- projectforge-jax-rs/src/main/kotlin/org/projectforge/rest/dto/Kunde.kt
+++ projectforge-jax-rs/src/main/kotlin/org/projectforge/rest/dto/Kunde.kt
@@ -3,7 +3,7 @@ package org.projectforge.rest.dto
 import org.projectforge.business.fibu.KundeDO
 import org.projectforge.business.fibu.KundeStatus
 
-class Kunde(var bereich: Int? = null,
+class Kunde(var nummer: Int? = null,
             var name: String? = null,
             var identifier: String? = null,
             var division: String? = null,
