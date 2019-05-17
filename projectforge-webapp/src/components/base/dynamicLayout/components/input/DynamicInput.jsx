20637994fce877b8c36d9c2b16cd62e5b76ea104
Fin Reinhard <fin.reinhard@icloud.com>
2019-05-17 14:37:08 +0200

    add ValidationManager

--- projectforge-webapp/src/components/base/dynamicLayout/components/input/DynamicInput.jsx
+++ projectforge-webapp/src/components/base/dynamicLayout/components/input/DynamicInput.jsx
@@ -1,13 +1,28 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import Input from '../../../../design/input';
+import ValidationManager from '../../../../design/input/ValidationManager';
+import { DynamicLayoutContext } from '../../context';
 
-function DynamicInput() {
+function DynamicInput({ id, ...props }) {
+    const { data, setData } = React.useContext(DynamicLayoutContext);
+
+    const handleInputChange = ({ target }) => setData({ [id]: target.value });
+
     return (
-        <h1>Input here</h1>
+        <ValidationManager>
+            <Input
+                id={id}
+                onChange={handleInputChange}
+                {...props}
+                value={data[id] || ''}
+            />
+        </ValidationManager>
     );
 }
 
 DynamicInput.propTypes = {
+    id: PropTypes.string.isRequired,
 };
 
 export default DynamicInput;
