a511cbc4ad1f1c12f771813e68e3a20ae9be22f7
Jan Brümmer <j.bruemmer@micromata.de>
2019-05-20 16:27:56 +0200

    AuftragDO.java -> AuftragDO.kt

---
+++ projectforge-jax-rs/src/main/kotlin/org/projectforge/rest/fibu/AuftragRest.kt
@@ -0,0 +1,36 @@
+package org.projectforge.rest.fibu
+
+import org.projectforge.business.fibu.AuftragDO
+import org.projectforge.business.fibu.AuftragDao
+import org.projectforge.framework.persistence.api.BaseSearchFilter
+import org.projectforge.rest.config.Rest
+import org.projectforge.rest.core.AbstractDORest
+import org.projectforge.ui.*
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("${Rest.URL}/auftrag")
+class AuftragRest() : AbstractDORest<AuftragDO, AuftragDao, BaseSearchFilter>(AuftragDao::class.java, BaseSearchFilter::class.java, "fibu.auftrag.title") {
+
+    /**
+     * LAYOUT List page
+     */
+    override fun createListLayout(): UILayout {
+        val layout = super.createListLayout()
+                .add(UITable.UIResultSetTable()
+                        .add(lc, "nummer", "status", "bezeichnung", "description"))
+        return LayoutUtils.processListPage(layout)
+    }
+
+    /**
+     * LAYOUT Edit page
+     */
+    override fun createEditLayout(dataObject: AuftragDO): UILayout {
+        val layout = super.createEditLayout(dataObject)
+                .add(UIRow()
+                        .add(UICol()
+                                .add(lc, "nummer", "status", "bezeichnung", "description")))
+        return LayoutUtils.processEditPage(layout, dataObject)
+    }
+}
