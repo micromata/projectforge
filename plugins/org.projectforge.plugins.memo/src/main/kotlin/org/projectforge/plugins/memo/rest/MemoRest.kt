59514630ff2076615d0439021cbc75a67437cd62
Kai Reinhard <k.reinhard@micromata.de>
2019-05-30 08:36:33 +0200

    MemoRest: set current user as owner in newBaseDO, list view: "modified" -> "lastUpdate".

--- plugins/org.projectforge.plugins.memo/src/main/kotlin/org/projectforge/plugins/memo/rest/MemoRest.kt
+++ plugins/org.projectforge.plugins.memo/src/main/kotlin/org/projectforge/plugins/memo/rest/MemoRest.kt
@@ -1,11 +1,14 @@
 package org.projectforge.plugins.memo.rest
 
 import org.projectforge.framework.persistence.api.BaseSearchFilter
+import org.projectforge.framework.persistence.user.api.ThreadLocalUserContext
 import org.projectforge.plugins.memo.MemoDO
 import org.projectforge.plugins.memo.MemoDao
 import org.projectforge.rest.config.Rest
 import org.projectforge.rest.core.AbstractDORest
-import org.projectforge.ui.*
+import org.projectforge.ui.LayoutUtils
+import org.projectforge.ui.UILayout
+import org.projectforge.ui.UITable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import javax.servlet.http.HttpServletRequest
@@ -18,6 +21,7 @@ class MemoRest() : AbstractDORest<MemoDO, MemoDao, BaseSearchFilter>(MemoDao::cl
      */
     override fun newBaseDO(request: HttpServletRequest): MemoDO {
         val memo = super.newBaseDO(request)
+        memo.owner = ThreadLocalUserContext.getUser()
         return memo
     }
 
@@ -27,7 +31,7 @@ class MemoRest() : AbstractDORest<MemoDO, MemoDao, BaseSearchFilter>(MemoDao::cl
     override fun createListLayout(): UILayout {
         val layout = super.createListLayout()
                 .add(UITable.UIResultSetTable()
-                        .add(lc, "created", "modified", "subject", "memo"))
+                        .add(lc, "created", "lastUpdate", "subject", "memo"))
         return LayoutUtils.processListPage(layout)
     }
 
