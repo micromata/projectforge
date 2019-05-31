d2aee12e571168bf012c1d8232bf7cb68d73ad92
Jan Brümmer <j.bruemmer@micromata.de>
2019-05-31 10:34:37 +0200

    PollDO.java -> PollDO.kt

---
+++ plugins/org.projectforge.plugins.poll/src/main/kotlin/org/projectforge/plugins/poll/PollDO.kt
@@ -0,0 +1,93 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2014 Kai Reinhard (k.reinhard@micromata.de)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.plugins.poll
+
+import org.hibernate.search.annotations.Indexed
+import org.hibernate.search.annotations.IndexedEmbedded
+import org.projectforge.framework.persistence.entities.DefaultBaseDO
+import org.projectforge.framework.persistence.user.entities.PFUserDO
+
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.FetchType
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+import javax.persistence.Table
+
+/**
+ * @author M. Lauterbach (m.lauterbach@micromata.de)
+ */
+@Entity
+@Indexed
+@Table(name = "T_PLUGIN_POLL", indexes = [javax.persistence.Index(name = "idx_fk_t_plugin_poll_tenant_id", columnList = "tenant_id")])
+class PollDO : DefaultBaseDO() {
+
+    @IndexedEmbedded(depth = 1)
+    @get:ManyToOne(fetch = FetchType.LAZY)
+    @get:JoinColumn(name = "owner_fk")
+    var owner: PFUserDO? = null
+
+    @get:Column
+    var title: String? = null
+
+    @get:Column
+    var location: String? = null
+
+    @get:Column
+    var description: String? = null
+
+    @get:Column
+    var active: Boolean = false
+
+    /**
+     * @see java.lang.Object.hashCode
+     */
+    override fun hashCode(): Int {
+        val prime = 31
+        var result = 1
+        result = prime * result + if (id == null) 0 else id!!.hashCode()
+        return result
+    }
+
+    /**
+     * @see java.lang.Object.equals
+     */
+    override fun equals(other: Any?): Boolean {
+        if (this === other)
+            return true
+        if (other == null)
+            return false
+        if (javaClass != other.javaClass)
+            return false
+        val other = other as PollDO?
+        if (id == null) {
+            return if (other!!.id != null)
+                false
+            else
+                super.equals(other)
+        } else if (id != other!!.id)
+            return false
+        return true
+    }
+}
