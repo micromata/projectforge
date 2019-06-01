2d5c341039ec54926c26ad20675e74fd29198cb7
Kai Reinhard <k.reinhard@micromata.de>
2019-06-01 21:01:17 +0200

    AdminRest: Class name fixed.

--- projectforge-jax-rs/src/main/kotlin/org/projectforge/rest/admin/AdminRest.kt
+++ projectforge-jax-rs/src/main/kotlin/org/projectforge/rest/admin/AdminRest.kt
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("${Rest.URL}/admin")
-class DevelopmentRest() {
+class AdminRest() {
     @Autowired
     private lateinit var accessChecker: AccessChecker
     @Autowired
