712f94b11ebae8629e3ba453ec317a72ba432046
Jan Brümmer <j.bruemmer@micromata.de>
2019-06-05 16:04:56 +0200

    LiquidityEntryDO: Add missing import

--- plugins/org.projectforge.plugins.liquidityplanning/src/main/java/org/projectforge/plugins/liquidityplanning/LiquidityEntryDao.java
+++ plugins/org.projectforge.plugins.liquidityplanning/src/main/java/org/projectforge/plugins/liquidityplanning/LiquidityEntryDao.java
@@ -28,6 +28,7 @@ import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.projectforge.plugins.liquidityplanning.LiquidityEntryDO;
 import org.projectforge.business.fibu.AmountType;
 import org.projectforge.business.fibu.PaymentStatus;
 import org.projectforge.framework.persistence.api.BaseDao;
