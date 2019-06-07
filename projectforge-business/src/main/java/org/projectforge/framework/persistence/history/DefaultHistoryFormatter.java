dd5ca38acdad2667d2172fdbe6af9de1bace7407
Kai Reinhard <k.reinhard@micromata.de>
2019-06-07 23:23:51 +0200

    CopyRight of all java file-header updated or created.

--- projectforge-business/src/main/java/org/projectforge/framework/persistence/history/DefaultHistoryFormatter.java
+++ projectforge-business/src/main/java/org/projectforge/framework/persistence/history/DefaultHistoryFormatter.java
@@ -1,16 +1,23 @@
 /////////////////////////////////////////////////////////////////////////////
 //
-// $RCSfile: DefaultHistoryFormatter.java,v $
+// Project ProjectForge Community Edition
+//         www.projectforge.org
 //
-// Project   HibernateHistory
+// Copyright (C) 2001-2019 Micromata GmbH, Germany (www.micromata.com)
 //
-// Author    Wolfgang Jung (w.jung@micromata.de)
-// Created   Sep 21, 2005
-// Copyright Micromata Sep 21, 2005
+// ProjectForge is dual-licensed.
 //
-// $Id: DefaultHistoryFormatter.java,v 1.4 2007-06-21 08:09:18 tung Exp $
-// $Revision: 1.4 $
-// $Date: 2007-06-21 08:09:18 $
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
 //
 /////////////////////////////////////////////////////////////////////////////
 
