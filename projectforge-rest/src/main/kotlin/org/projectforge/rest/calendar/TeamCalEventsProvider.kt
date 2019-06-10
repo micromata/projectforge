40ee24156293dffe8b6dc8ae751a1f3ee19801c1
Kai Reinhard <k.reinhard@micromata.de>
2019-06-10 19:53:19 +0200

    WIP: Calendar: styles and birthdays.

--- projectforge-rest/src/main/kotlin/org/projectforge/rest/calendar/TeamCalEventsProvider.kt
+++ projectforge-rest/src/main/kotlin/org/projectforge/rest/calendar/TeamCalEventsProvider.kt
@@ -45,7 +45,7 @@ class TeamCalEventsProvider() {
     fun addEvents(start: PFDateTime,
                   end: PFDateTime,
                   events: MutableList<BigCalendarEvent>,
-                  teamCalendarIds: Set<Int>?,
+                  teamCalendarIds: List<Int>?,
                   styleMap: CalendarStyleMap) {
         if (teamCalendarIds.isNullOrEmpty())
             return
