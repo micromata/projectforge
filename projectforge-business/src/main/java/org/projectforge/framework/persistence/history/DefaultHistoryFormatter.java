0e2dd2b3081ea4a0633019f88fff0f72c860cdd2
Jan Brümmer <j.bruemmer@micromata.de>
2019-06-12 14:45:27 +0200

    lang3.StringEscapeUtils -> text.StringEscapeUtils

--- projectforge-business/src/main/java/org/projectforge/framework/persistence/history/DefaultHistoryFormatter.java
+++ projectforge-business/src/main/java/org/projectforge/framework/persistence/history/DefaultHistoryFormatter.java
@@ -30,7 +30,7 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 import java.util.Set;
 
-import org.apache.commons.lang3.StringEscapeUtils;
+import org.apache.commons.text.StringEscapeUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.hibernate.Session;
 import org.slf4j.Logger;
