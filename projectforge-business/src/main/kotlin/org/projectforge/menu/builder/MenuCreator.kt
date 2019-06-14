00372579bb1c169e64f5c8cb2985dfcabd57b0a7
Jan Brümmer <j.bruemmer@micromata.de>
2019-06-14 11:00:21 +0200

    RechnungDO is now linked to Menu.

--- projectforge-business/src/main/kotlin/org/projectforge/menu/builder/MenuCreator.kt
+++ projectforge-business/src/main/kotlin/org/projectforge/menu/builder/MenuCreator.kt
@@ -233,7 +233,7 @@ open class MenuCreator {
         //
         val fibuMenu = menuItemDefHolder.add(MenuItemDef(MenuItemDefId.FIBU,
                 checkAccess = { isInGroup(*FIBU_ORGA_GROUPS) }))
-                .add(MenuItemDef(MenuItemDefId.OUTGOING_INVOICE_LIST, "wa/invoiceList",
+                .add(MenuItemDef(MenuItemDefId.OUTGOING_INVOICE_LIST, "invoice",
                         checkAccess = {
                             hasRight(RechnungDao.USER_RIGHT_ID, *READONLY_READWRITE) ||
                                     isInGroup(ProjectForgeGroup.CONTROLLING_GROUP)
