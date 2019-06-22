d87843d3820a870814e41e2163f9bbd2867debe5
Fin Reinhard <fin.reinhard@icloud.com>
2019-06-22 23:58:54 +0200

    moved footer version to SystemStatusContext

--- projectforge-webapp/src/components/base/footer/index.jsx
+++ projectforge-webapp/src/components/base/footer/index.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import React from 'react';
+import { SystemStatusContext } from '../../../containers/SystemStatusContext';
 import style from './Footer.module.scss';
 
-function Footer({ version, releaseTimestamp, updateAvailable }) {
+function Footer() {
+    const { version, releaseTimestamp, updateAvailable } = React.useContext(SystemStatusContext);
+
     return (
-        <div className={classNames(style.footer, 'footer',)}>
+        <div className={classNames(style.footer, 'footer')}>
             <ul className={classNames(style.list, style.copyHint)}>
                 <li>
                     <a
@@ -14,7 +16,7 @@ function Footer({ version, releaseTimestamp, updateAvailable }) {
                         target="_blank"
                         rel="noopener noreferrer"
                     >
-                        &copy;2001-2019{' '}
+                        &copy;2001-2019
                     </a>
                     <a
                         href="https://www.micromata.com"
@@ -65,14 +67,8 @@ function Footer({ version, releaseTimestamp, updateAvailable }) {
     );
 }
 
-Footer.propTypes = {
-    version: PropTypes.string.isRequired,
-    releaseTimestamp: PropTypes.string.isRequired,
-    updateAvailable: PropTypes.bool,
-};
+Footer.propTypes = {};
 
-Footer.defaultProps = {
-    updateAvailable: false,
-};
+Footer.defaultProps = {};
 
 export default Footer;
