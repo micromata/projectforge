51295708cea4c36bed8997e049abeacd04e9b266
Jan Brümmer <j.bruemmer@micromata.de>
2019-07-01 14:27:50 +0200

    BankAccountDO.java -> BankAccountDO.kt

---
+++ plugins/org.projectforge.plugins.banking/src/main/kotlin/org/projectforge/plugins/banking/BankAccountBalanceDO.kt
@@ -0,0 +1,76 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2019 Micromata GmbH, Germany (www.micromata.com)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.plugins.banking
+
+import de.micromata.genome.db.jpa.history.api.WithHistory
+import org.hibernate.search.annotations.Analyze
+import org.hibernate.search.annotations.DateBridge
+import org.hibernate.search.annotations.EncodingType
+import org.hibernate.search.annotations.Field
+import org.hibernate.search.annotations.Index
+import org.hibernate.search.annotations.Indexed
+import org.hibernate.search.annotations.IndexedEmbedded
+import org.hibernate.search.annotations.Resolution
+import org.hibernate.search.annotations.Store
+import org.projectforge.framework.persistence.entities.DefaultBaseDO
+
+import java.math.BigDecimal
+import java.sql.Date
+
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.FetchType
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+import javax.persistence.Table
+
+/**
+ * Die Kontostände für ein Konto zu festen Zeitpunkten soll die Integrität der Kontobewegungen sicherstellen. So kann
+ * bei abweichenden Kontoständen geprüft werden, in welcher Periode abweichende Kontodaten importiert wurden.
+ *
+ * @author Kai Reinhard (k.reinhard@micromata.de)
+ */
+@Entity
+@Indexed
+@Table(name = "T_PLUGIN_BANK_ACCOUNT_BALANCE", indexes = [javax.persistence.Index(name = "idx_fk_t_plugin_bank_account_balance_tenant_id", columnList = "tenant_id")])
+@WithHistory
+class BankAccountBalanceDO : DefaultBaseDO() {
+
+    @IndexedEmbedded(depth = 1)
+    @get:ManyToOne(fetch = FetchType.LAZY)
+    @get:JoinColumn(name = "account_fk", nullable = false)
+    var account: BankAccountDO? = null
+
+    @Field(analyze = Analyze.NO)
+    @DateBridge(resolution = Resolution.DAY, encoding = EncodingType.STRING)
+    @get:Column(name = "date_col", nullable = false)
+    var date: Date? = null
+
+    @get:Column(nullable = false, scale = 5, precision = 18)
+    var amount: BigDecimal? = null
+
+    @Field
+    @get:Column(length = 4000)
+    var description: String? = null
+}
---
+++ plugins/org.projectforge.plugins.banking/src/main/kotlin/org/projectforge/plugins/banking/BankAccountDO.kt
@@ -0,0 +1,70 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2019 Micromata GmbH, Germany (www.micromata.com)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.plugins.banking
+
+import de.micromata.genome.db.jpa.history.api.WithHistory
+import org.hibernate.search.annotations.Field
+import org.hibernate.search.annotations.Index
+import org.hibernate.search.annotations.Indexed
+import org.hibernate.search.annotations.Store
+import org.projectforge.framework.persistence.api.ShortDisplayNameCapable
+import org.projectforge.framework.persistence.entities.DefaultBaseDO
+
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Table
+import javax.persistence.Transient
+import javax.persistence.UniqueConstraint
+
+@Entity
+@Indexed
+@Table(name = "T_PLUGIN_BANK_ACCOUNT", uniqueConstraints = [UniqueConstraint(columnNames = ["account_number", "tenant_id"])], indexes = [javax.persistence.Index(name = "idx_fk_t_plugin_bank_account_tenant_id", columnList = "tenant_id")])
+@WithHistory
+class BankAccountDO : DefaultBaseDO(), ShortDisplayNameCapable {
+
+    @Field
+    @get:Column(name = "account_number", length = 255, nullable = false)
+    var accountNumber: String? = null
+
+    @Field
+    @get:Column(length = 255)
+    var bank: String? = null
+
+    @Field
+    @get:Column(name = "bank_identification_code", length = 100)
+    var bankIdentificationCode: String? = null
+
+    @Field
+    @get:Column(length = 255)
+    var name: String? = null
+
+    @Field
+    @get:Column(length = 4000)
+    var description: String? = null
+
+    @Transient
+    override fun getShortDisplayName(): String {
+        return accountNumber.toString()
+    }
+}
