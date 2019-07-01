693093843f5d36eafe7b65d95001f4f25d559b40
Kai Reinhard <k.reinhard@micromata.de>
2019-07-01 23:45:39 +0200

    Flyway migration tested for hsqldb.

--- projectforge-business/src/main/resources/flyway/migrate/hsqldb/V7.0.0__7.0.0-RELEASE.sql
+++ projectforge-business/src/main/resources/flyway/migrate/hsqldb/V7.0.0__7.0.0-RELEASE.sql
@@ -1,5 +1,5 @@
 ALTER TABLE t_user_pref ADD COLUMN value_string VARCHAR(100000);
 ALTER TABLE t_user_pref ADD COLUMN value_type VARCHAR(1000);
-ALTER TABLE t_user_pref ALTER COLUMN area MODIFY varchar(255);
+ALTER TABLE t_user_pref ALTER COLUMN area varchar(255);
 
 ALTER TABLE t_book DROP COLUMN task_id;
