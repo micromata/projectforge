62f7e6894d8858b9bce4473a69754624cd349b01
Kai Reinhard <k.reinhard@micromata.de>
2019-07-02 08:08:23 +0200

    Flyway PostgreSQL: add columns only if not exists and drop columns only if exists.

--- projectforge-business/src/main/resources/flyway/migrate/postgresql/V7.0.0__7.0.0-RELEASE.sql
+++ projectforge-business/src/main/resources/flyway/migrate/postgresql/V7.0.0__7.0.0-RELEASE.sql
@@ -1,5 +1,5 @@
-ALTER TABLE t_user_pref ADD COLUMN value_string VARCHAR(100000);
-ALTER TABLE t_user_pref ADD COLUMN value_type VARCHAR(1000);
+ALTER TABLE t_user_pref ADD COLUMN IF NOT EXISTS value_string VARCHAR(100000);
+ALTER TABLE t_user_pref ADD COLUMN IF NOT EXISTS value_type VARCHAR(1000);
 ALTER TABLE t_user_pref ALTER COLUMN area TYPE varchar(255);
 
-ALTER TABLE t_book DROP COLUMN task_id;
+ALTER TABLE t_book DROP COLUMN IF EXISTS task_id;
