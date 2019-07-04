859d348316db553b4fecccd6e7ae42aa89fe6f25
Kai Reinhard <k.reinhard@micromata.de>
2019-07-04 06:52:15 +0200

    Bug fix in initial test data script for HSQLDB based systems: Postgres specific sql statements separated from initial test script. BookDO.taskId removed, too.

---
+++ projectforge-business/src/main/resources/data/pfTestdataPostgres.sql
@@ -0,0 +1,7 @@
+SELECT pg_catalog.setval('hibernate_sequence', 617, TRUE);
+
+SELECT pg_catalog.setval('sq_base_ghistory_attr_data_pk', 1, FALSE);
+
+SELECT pg_catalog.setval('sq_base_ghistory_attr_pk', 1, FALSE);
+
+SELECT pg_catalog.setval('sq_base_ghistory_pk', 1, FALSE);
