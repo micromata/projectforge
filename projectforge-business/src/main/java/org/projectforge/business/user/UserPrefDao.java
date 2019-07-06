650ce67c780e4841259bc863c2cd44dcc87b238d
Kai Reinhard <k.reinhard@micromata.de>
2019-07-06 23:00:27 +0200

    UserPrefDao: unused imports.

--- projectforge-business/src/main/java/org/projectforge/business/user/UserPrefDao.java
+++ projectforge-business/src/main/java/org/projectforge/business/user/UserPrefDao.java
@@ -62,9 +62,6 @@ import org.projectforge.framework.time.PFDateTime;
 import org.projectforge.framework.utils.NumberHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Isolation;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
 import java.io.Serializable;
