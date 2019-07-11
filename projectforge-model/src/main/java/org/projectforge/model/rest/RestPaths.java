49074180b2fa23d7737b0e9825b25343f6bc799f
Kai Reinhard <k.reinhard@micromata.de>
2019-07-11 21:34:21 +0200

    ResponseAction improved/fixed.

--- projectforge-model/src/main/java/org/projectforge/model/rest/RestPaths.java
+++ projectforge-model/src/main/java/org/projectforge/model/rest/RestPaths.java
@@ -81,6 +81,8 @@ public class RestPaths
 
   public static final String CANCEL = "cancel";
 
+  public static final String EDIT = "edit";
+
   public static final String SAVE = "save";
 
   public static final String UPDATE = "update";
