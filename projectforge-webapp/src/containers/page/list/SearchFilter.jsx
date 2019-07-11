2bcc94e0fa3e523e004888206938a03a50e9edb2
Fin Reinhard <fin.reinhard@icloud.com>
2019-07-11 14:03:25 +0200

    fix SearchFilter favorites handler

--- projectforge-webapp/src/containers/page/list/SearchFilter.jsx
+++ projectforge-webapp/src/containers/page/list/SearchFilter.jsx
@@ -22,10 +22,18 @@ function SearchFilter() {
         filter,
         filterFavorites,
         filterHelper,
+        setFilterFavorites,
         setUI,
     } = React.useContext(ListPageContext);
 
-    const saveUpdateResponse = ({ data: responseData, ui: responseUI, filter: responseFilter }) => {
+    const saveUpdateResponse = (
+        {
+            data: responseData,
+            ui: responseUI,
+            filter: responseFilter,
+            filterFavorites: responseFilterFavorites,
+        },
+    ) => {
         if (responseData) {
             setData(responseData);
         }
@@ -34,6 +42,9 @@ function SearchFilter() {
         }
         if (responseFilter) {
             filterHelper.setFilter(responseFilter);
+        }
+        if (responseFilterFavorites) {
+            setFilterFavorites(responseFilterFavorites);
         }
     };
 
