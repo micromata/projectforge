d19c700075ad25577388fba083d1a708772dbf4a
Kai Reinhard <k.reinhard@micromata.de>
2019-07-11 23:27:57 +0200

    React: Favorites.htmlId

--- projectforge-webapp/src/containers/page/list/SearchFilter.jsx
+++ projectforge-webapp/src/containers/page/list/SearchFilter.jsx
@@ -194,6 +194,7 @@ function SearchFilter() {
                                 isModified
                                 closeOnSelect={false}
                                 translations={ui.translations}
+                                htmlId="searchFilterFavoritesPopover"
                             />
                         </Col>
                     </Row>
