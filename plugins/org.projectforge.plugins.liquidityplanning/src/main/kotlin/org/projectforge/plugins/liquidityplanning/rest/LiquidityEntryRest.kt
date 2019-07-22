70713e7b72cf367a585cea6440ecaaf3213005af
Jan Brümmer <j.bruemmer@micromata.de>
2019-07-22 15:25:20 +0200

    Add LiquidityEntryRest page

---
+++ plugins/org.projectforge.plugins.liquidityplanning/src/main/kotlin/org/projectforge/plugins/liquidityplanning/rest/LiquidityEntryRest.kt
@@ -0,0 +1,61 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2019 Micromata GmbH, Germany (www.micromata.com)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.plugins.liquidityplanning.rest
+
+import org.projectforge.business.fibu.kost.BuchungssatzDO
+import org.projectforge.business.fibu.kost.BuchungssatzDao
+import org.projectforge.business.scripting.ScriptDO
+import org.projectforge.business.scripting.ScriptDao
+import org.projectforge.plugins.liquidityplanning.LiquidityEntryDO
+import org.projectforge.plugins.liquidityplanning.LiquidityEntryDao
+import org.projectforge.rest.config.Rest
+import org.projectforge.rest.core.AbstractDORest
+import org.projectforge.ui.*
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("${Rest.URL}/liquidity")
+class LiquidityEntryRest: AbstractDORest<LiquidityEntryDO, LiquidityEntryDao>(baseDaoClazz = LiquidityEntryDao::class.java, i18nKeyPrefix = "plugins.liquidityplanning.entry.title") {
+
+    /**
+     * LAYOUT List page
+     */
+    override fun createListLayout(): UILayout {
+        val layout = super.createListLayout()
+                .add(UITable.UIResultSetTable()
+                        .add(lc, "dateOfPayment", "amount", "paid", "subject", "comment"))
+        layout.getTableColumnById("dateOfPayment").formatter = Formatter.DATE
+        return LayoutUtils.processListPage(layout, this)
+    }
+
+    /**
+     * LAYOUT Edit page
+     */
+    override fun createEditLayout(dto: LiquidityEntryDO): UILayout {
+        val layout = super.createEditLayout(dto)
+                .add(UILabel("TODO"))
+        return LayoutUtils.processEditPage(layout, dto, this)
+    }
+}
