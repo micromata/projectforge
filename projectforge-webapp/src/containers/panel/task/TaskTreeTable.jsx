e23896b66b1f4e949f093abb0622dbd5b1fa5f27
Fin Reinhard <fin.reinhard@icloud.com>
2019-08-01 15:23:51 +0200

    wip: new task tree table

---
+++ projectforge-webapp/src/containers/panel/task/TaskTreeTable.jsx
@@ -0,0 +1,93 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Table } from '../../../components/design';
+
+function TaskTreeTable(
+    {
+        columnsVisibility,
+        nodes,
+        onSelect,
+        shortForm,
+        translations,
+    },
+) {
+    return (
+        <Table striped hover responsive>
+            <thead>
+                <tr>
+                    <th>{translations.task}</th>
+                    <th>{translations['task.consumption']}</th>
+
+                    {columnsVisibility.kost2 ? <th>{translations['fibu.kost2']}</th> : undefined}
+
+                    {!shortForm && columnsVisibility.orders
+                        ? <th>{translations['fibu.auftrag.auftraege']}</th>
+                        : undefined}
+
+                    <th>{translations.shortDescription}</th>
+
+                    {!shortForm ? (
+                        <React.Fragment>
+
+                            {columnsVisibility.protectionUntil
+                                ? <th>{translations['task.protectTimesheetsUntil.short']}</th>
+                                : undefined}
+
+                            {columnsVisibility.reference
+                                ? <th>{translations['task.reference']}</th>
+                                : undefined}
+
+                            {columnsVisibility.priority
+                                ? <th>{translations.priority}</th>
+                                : undefined}
+
+                            <th>{translations.status}</th>
+
+                            {columnsVisibility.assignedUser
+                                ? <th>{translations['task.assignedUser']}</th>
+                                : undefined}
+
+                        </React.Fragment>
+                    ) : undefined}
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </Table>
+    );
+}
+
+TaskTreeTable.propTypes = {
+    nodes: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+    onSelect: PropTypes.func.isRequired,
+    columnsVisibility: PropTypes.shape({
+        assignedUser: PropTypes.bool,
+        kost2: PropTypes.bool,
+        orders: PropTypes.bool,
+        priority: PropTypes.bool,
+        protectionUntil: PropTypes.bool,
+        reference: PropTypes.bool,
+    }),
+    shortForm: PropTypes.bool,
+    translations: PropTypes.shape({
+        'fibu.auftrag.auftraege': PropTypes.string,
+        'fibu.kost2': PropTypes.string,
+        priority: PropTypes.string,
+        shortDescription: PropTypes.string,
+        status: PropTypes.string,
+        task: PropTypes.string,
+        'task.assignedUser': PropTypes.string,
+        'task.consumption': PropTypes.string,
+        'task.protectTimesheetsUntil.short': PropTypes.string,
+        'task.referece': PropTypes.string,
+    }),
+};
+
+TaskTreeTable.defaultProps = {
+    columnsVisibility: {},
+    shortForm: false,
+    translations: {},
+};
+
+export default TaskTreeTable;
