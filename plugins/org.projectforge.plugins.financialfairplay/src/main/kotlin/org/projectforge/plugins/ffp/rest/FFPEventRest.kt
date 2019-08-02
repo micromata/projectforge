eda637782b5cba0b98def6c950a5dd4703673a42
Jan Brümmer <j.bruemmer@micromata.de>
2019-08-02 14:31:34 +0200

    Add FFPEventRest page

---
+++ plugins/org.projectforge.plugins.financialfairplay/src/main/kotlin/org/projectforge/plugins/ffp/rest/FFPEventRest.kt
@@ -0,0 +1,60 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2019 Micromata GmbH, Germany (www.micromata.com)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.plugins.ffp.rest
+
+import org.projectforge.plugins.ffp.model.FFPEventDO
+import org.projectforge.plugins.ffp.repository.FFPEventDao
+import org.projectforge.rest.config.Rest
+import org.projectforge.rest.core.AbstractDORest
+import org.projectforge.ui.LayoutUtils
+import org.projectforge.ui.UILabel
+import org.projectforge.ui.UILayout
+import org.projectforge.ui.UITable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("${Rest.URL}/ffpEvent")
+class FFPEventRest : AbstractDORest<FFPEventDO, FFPEventDao>(FFPEventDao::class.java, "plugins.ffp.title") {
+
+    /**
+     * LAYOUT List page
+     */
+    override fun createListLayout(): UILayout {
+        val layout = super.createListLayout()
+                .add(UITable.UIResultSetTable()
+                        .add(lc, "eventDate", "title", "status"))
+        return LayoutUtils.processListPage(layout, this)
+    }
+
+    /**
+     * LAYOUT Edit page
+     */
+    override fun createEditLayout(dto: FFPEventDO): UILayout {
+        val layout = super.createEditLayout(dto)
+                .add(lc, "organizer", "eventDate", "title", "attendeeList")
+                .add(UILabel("TODO: Weighting Table here"))
+        return LayoutUtils.processEditPage(layout, dto, this)
+    }
+}
