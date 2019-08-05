d8bc35e5d5aff3f52d8cb0e286debb2850f1387c
Fin Reinhard <fin.reinhard@icloud.com>
2019-08-05 19:24:08 +0200

    fix memo in TimesheetTemplatesAndRecents

--- projectforge-webapp/src/components/base/dynamicLayout/components/customized/components/TimesheetTemplatesAndRecents.jsx
+++ projectforge-webapp/src/components/base/dynamicLayout/components/customized/components/TimesheetTemplatesAndRecents.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { DynamicLayoutContext } from '../../../context';
 import FavoritesPanel from '../../../../../../containers/panel/favorite/FavoritesPanel';
 import { fetchJsonGet, fetchJsonPost } from '../../../../../../utilities/rest';
+import { DynamicLayoutContext } from '../../../context';
 
 function TimesheetTemplatesAndRecents() {
     const { ui, variables, data } = React.useContext(DynamicLayoutContext);
@@ -10,60 +10,62 @@ function TimesheetTemplatesAndRecents() {
         setTimesheetFavorites,
     ] = React.useState(variables.timesheetFavorites);
 
-    const handleFavoriteCreate = (newFilterName) => fetchJsonPost('timesheet/favorites/create',
-        {
-            name: newFilterName,
-            timesheet: data,
-        },
-        setTimesheetFavorites);
-
-    // @Fin: Die zurückgebene Liste der Favoriten wird nicht in setTimesheetFavorites aktualisiert. Der gelöschte
-    // Eintrag bleibt in der Liste. Erst wenn ich die Seite neu lade, stimmt die Liste wieder.
-    const handleFavoriteDelete = id => fetchJsonGet('timesheet/favorites/delete',
-        { id },
-        setTimesheetFavorites);
-
-    // @Fin: Hier soll das Edit-Page-Model geändert werden.
-    const saveUpdateResponseInState = (json) => {
-        console.log(json);
-        const newState = {
-            ...json,
-        };
-        this.setState(newState);
-    };
-
-    const handleFavoriteSelect = id => fetchJsonPost('timesheet/favorites/select',
-        {
-            id,
-            timesheet: data,
-        },
-        saveUpdateResponseInState);
-
-    const handleFavoriteRename = (favoriteId, newName) => fetchJsonGet('timesheet/favorites/rename',
-        {
-            id: favoriteId,
-            newName,
-        },
-        setTimesheetFavorites);
-
     return React.useMemo(
-        () => (
-            <React.Fragment>
-                <FavoritesPanel
-                    onFavoriteCreate={handleFavoriteCreate}
-                    onFavoriteDelete={handleFavoriteDelete}
-                    onFavoriteRename={handleFavoriteRename}
-                    onFavoriteSelect={handleFavoriteSelect}
-                    translations={ui.translations}
-                    favorites={timesheetFavorites}
-                    closeOnSelect={false}
-                    htmlId="timesheetFavoritesPopover"
-                    favoriteButtonText={`${ui.translations.templates} | `}
-                />
-                Recents
-            </React.Fragment>
-        ),
-        [variables.task.consumption], // @Fin: Was muss hierhin? consumption ist noch von Copy&Paste.
+        () => {
+            const handleFavoriteCreate = newFilterName => fetchJsonPost(
+                'timesheet/favorites/create',
+                {
+                    name: newFilterName,
+                    timesheet: data,
+                },
+                setTimesheetFavorites,
+            );
+
+            // @Fin: Die zurückgebene Liste der Favoriten wird nicht in setTimesheetFavorites aktualisiert. Der gelöschte
+            // Eintrag bleibt in der Liste. Erst wenn ich die Seite neu lade, stimmt die Liste wieder.
+            const handleFavoriteDelete = id => fetchJsonGet('timesheet/favorites/delete',
+                { id },
+                setTimesheetFavorites);
+
+            // @Fin: Hier soll das Edit-Page-Model geändert werden.
+            const saveUpdateResponseInState = (json) => {
+                console.log(json);
+            };
+
+            const handleFavoriteSelect = id => fetchJsonPost('timesheet/favorites/select',
+                {
+                    id,
+                    timesheet: data,
+                },
+                saveUpdateResponseInState);
+
+            const handleFavoriteRename = (favoriteId, newName) => fetchJsonGet(
+                'timesheet/favorites/rename',
+                {
+                    id: favoriteId,
+                    newName,
+                },
+                setTimesheetFavorites,
+            );
+
+            return (
+                <React.Fragment>
+                    <FavoritesPanel
+                        onFavoriteCreate={handleFavoriteCreate}
+                        onFavoriteDelete={handleFavoriteDelete}
+                        onFavoriteRename={handleFavoriteRename}
+                        onFavoriteSelect={handleFavoriteSelect}
+                        translations={ui.translations}
+                        favorites={timesheetFavorites}
+                        closeOnSelect={false}
+                        htmlId="timesheetFavoritesPopover"
+                        favoriteButtonText={`${ui.translations.templates} | `}
+                    />
+                    Recents
+                </React.Fragment>
+            );
+        },
+        [timesheetFavorites, ui.translations, data, setTimesheetFavorites],
     );
 }
 
