f5aa8e5054ab7594c4880f8f9aa16ba4aa1b1ba4
Fin Reinhard <fin.reinhard@icloud.com>
2019-08-28 11:27:58 +0200

    implement task debounced search !!! yarn/npm install required!!!

--- projectforge-webapp/src/containers/panel/task/TaskTreePanel.jsx
+++ projectforge-webapp/src/containers/panel/task/TaskTreePanel.jsx
@@ -1,3 +1,4 @@
+import AwesomeDebouncePromise from 'awesome-debounce-promise';
 import PropTypes from 'prop-types';
 import React from 'react';
 import LoadingContainer from '../../../components/design/loading-container';
@@ -5,6 +6,73 @@ import { getServiceURL, handleHTTPErrors } from '../../../utilities/rest';
 import TaskTreeTable from './table/TaskTreeTable';
 import TaskFilter from './TaskFilter';
 import TaskTreeContext, { taskTreeContextDefaultValues } from './TaskTreeContext';
+
+const loadTasksBounced = (
+    {
+        close,
+        filter,
+        highlightTaskId,
+        initial,
+        loading,
+        open,
+        setColumnsVisibility,
+        setFilter,
+        setLoading,
+        setNodes,
+        setTranslations,
+        showRootForAdmins,
+    },
+) => {
+    setLoading(loading || initial);
+
+    fetch(
+        getServiceURL(
+            'task/tree',
+            {
+                table: true,
+                initial: initial || '',
+                open: open || '',
+                highlightedTaskId: highlightTaskId || '',
+                close: close || '',
+                ...filter,
+                showRootForAdmins,
+            },
+        ),
+        {
+            method: 'GET',
+            credentials: 'include',
+            headers: { Accept: 'application/json' },
+        },
+    )
+        .then(handleHTTPErrors)
+        .then(response => response.json())
+        .then((
+            {
+                root,
+                translations: responseTranslations,
+                initFilter,
+                columnsVisibility: responseColumnsVisibility,
+            },
+        ) => {
+            if (responseTranslations) {
+                setTranslations(responseTranslations);
+            }
+
+            // TODO: RENAME CHILDS TO CHILDREN
+            setNodes(root.childs);
+            setColumnsVisibility(responseColumnsVisibility);
+
+            // TODO: SCROLL TO HIGHLIGHTED TASK
+
+            if (initial && initFilter) {
+                setFilter(initFilter);
+            }
+
+            setLoading(false);
+        })
+        // TODO: ERROR HANDLING
+        .catch(() => setLoading(false));
+};
 
 function TaskTreePanel(
     {
@@ -26,83 +94,78 @@ function TaskTreePanel(
     const [translations, setTranslations] = React.useState({});
     const [nodes, setNodes] = React.useState([]);
     const [columnsVisibility, setColumnsVisibility] = React.useState({});
-    const scrollToRef = React.useRef(undefined);
-
-    const loadTasks = (initial, open, close) => {
-        setLoading(loading || initial);
-
-        fetch(
-            getServiceURL(
-                'task/tree',
-                {
-                    table: true,
-                    initial: initial || '',
-                    open: open || '',
-                    highlightedTaskId: highlightTaskId || '',
-                    close: close || '',
-                    ...filter,
-                    showRootForAdmins,
-                },
-            ),
-            {
-                method: 'GET',
-                credentials: 'include',
-                headers: { Accept: 'application/json' },
-            },
-        )
-            .then(handleHTTPErrors)
-            .then(response => response.json())
-            .then((
-                {
-                    root,
-                    translations: responseTranslations,
-                    initFilter,
-                    columnsVisibility: responseColumnsVisibility,
-                },
-            ) => {
-                if (responseTranslations) {
-                    setTranslations(responseTranslations);
-                }
-
-                // TODO: RENAME CHILDS TO CHILDREN
-                setNodes(root.childs);
-                setColumnsVisibility(responseColumnsVisibility);
-
-                if (initial && scrollToRef.current) {
-                    // Scroll only once on initial call to highlighted row:
-                    window.scrollTo(0, scrollToRef.current.offsetTop);
-                }
-
-                if (initial && initFilter) {
-                    setFilter(initFilter);
-                }
-
-                setLoading(false);
-            })
-            // TODO: ERROR HANDLING
-            .catch(() => setLoading(false));
-    };
-
-    const toggleTask = (taskId, from) => loadTasks(
-        false,
-        from === 'CLOSED' ? taskId : undefined,
-        from === 'OPENED' ? taskId : undefined,
+    const [loadTasksDebounced] = React.useState(
+        () => AwesomeDebouncePromise(loadTasksBounced, 500),
     );
 
-    const handleCheckBoxChange = ({ target }) => setFilter({
-        ...filter,
-        [target.id]: target.checked,
-    });
+    const loadTasks = (
+        {
+            initial,
+            open,
+            close,
+            filter: newFilter,
+        } = {},
+    ) => {
+        let loadFunction = loadTasksDebounced;
 
-    const handleSearchChange = ({ target }) => setFilter({
-        ...filter,
-        searchString: target.value,
-    });
+        if (initial) {
+            loadFunction = loadTasksBounced;
+        }
+
+        loadFunction({
+            close,
+            filter: newFilter || filter,
+            highlightTaskId,
+            initial,
+            loading,
+            open,
+            setColumnsVisibility,
+            setFilter,
+            setLoading,
+            setNodes,
+            setTranslations,
+            showRootForAdmins,
+        });
+    };
+
+    const toggleTask = (taskId, from) => {
+        const options = {};
+
+        switch (from) {
+            case 'CLOSED':
+                options.open = taskId;
+                break;
+            case 'OPENED':
+                options.close = taskId;
+                break;
+            default:
+        }
+
+        loadTasks(options);
+    };
+
+    const handleCheckBoxChange = ({ target }) => {
+        const newFilter = {
+            ...filter,
+            [target.id]: target.checked,
+        };
+        setFilter(newFilter);
+        loadTasks({ filter: newFilter });
+    };
+
+    const handleSearchChange = ({ target }) => {
+        const newFilter = {
+            ...filter,
+            searchString: target.value,
+        };
+        setFilter(newFilter);
+        loadTasks({ filter: newFilter });
+    };
 
     // Reload Tasks when highlight task id changes
     React.useEffect(() => {
         if (visible && !loading) {
-            loadTasks(true);
+            loadTasks({ initial: true });
         }
     }, [highlightTaskId]);
 
@@ -110,7 +173,7 @@ function TaskTreePanel(
     // are missing.
     React.useEffect(() => {
         if (!loading && visible && Object.isEmpty(translations)) {
-            loadTasks(true);
+            loadTasks({ initial: true });
         }
     }, [visible, loading, translations]);
 
