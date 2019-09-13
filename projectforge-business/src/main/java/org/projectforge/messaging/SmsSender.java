185f56a2eb26d14f98a6dcff6cccadb9c8ea31b8
Kai Reinhard <k.reinhard@micromata.de>
2019-09-13 12:16:05 +0200

    SMS*: why does Jenkins fail???

--- projectforge-business/src/main/java/org/projectforge/messaging/SmsSender.java
+++ projectforge-business/src/main/java/org/projectforge/messaging/SmsSender.java
@@ -111,6 +111,7 @@ public class SmsSender {
         responseCode = HttpResponseCode.UNKNOWN_ERROR;
       }
       if (responseCode != HttpResponseCode.SUCCESS) {
+        System.err.println("SmsSender: phoneNumber=" + phoneNumber + ", message=" + message);
         log.error("Unexpected response from sms gateway: " + responseNumber + ": " + response + " (if this call was successful, did you configured projectforge.sms.returnCodePattern.success?).");
       }
       return responseCode;
