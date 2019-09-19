e72cab961ee9cfe29ff8008161846ab6230f07a0
Kai Reinhard <k.reinhard@micromata.de>
2019-09-19 15:33:38 +0200

    CalendarFilterFavoritesTest: translations are available on Jenkins. Fixed.

--- projectforge-business/src/test/kotlin/org/projectforge/business/calendar/CalendarFilterFavoritesTest.kt
+++ projectforge-business/src/test/kotlin/org/projectforge/business/calendar/CalendarFilterFavoritesTest.kt
@@ -31,6 +31,7 @@ import org.junit.jupiter.api.fail
 import org.projectforge.business.user.UserPrefDao
 import org.projectforge.favorites.Favorites
 import org.projectforge.framework.configuration.ConfigXml
+import org.projectforge.framework.i18n.translate
 import org.projectforge.framework.persistence.user.api.ThreadLocalUserContext
 import org.projectforge.framework.persistence.user.api.UserContext
 import org.projectforge.framework.persistence.user.entities.PFUserDO
@@ -60,8 +61,7 @@ class CalendarFilterFavoritesTest {
         val favs = Favorites<CalendarFilter>()
         favs.add(CalendarFilter())
         val prefix = favs.getElementAt(0)!!.name ?: fail("prefix can't be null")
-        assertTrue(prefix.startsWith("???"), "Prefix should start with ???, but it doesn't: '$prefix'") // Translations not available
-        assertTrue(prefix.endsWith("???"), "Prefix should end with ???, but it doesn't: '$prefix'") // Translations not available
+        assertEquals(translate("favorite.untitled"), prefix)
         favs.add(CalendarFilter())
         assertEquals("$prefix 1", favs.getElementAt(1)!!.name)
         favs.add(CalendarFilter(name = "My favorite"))
