49943d70daaad88251caba01165e5158a6c9e57f
Jan Brümmer <j.bruemmer@micromata.de>
2019-10-09 13:26:02 +0200

    QueryFilter: Change third parameter of method createAlias from int to JoinType

--- projectforge-business/src/main/java/org/projectforge/framework/persistence/api/QueryFilter.java
+++ projectforge-business/src/main/java/org/projectforge/framework/persistence/api/QueryFilter.java
@@ -29,6 +29,7 @@ import org.hibernate.Session;
 import org.hibernate.criterion.Criterion;
 import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
+import org.hibernate.sql.JoinType;
 import org.projectforge.business.multitenancy.TenantService;
 import org.projectforge.framework.configuration.ApplicationContextProvider;
 import org.projectforge.framework.persistence.user.api.ThreadLocalUserContext;
@@ -177,8 +178,8 @@ public class QueryFilter {
     if (year > 0) {
       final Calendar cal = DateHelper.getUTCCalendar();
       cal.set(Calendar.YEAR, year);
-      java.sql.Date lo = null;
-      java.sql.Date hi = null;
+      java.sql.Date lo;
+      java.sql.Date hi;
       if (month >= 0) {
         cal.set(Calendar.MONTH, month);
         cal.set(Calendar.DAY_OF_MONTH, 1);
@@ -239,26 +240,24 @@ public class QueryFilter {
   }
 
   /**
-   * @see org.hibernate.Criteria#createAlias(String, String, int)
+   * @see org.hibernate.Criteria#createAlias(String, String, JoinType)
    */
-  public QueryFilter createAlias(final String arg0, final String arg1, final int joinType) {
+  public QueryFilter createAlias(final String arg0, final String arg1, final JoinType joinType) {
     filterSettings.add(new Alias(arg0, arg1, joinType));
     return this;
   }
 
   class Alias {
     String arg0;
-
     String arg1;
-
-    int joinType = Criteria.INNER_JOIN;
+    JoinType joinType = JoinType.INNER_JOIN;
 
     Alias(final String arg0, final String arg1) {
       this.arg0 = arg0;
       this.arg1 = arg1;
     }
 
-    Alias(final String arg0, final String arg1, final int joinType) {
+    Alias(final String arg0, final String arg1, final JoinType joinType) {
       this.arg0 = arg0;
       this.arg1 = arg1;
       this.joinType = joinType;
