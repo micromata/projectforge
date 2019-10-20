3f99edb96d0bd66195961aa7298017c56a3081a2
Kai Reinhard <k.reinhard@micromata.de>
2019-10-20 10:48:54 +0200

    VacationFormValidatorTest: Powermock issues with java 9 (ugly stuff...) They should do there homework to run smoothly with 11.

--- projectforge-wicket/src/test/java/org/projectforge/web/vacation/VacationFormValidatorTest.java
+++ projectforge-wicket/src/test/java/org/projectforge/web/vacation/VacationFormValidatorTest.java
@@ -6,6 +6,7 @@ import org.apache.wicket.markup.html.form.Form;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.projectforge.business.configuration.ConfigurationService;
@@ -36,6 +37,7 @@ import static org.powermock.api.mockito.PowerMockito.when;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({ DatePanel.class, Form.class })
+@PowerMockIgnore({ "javax.activation.*", "javax.management.*", "javax.xml.*", "org.xml.*", "org.w3c.dom.*", "com.sun.org.apache.xerces.*", "ch.qos.logback.*", "org.slf4j.*" })
 public class VacationFormValidatorTest
 {
   private EmployeeDO employee;
