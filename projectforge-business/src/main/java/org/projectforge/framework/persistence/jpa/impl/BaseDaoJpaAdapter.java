87d0b0a1af51ea7701c49ef928d762df15a7d298
Kai Reinhard <k.reinhard@micromata.de>
2019-11-11 01:22:38 +0100

    BaseDaoJpaAdapter.getModificationStatus(ModificationStatus, BaseDO) must handle HibernateProxies (may work now).

--- projectforge-business/src/main/java/org/projectforge/framework/persistence/jpa/impl/BaseDaoJpaAdapter.java
+++ projectforge-business/src/main/java/org/projectforge/framework/persistence/jpa/impl/BaseDaoJpaAdapter.java
@@ -43,6 +43,7 @@ import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.ClassUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.Validate;
+import org.hibernate.proxy.HibernateProxy;
 import org.projectforge.framework.configuration.ApplicationContextProvider;
 import org.projectforge.framework.persistence.api.*;
 import org.projectforge.framework.persistence.entities.AbstractHistorizableBaseDO;
@@ -63,16 +64,13 @@ import java.util.stream.Collectors;
 
 /**
  * Utilities to create compat with BaseDao
- * 
- * @author Roger Rene Kommer (r.kommer.extern@micromata.de)
  *
+ * @author Roger Rene Kommer (r.kommer.extern@micromata.de)
  */
-public class BaseDaoJpaAdapter
-{
+public class BaseDaoJpaAdapter {
   private static final Logger log = LoggerFactory.getLogger(BaseDaoJpaAdapter.class);
 
-  public static void prepareInsert(ExtendedBaseDO<?> dbObj)
-  {
+  public static void prepareInsert(ExtendedBaseDO<?> dbObj) {
     PfEmgrFactory emf = ApplicationContextProvider.getApplicationContext().getBean(PfEmgrFactory.class);
     emf.runInTrans((emgr) -> {
       EmgrInitForInsertEvent nev = new EmgrInitForInsertEvent(emgr, dbObj);
@@ -82,23 +80,19 @@ public class BaseDaoJpaAdapter
 
   }
 
-  public static void inserted(ExtendedBaseDO<?> dbObj)
-  {
+  public static void inserted(ExtendedBaseDO<?> dbObj) {
     HistoryBaseDaoAdapter.inserted(dbObj);
   }
 
-  public static void beforeUpdateCopyMarkDelete(ExtendedBaseDO<?> dbObj, ExtendedBaseDO<?> obj)
-  {
+  public static void beforeUpdateCopyMarkDelete(ExtendedBaseDO<?> dbObj, ExtendedBaseDO<?> obj) {
     HistoryBaseDaoAdapter.markedAsDeleted(dbObj, obj);
   }
 
-  public static void beforeUpdateCopyMarkUnDelete(ExtendedBaseDO<?> dbObj, ExtendedBaseDO<?> obj)
-  {
+  public static void beforeUpdateCopyMarkUnDelete(ExtendedBaseDO<?> dbObj, ExtendedBaseDO<?> obj) {
     HistoryBaseDaoAdapter.markedAsUnDeleted(dbObj, obj);
   }
 
-  public static void prepareUpdate(ExtendedBaseDO<?> dbObj)
-  {
+  public static void prepareUpdate(ExtendedBaseDO<?> dbObj) {
     PfEmgrFactory emf = ApplicationContextProvider.getApplicationContext().getBean(PfEmgrFactory.class);
     emf.runInTrans((emgr) -> {
       EmgrInitForUpdateEvent nev = new EmgrInitForUpdateEvent(emgr, dbObj);
@@ -108,20 +102,18 @@ public class BaseDaoJpaAdapter
     });
   }
 
-  public static void updated(ExtendedBaseDO<?> dbObj, ExtendedBaseDO<?> obj)
-  {
+  public static void updated(ExtendedBaseDO<?> dbObj, ExtendedBaseDO<?> obj) {
 
   }
 
   @SuppressWarnings("unchecked")
-  public static ModificationStatus copyValues(final BaseDO src, final BaseDO dest, final String... ignoreFields)
-  {
+  public static ModificationStatus copyValues(final BaseDO src, final BaseDO dest, final String... ignoreFields) {
     if (!ClassUtils.isAssignable(src.getClass(), dest.getClass())) {
       throw new RuntimeException("Try to copyValues from different BaseDO classes: this from type "
-          + dest.getClass().getName()
-          + " and src from type"
-          + src.getClass().getName()
-          + "!");
+              + dest.getClass().getName()
+              + " and src from type"
+              + src.getClass().getName()
+              + "!");
     }
     if (src.getId() != null && (ignoreFields == null || !ArrayUtils.contains(ignoreFields, "id"))) {
       dest.setId(src.getId());
@@ -131,8 +123,7 @@ public class BaseDaoJpaAdapter
 
   @SuppressWarnings("rawtypes")
   public static ModificationStatus copyValues(JpaTabAttrBaseDO destEntry, JpaTabAttrBaseDO sourceEntry,
-      String... ignoreFields)
-  {
+                                              String... ignoreFields) {
     ModificationStatus modificationStatus = ModificationStatus.NONE;
     String tsd = destEntry.getStringData();
     JpaTabAttrBaseDO<?, ?> src = sourceEntry;
@@ -148,27 +139,26 @@ public class BaseDaoJpaAdapter
 
   /**
    * Merges timed attributes.
-   * 
+   *
    * @param target
    * @param source
    * @return
    */
   public static <PK extends Serializable, T extends TimeableAttrRow<PK>> ModificationStatus copyTimeableAttribute(
-      final EntityWithTimeableAttr<PK, T> target,
-      final EntityWithTimeableAttr<PK, T> source)
-  {
+          final EntityWithTimeableAttr<PK, T> target,
+          final EntityWithTimeableAttr<PK, T> source) {
     ModificationStatus mod = ModificationStatus.NONE;
 
     Set<Serializable> sourcePks = source
-        .getTimeableAttributes()
-        .stream()
-        .map(T::getPk)
-        .collect(Collectors.toSet());
+            .getTimeableAttributes()
+            .stream()
+            .map(T::getPk)
+            .collect(Collectors.toSet());
 
     Map<Serializable, T> targetRows = target
-        .getTimeableAttributes()
-        .stream()
-        .collect(Collectors.toMap(T::getPk, o -> o));
+            .getTimeableAttributes()
+            .stream()
+            .collect(Collectors.toMap(T::getPk, o -> o));
 
     // copy rows from source to target
     for (T sourceRow : source.getTimeableAttributes()) {
@@ -183,7 +173,7 @@ public class BaseDaoJpaAdapter
     }
 
     // remove deleted rows from target, these are rows which are not in source but in target
-    for (Iterator<T> lis = target.getTimeableAttributes().iterator(); lis.hasNext();) {
+    for (Iterator<T> lis = target.getTimeableAttributes().iterator(); lis.hasNext(); ) {
       T tagetrow = lis.next();
       if (!sourcePks.contains(tagetrow.getPk())) {
         mod = mod.combine(ModificationStatus.MAJOR);
@@ -197,27 +187,24 @@ public class BaseDaoJpaAdapter
   @SuppressWarnings("unchecked")
   public static ModificationStatus copyDeclaredSimpleFields(Class<?> srcClazz, Object dest, Object src,
 
-      String... ignoreFields)
-  {
+                                                            String... ignoreFields) {
 
     ModificationStatus mod = ModificationStatus.NONE;
     Matcher<Field> matcher = CommonMatchers.and(
-        FieldMatchers.hasNotModifier(Modifier.STATIC),
-        FieldMatchers.hasNotModifier(Modifier.TRANSIENT),
-        new MatcherBase<Field>()
-        {
-          @Override
-          public boolean match(Field object)
-          {
-            return !ArrayUtils.contains(ignoreFields, object.getName());
-          }
-        },
-        CommonMatchers.not(
-            CommonMatchers.or(
-                FieldMatchers.assignableTo(Collection.class),
-                FieldMatchers.assignableTo(Map.class),
-                FieldMatchers.assignableTo(DbRecord.class),
-                FieldMatchers.assignableTo(IdObject.class))));
+            FieldMatchers.hasNotModifier(Modifier.STATIC),
+            FieldMatchers.hasNotModifier(Modifier.TRANSIENT),
+            new MatcherBase<Field>() {
+              @Override
+              public boolean match(Field object) {
+                return !ArrayUtils.contains(ignoreFields, object.getName());
+              }
+            },
+            CommonMatchers.not(
+                    CommonMatchers.or(
+                            FieldMatchers.assignableTo(Collection.class),
+                            FieldMatchers.assignableTo(Map.class),
+                            FieldMatchers.assignableTo(DbRecord.class),
+                            FieldMatchers.assignableTo(IdObject.class))));
     List<Field> foundFields = PrivateBeanUtils.findAllFields(srcClazz, matcher);
 
     for (Field field : foundFields) {
@@ -231,8 +218,7 @@ public class BaseDaoJpaAdapter
     return mod;
   }
 
-  public static ModificationStatus copyTabAttributes(EntityWithAttributes target, EntityWithAttributes source)
-  {
+  public static ModificationStatus copyTabAttributes(EntityWithAttributes target, EntityWithAttributes source) {
     ModificationStatus mod = ModificationStatus.NONE;
 
     //    for (String key : source.getAttributeKeys()) {
@@ -284,9 +270,8 @@ public class BaseDaoJpaAdapter
   }
 
   public static ModificationStatus copyDeclaredFields(final Class<?> srcClazz, final BaseDO<?> src,
-      final BaseDO<?> dest,
-      final String... ignoreFields)
-  {
+                                                      final BaseDO<?> dest,
+                                                      final String... ignoreFields) {
     final Field[] fields = srcClazz.getDeclaredFields();
     AccessibleObject.setAccessible(fields, true);
     ModificationStatus modificationStatus = ModificationStatus.NONE;
@@ -373,7 +358,7 @@ public class BaseDaoJpaAdapter
           final Serializable srcFieldValueId = HibernateUtils.getIdentifier((BaseDO<?>) srcFieldValue);
           if (srcFieldValueId != null) {
             if (destFieldValue == null
-                || !Objects.equals(srcFieldValueId, ((BaseDO<?>) destFieldValue).getId())) {
+                    || !Objects.equals(srcFieldValueId, ((BaseDO<?>) destFieldValue).getId())) {
               field.set(dest, srcFieldValue);
               modificationStatus = getModificationStatus(modificationStatus, src, fieldName);
             }
@@ -419,14 +404,13 @@ public class BaseDaoJpaAdapter
   }
 
   protected static ModificationStatus getModificationStatus(final ModificationStatus currentStatus, final BaseDO<?> src,
-      final String modifiedField)
-  {
+                                                            final String modifiedField) {
     HistoryService historyService = HistoryServiceManager.get().getHistoryService();
     PfEmgrFactory emf = ApplicationContextProvider.getApplicationContext().getBean(PfEmgrFactory.class);
     if (currentStatus == ModificationStatus.MAJOR
-        || !(src instanceof AbstractHistorizableBaseDO)
-        || !historyService.getNoHistoryProperties(emf, src.getClass())
-        .contains(modifiedField)) {
+            || !(src instanceof AbstractHistorizableBaseDO)
+            || (!(src instanceof HibernateProxy)
+            && !historyService.getNoHistoryProperties(emf, src.getClass()).contains(modifiedField))) {
       return ModificationStatus.MAJOR;
     }
     return ModificationStatus.MINOR;
@@ -439,12 +423,11 @@ public class BaseDaoJpaAdapter
    * <li>Ignore static fields
    * <li>Ignore inner class fields</li>
    * </ul>
-   * 
+   *
    * @param field The Field to test.
    * @return Whether or not to consider the given <code>Field</code>.
    */
-  protected static boolean accept(final Field field)
-  {
+  protected static boolean accept(final Field field) {
     if (field.getName().indexOf(ClassUtils.INNER_CLASS_SEPARATOR_CHAR) != -1) {
       // Reject field from inner class.
       return false;
@@ -464,8 +447,7 @@ public class BaseDaoJpaAdapter
   }
 
   public static ModificationStatus getModificationStatus(final ModificationStatus currentStatus,
-      final ModificationStatus status)
-  {
+                                                         final ModificationStatus status) {
     return currentStatus.combine(status);
   }
 }
