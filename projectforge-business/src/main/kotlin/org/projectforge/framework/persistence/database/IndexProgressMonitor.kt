3cc9e807351ad73064ea7fdae4dbd143f28db0f7
Kai Reinhard <k.reinhard@micromata.de>
2019-11-17 22:41:34 +0100

    IndexProgressMonitor: Source code file header fixed.

--- projectforge-business/src/main/kotlin/org/projectforge/framework/persistence/database/IndexProgressMonitor.kt
+++ projectforge-business/src/main/kotlin/org/projectforge/framework/persistence/database/IndexProgressMonitor.kt
@@ -20,6 +20,7 @@
 // with this program; if not, see http://www.gnu.org/licenses/.
 //
 /////////////////////////////////////////////////////////////////////////////
+
 package org.projectforge.framework.persistence.database
 
 import org.hibernate.search.batchindexing.impl.SimpleIndexingProgressMonitor
