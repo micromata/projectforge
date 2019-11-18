be3bdb64cf61283cc143ff86c70301e5fb99dc37
Kai Reinhard <k.reinhard@micromata.de>
2019-11-18 22:43:23 +0100

    SystemStatisticsPage: Run gc and display memory usages...

--- projectforge-wicket/src/main/java/org/projectforge/web/statistics/SystemStatisticsPage.java
+++ projectforge-wicket/src/main/java/org/projectforge/web/statistics/SystemStatisticsPage.java
@@ -23,11 +23,9 @@
 
 package org.projectforge.web.statistics;
 
-import java.math.BigDecimal;
-
-import javax.sql.DataSource;
-
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.repeater.RepeatingView;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.projectforge.business.task.TaskDO;
@@ -43,8 +41,14 @@ import org.projectforge.framework.utils.NumberHelper;
 import org.projectforge.web.wicket.AbstractSecuredPage;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-public class SystemStatisticsPage extends AbstractSecuredPage
-{
+import javax.sql.DataSource;
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryPoolMXBean;
+import java.lang.management.MemoryType;
+import java.lang.management.MemoryUsage;
+import java.math.BigDecimal;
+
+public class SystemStatisticsPage extends AbstractSecuredPage {
   private static final long serialVersionUID = 8587252641914110851L;
 
   private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(SystemStatisticsPage.class);
@@ -52,28 +56,46 @@ public class SystemStatisticsPage extends AbstractSecuredPage
   @SpringBean
   private DataSource dataSource;
 
-  public SystemStatisticsPage(final PageParameters parameters)
-  {
+  public SystemStatisticsPage(final PageParameters parameters) {
     super(parameters);
     final JdbcTemplate jdbc = new JdbcTemplate(dataSource);
     body.add(new Label("totalNumberOfTimesheets", NumberFormatter.format(getTableCount(jdbc, TimesheetDO.class))));
     final TaskTree taskTree = TaskTreeHelper.getTaskTree();
     final long totalDuration = taskTree.getRootTaskNode().getDuration(taskTree, true);
     BigDecimal tatalPersonDays = new BigDecimal(totalDuration).divide(DateHelper.SECONDS_PER_WORKING_DAY, 2,
-        BigDecimal.ROUND_HALF_UP);
+            BigDecimal.ROUND_HALF_UP);
     tatalPersonDays = NumberHelper.setDefaultScale(tatalPersonDays);
     body.add(new Label("totalNumberOfTimesheetDurations",
-        NumberHelper.getNumberFractionFormat(getLocale(), tatalPersonDays.scale())
-            .format(tatalPersonDays)));
+            NumberHelper.getNumberFractionFormat(getLocale(), tatalPersonDays.scale())
+                    .format(tatalPersonDays)));
     body.add(new Label("totalNumberOfUsers", NumberFormatter.format(getTableCount(jdbc, PFUserDO.class))));
     body.add(new Label("totalNumberOfTasks", NumberFormatter.format(getTableCount(jdbc, TaskDO.class))));
     final int totalNumberOfHistoryEntries = getTableCount(jdbc, PfHistoryMasterDO.class)
-        + getTableCount(jdbc, PfHistoryMasterDO.class);
+            + getTableCount(jdbc, PfHistoryMasterDO.class);
     body.add(new Label("totalNumberOfHistoryEntries", NumberFormatter.format(totalNumberOfHistoryEntries)));
+
+    RepeatingView listItems = new RepeatingView("memoryStatisticsIterator");
+    body.add(listItems);
+    System.gc();
+    for (MemoryPoolMXBean mpBean : ManagementFactory.getMemoryPoolMXBeans()) {
+      if (mpBean.getType() == MemoryType.HEAP) {
+        MemoryUsage usageBean = mpBean.getUsage();
+        String usage = new StringBuilder()
+                .append("max=").append(NumberHelper.formatBytes(usageBean.getMax()))
+                .append(", used=").append(NumberHelper.formatBytes(usageBean.getUsed()))
+                .append(", committed=").append(NumberHelper.formatBytes(usageBean.getCommitted()))
+                .append(", init=").append(NumberHelper.formatBytes(usageBean.getInit()))
+                .toString();
+        WebMarkupContainer row = new WebMarkupContainer(listItems.newChildId());
+        listItems.add(row);
+        row.add(new Label("memoryType", "Memory " + mpBean.getName()));
+        row.add(new Label("memoryStatistics", usage));
+        log.info("Memory: " + usage);
+      }
+    }
   }
 
-  private int getTableCount(final JdbcTemplate jdbc, final Class<?> entity)
-  {
+  private int getTableCount(final JdbcTemplate jdbc, final Class<?> entity) {
     try {
       return jdbc.queryForObject("SELECT COUNT(*) FROM " + HibernateUtils.getDBTableName(entity), Integer.class);
     } catch (final Exception ex) {
@@ -83,8 +105,7 @@ public class SystemStatisticsPage extends AbstractSecuredPage
   }
 
   @Override
-  protected String getTitle()
-  {
+  protected String getTitle() {
     return getString("system.statistics.title");
   }
 
