7d3decb181caf1c2123377cc7599ef67269929e4
Fin Reinhard <fin.reinhard@icloud.com>
2019-11-25 17:02:09 +0100

    removing unnecessary rows and cols

--- projectforge-rest/src/main/kotlin/org/projectforge/ui/UICol.kt
+++ projectforge-rest/src/main/kotlin/org/projectforge/ui/UICol.kt
@@ -56,7 +56,7 @@ open class UICol(
      * Convenient method for adding a bunch of UIInput fields with the given ids.
      * @param createRowCol If true (default), the elements will be surrounded with [UIRow] and [UICol] each, otherwise not.
      */
-    fun add(layoutSettings: LayoutContext, vararg ids: String, createRowCol: Boolean = true): UICol {
+    fun add(layoutSettings: LayoutContext, vararg ids: String, createRowCol: Boolean = false): UICol {
         ids.forEach {
             val element = LayoutUtils.buildLabelInputElement(layoutSettings, it)
             if (element != null) {
