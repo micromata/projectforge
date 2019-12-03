3cb77a4ba216174ad73417753726fd5fb24f0eee
Fin Reinhard <fin.reinhard@icloud.com>
2019-12-03 16:27:26 +0100

    add variables in list page

--- projectforge-webapp/src/containers/page/list/ListPage.jsx
+++ projectforge-webapp/src/containers/page/list/ListPage.jsx
@@ -23,6 +23,7 @@ function ListPage(
         extended: {},
     });
     const [filterFavorites, setFilterFavorites] = React.useState([]);
+    const [variables, setVariables] = React.useState({});
     const [loading, setLoading] = React.useState(false);
     const [error, setError] = React.useState(undefined);
 
@@ -127,12 +128,14 @@ function ListPage(
                     data: responseData,
                     filter: responseFilter,
                     filterFavorites: responseFilterFavorites,
+                    variables: responseVariables,
                 },
             ) => {
                 setFilter(responseFilter);
                 setFilterFavorites(responseFilterFavorites);
                 setData(responseData);
                 setUI(responseUi);
+                setVariables(responseVariables);
                 setLoading(false);
             })
             .catch(setError);
@@ -245,6 +248,7 @@ function ListPage(
                         setBrowserTitle: true,
                         showActionButtons: false,
                     }}
+                    variables={variables}
                 >
                     <SearchFilter />
                 </DynamicLayout>
