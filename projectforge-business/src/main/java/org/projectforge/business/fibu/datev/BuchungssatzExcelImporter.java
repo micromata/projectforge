5d239d87e06426589fa2cd6f3a47795ee667e4d2
Kai Reinhard <k.reinhard@micromata.de>
2019-12-30 01:20:10 +0100

    WIP: BuchungssatzExcelImport: Merlin support.

--- projectforge-business/src/main/java/org/projectforge/business/fibu/datev/BuchungssatzExcelImporter.java
+++ projectforge-business/src/main/java/org/projectforge/business/fibu/datev/BuchungssatzExcelImporter.java
@@ -29,7 +29,6 @@ import org.projectforge.business.fibu.KontoDO;
 import org.projectforge.business.fibu.KontoDao;
 import org.projectforge.business.fibu.KostFormatter;
 import org.projectforge.business.fibu.kost.*;
-import org.projectforge.framework.i18n.UserException;
 import org.projectforge.framework.persistence.utils.ImportStorage;
 import org.projectforge.framework.persistence.utils.ImportedElement;
 import org.projectforge.framework.persistence.utils.ImportedSheet;
@@ -62,6 +61,9 @@ public class BuchungssatzExcelImporter {
 
   private final ActionLog actionLog;
 
+  private ExcelColumnDateValidator dateValidator = new ExcelColumnDateValidator("dd.MM.yyyy", "dd.MM.yy", "yyyy-MM-dd");
+
+
   public BuchungssatzExcelImporter(final ImportStorage<BuchungssatzDO> storage, final KontoDao kontoDao, final Kost1Dao kost1Dao,
                                    final Kost2Dao kost2Dao, final ActionLog actionLog) {
     this.storage = storage;
@@ -71,7 +73,7 @@ public class BuchungssatzExcelImporter {
     this.actionLog = actionLog;
   }
 
-  public void doImport(final InputStream is) throws Exception {
+  public void doImport(final InputStream is) {
     final ExcelWorkbook workbook = new ExcelWorkbook(is, storage.getFilename());
     for (short idx = 0; idx < workbook.getNumberOfSheets(); idx++) {
       final ImportedSheet<BuchungssatzDO> importedSheet = importBuchungssaetze(workbook, idx);
@@ -81,8 +83,7 @@ public class BuchungssatzExcelImporter {
     }
   }
 
-  private ImportedSheet<BuchungssatzDO> importBuchungssaetze(final ExcelWorkbook workbook, final int idx) throws Exception {
-    ImportedSheet<BuchungssatzDO> importedSheet = null;
+  private ImportedSheet<BuchungssatzDO> importBuchungssaetze(final ExcelWorkbook workbook, final int idx) {
     ExcelSheet sheet = workbook.getSheet(idx);
     sheet.setAutotrimCellValues(true);
     final String name = sheet.getSheetName();
@@ -92,30 +93,21 @@ public class BuchungssatzExcelImporter {
     } catch (final NumberFormatException ex) {
       // ignore
     }
-    if (month != null) {
-      actionLog.logInfo("Importing sheet '" + name + "'.");
-      importedSheet = importBuchungssaetze(sheet, month);
-    } else {
+    if (month == null) {
       log.info("Ignoring sheet '" + name + "' for importing Buchungssätze.");
+      return null;
     }
-    return importedSheet;
-  }
-
-  /**
-   * @param month 1-January, ..., 12-December
-   * @throws Exception
-   */
-  private ImportedSheet<BuchungssatzDO> importBuchungssaetze(final ExcelSheet sheet, final Integer month) throws Exception {
-    final ImportedSheet<BuchungssatzDO> importedSheet = new ImportedSheet<>();
+    ImportedSheet<BuchungssatzDO> importedSheet = null;
+    actionLog.logInfo("Importing sheet '" + name + "'.");
     sheet.registerColumn("SatzNr.", "Satz-Nr.").addColumnListener(new ExcelColumnNumberValidator(1.0).setRequired().setUnique());
     sheet.registerColumn("Betrag").addColumnListener(new ExcelColumnNumberValidator().setRequired());
-    sheet.registerColumn("SH").addColumnListener(new ExcelColumnOptionsValidator("S", "H").setRequired());
+    sheet.registerColumn("SH", "S/H").addColumnListener(new ExcelColumnOptionsValidator("S", "H").setRequired());
     sheet.registerColumn("Konto").addColumnListener(new ExcelColumnNumberValidator().setRequired());
-    sheet.registerColumn("Kostenstelle/-träger", "Kost2").addColumnListener(new ExcelColumnValidator().setRequired());
+    sheet.registerColumn("Kostenstelle/-träger", "Kost2", "Kst.").addColumnListener(new ExcelColumnValidator().setRequired());
     sheet.registerColumn("Menge");
-    //sheet.registerColumn("SH"); // Second column not needed.
+    //sheet.registerColumn("SH", "S/H"); // Second column not needed.
     sheet.registerColumn("Beleg");
-    sheet.registerColumn("Datum").addColumnListener(new ExcelColumnDateValidator().setRequired());
+    sheet.registerColumn("Datum").addColumnListener(dateValidator);
     sheet.registerColumn("Gegenkonto").addColumnListener(new ExcelColumnValidator().setRequired());
     sheet.registerColumn("Text");
     sheet.registerColumn("Alt.-Kst.", "Kost1").addColumnListener(new ExcelColumnValidator().setRequired());
@@ -124,7 +116,20 @@ public class BuchungssatzExcelImporter {
     //sheet.registerColumn("ZI");
     sheet.registerColumn("Kommentar", "Bemerkung");
     sheet.analyze(true);
+    if (sheet.getHeadRow() == null) {
+      log.info("Ignoring sheet '" + name + "' for importing Buchungssätze, no valid head row found.");
+      return null;
+    }
+    importedSheet = importBuchungssaetze(sheet, month);
+    return importedSheet;
+  }
 
+  /**
+   * @param month 1-January, ..., 12-December
+   * @throws Exception
+   */
+  private ImportedSheet<BuchungssatzDO> importBuchungssaetze(final ExcelSheet sheet, final Integer month) {
+    final ImportedSheet<BuchungssatzDO> importedSheet = new ImportedSheet<>();
     Iterator<Row> it = sheet.getDataRowIterator();
     int year = 0;
     while (it.hasNext()) {
@@ -134,7 +139,9 @@ public class BuchungssatzExcelImporter {
       final BuchungssatzDO satz = new BuchungssatzDO();
       element.setValue(satz);
       satz.setSatznr(sheet.getCellInt(row, "SatzNr."));
-      PFDay day = PFDay.from(sheet.getCellDate(row, "Datum"));
+      PFDay day = PFDay.from(dateValidator.convert(sheet.getCell(row, "Datum")));
+      if (day == null)
+        continue; // Empty row? date not given.
       satz.setDatum(day.getSqlDate());
       if (year == 0) {
         year = day.getYear();
@@ -142,7 +149,7 @@ public class BuchungssatzExcelImporter {
         final String msg =
                 "Not supported: Buchungssätze innerhalb eines Excel-Sheets liegen in verschiedenen Jahren: Im Blatt '" + sheet.getSheetName() + "', in Zeile " + (row.getRowNum() + 1);
         actionLog.logError(msg);
-        throw new UserException(msg);
+        element.putErrorProperty("datum", "Buchungssatz liegt außerhalb des Buchungsmonats.");
       }
       if (day.getMonthValue() > month) {
         final String msg = "Buchungssätze können nicht in die Zukunft für den aktuellen Monat '"
@@ -150,7 +157,7 @@ public class BuchungssatzExcelImporter {
                 + " gebucht werden! "
                 + satz;
         actionLog.logError(msg);
-        throw new RuntimeException(msg);
+        element.putErrorProperty("datum", "Buchungssätze können nicht in die Zukunft für den aktuellen Monat erfasst werden.");
       } else if (day.getMonthValue() < month) {
         final String msg = "Buchungssatz liegt vor Monat '" + KostFormatter.formatBuchungsmonat(year, month) + "' (OK): " + satz;
         actionLog.logInfo(msg);
@@ -194,7 +201,7 @@ public class BuchungssatzExcelImporter {
       } else {
         element.putErrorProperty("kost2", kostString);
       }
-      satz.calculate();
+      satz.calculate(true);
       importedSheet.addElement(element);
       log.debug(satz.toString());
     }
