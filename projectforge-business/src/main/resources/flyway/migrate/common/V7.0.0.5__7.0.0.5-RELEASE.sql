1a8801134887886087ab2f967c59401f318d563f
Kai Reinhard <k.reinhard@micromata.de>
2020-01-03 15:25:28 +0100

    RemainingDaysOfVacationDO/Dao added.

---
+++ projectforge-business/src/main/resources/flyway/migrate/common/V7.0.0.5__7.0.0.5-RELEASE.sql
@@ -0,0 +1,25 @@
+CREATE TABLE t_fibu_employee_vacation_remaining (
+  pk                                     INTEGER                      NOT NULL,
+  created                                TIMESTAMP WITHOUT TIME ZONE,
+  deleted                                BOOLEAN                      NOT NULL,
+  last_update                            TIMESTAMP WITHOUT TIME ZONE,
+  tenant_id                              INTEGER,
+  employee_id                            INTEGER                      NOT NULL,
+  year                                   INTEGER                      NOT NULL,
+  carry_vacation_days_from_previous_year INTEGER                      NOT NULL
+);
+
+ALTER TABLE t_fibu_employee_vacation_remaining
+  ADD CONSTRAINT t_fibu_employee_vacation_remaining_pkey PRIMARY KEY (pk);
+
+ALTER TABLE t_fibu_employee_vacation_remaining
+  ADD CONSTRAINT unique_t_fibu_employee_vacation_remaining UNIQUE (tenant_id, employee_id, year);
+
+CREATE INDEX idx_fk_t_vacation_remaining_employee_id
+  ON t_fibu_employee_vacation_remaining (pk);
+
+ALTER TABLE t_fibu_employee_vacation_remaining
+  ADD CONSTRAINT t_fibu_employee_vacation_remaining_tenant_fk FOREIGN KEY (tenant_id) REFERENCES t_tenant (pk);
+
+ALTER TABLE t_fibu_employee_vacation_remaining
+  ADD CONSTRAINT t_fibu_employee_vacation_remaining_employee_fk FOREIGN KEY (employee_id) REFERENCES t_fibu_employee (pk);
