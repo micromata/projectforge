5af5f45e5f887c1cd5d0729f65cacfe5c2fe99fc
Kai Reinhard <k.reinhard@micromata.de>
2020-01-08 01:19:43 +0100

    Source file headers fixed.

--- projectforge-business/src/main/kotlin/org/projectforge/business/vacation/VacationRight.kt
+++ projectforge-business/src/main/kotlin/org/projectforge/business/vacation/VacationRight.kt
@@ -20,6 +20,7 @@
 // with this program; if not, see http://www.gnu.org/licenses/.
 //
 /////////////////////////////////////////////////////////////////////////////
+
 package org.projectforge.business.vacation
 
 import org.projectforge.business.multitenancy.TenantRegistryMap
