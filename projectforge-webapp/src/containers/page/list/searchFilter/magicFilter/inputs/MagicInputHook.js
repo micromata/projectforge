139f9f2a83409b762cc067f273d827a3b10f97c7
Fin Reinhard <fin.reinhard@icloud.com>
2020-01-09 14:51:29 +0100

    implement TimeStamp Magic Input

--- projectforge-webapp/src/containers/page/list/searchFilter/magicFilter/inputs/MagicInputHook.js
+++ projectforge-webapp/src/containers/page/list/searchFilter/magicFilter/inputs/MagicInputHook.js
@@ -2,6 +2,7 @@ import React from 'react';
 import MagicInputNotImplemented from './MagicInputNotImplemented';
 import MagicSelectInput from './MagicSelectInput';
 import MagicStringInput from './MagicStringInput';
+import MagicTimeStampInput from './MagicTimeStampInput';
 
 const useMagicInput = (type) => {
     const [inputTag, setInputTag] = React.useState(() => MagicInputNotImplemented);
@@ -15,6 +16,9 @@ const useMagicInput = (type) => {
                 break;
             case 'SELECT':
                 tag = MagicSelectInput;
+                break;
+            case 'TIME_STAMP':
+                tag = MagicTimeStampInput;
                 break;
             default:
                 tag = MagicInputNotImplemented;
