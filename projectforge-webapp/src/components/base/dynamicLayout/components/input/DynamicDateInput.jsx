0e2e3bddb66867bc5b7b46e5ffb370794690a997
Kai Reinhard <k.reinhard@micromata.de>
2020-01-10 23:30:34 +0100

    DayPicker: new version. Problems with localized date format. Fall-back to ISO format.

--- projectforge-webapp/src/components/base/dynamicLayout/components/input/DynamicDateInput.jsx
+++ projectforge-webapp/src/components/base/dynamicLayout/components/input/DynamicDateInput.jsx
@@ -16,19 +16,22 @@ function DynamicDateInput(props) {
         additionalLabel,
         focus,
         id,
-        jsDateFormat,
+        // jsDateFormat,
         label,
-        locale,
+        // locale,
         required,
+        // timeZone,
     } = props;
-
     const { data, setData, ui } = React.useContext(DynamicLayoutContext);
     const value = Object.getByString(data, id);
+    const format = 'YYYY-MM-DD';
 
+    // Might be not work in if time zone of browser differs from PF-Users timezone:
+    // moment.tz.setDefault(timeZone);
     return React.useMemo(() => {
         const handleDayChange = day => setData({
             [id]: day ? timezone(day)
-                .format('YYYY-MM-DD') : null,
+                .format(format) : null,
         });
 
         return (
@@ -39,15 +42,16 @@ function DynamicDateInput(props) {
                         autoFocus={focus}
                         formatDate={formatDate}
                         parseDate={parseDate}
-                        format={jsDateFormat}
+                        format={format}
                         value={value}
                         onDayChange={handleDayChange}
                         dayPickerProps={{
-                            locale,
+                            // locale,
                             localeUtils: MomentLocaleUtils,
                             todayButton: ui.translations['calendar.today'],
                         }}
-                        placeholder={jsDateFormat}
+                        month={value}
+                        placeholder={format}
                         required={required}
                     />
                     <AdditionalLabel title={additionalLabel} />
@@ -59,24 +63,25 @@ function DynamicDateInput(props) {
 
 DynamicDateInput.propTypes = {
     id: PropTypes.string.isRequired,
-    jsDateFormat: PropTypes.string.isRequired,
+    // jsDateFormat: PropTypes.string.isRequired,
     label: PropTypes.string.isRequired,
     additionalLabel: PropTypes.string,
     focus: PropTypes.bool,
-    locale: PropTypes.string,
+    // locale: PropTypes.string,
     required: PropTypes.bool,
 };
 
 DynamicDateInput.defaultProps = {
     additionalLabel: undefined,
     focus: false,
-    locale: 'en',
+    // locale: 'en',
     required: false,
 };
 
 const mapStateToProps = ({ authentication }) => ({
-    jsDateFormat: authentication.user.jsDateFormat,
-    locale: authentication.user.locale,
+    // jsDateFormat: authentication.user.jsDateFormat,
+    // locale: authentication.user.locale,
+    // timeZone: authentication.user.timeZone,
 });
 
 export default connect(mapStateToProps)(DynamicDateInput);
