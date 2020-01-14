bb4966dd8d60a601307d246c652adcc8f38972cc
Kai Reinhard <k.reinhard@micromata.de>
2020-01-14 17:14:39 +0100

    VacationDO.employeeId

--- projectforge-business/src/main/kotlin/org/projectforge/business/vacation/model/VacationDO.kt
+++ projectforge-business/src/main/kotlin/org/projectforge/business/vacation/model/VacationDO.kt
@@ -58,6 +58,10 @@ open class VacationDO : DefaultBaseDO() {
     @get:JoinColumn(name = "employee_id", nullable = false)
     open var employee: EmployeeDO? = null
 
+    val employeeId: Int?
+        @Transient
+        get() = employee?.id
+
     @PropertyInfo(i18nKey = "vacation.startdate")
     @get:Column(name = "start_date", nullable = false)
     open var startDate: LocalDate? = null
