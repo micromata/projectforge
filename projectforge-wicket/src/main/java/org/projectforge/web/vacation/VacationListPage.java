b4281393937cb78ae8a3ff86da7e3816f70cd6ef
Kai Reinhard <k.reinhard@micromata.de>
2020-01-19 14:11:59 +0100

    VacationService prepared for leave days with half day begin and/or half day end.

--- projectforge-wicket/src/main/java/org/projectforge/web/vacation/VacationListPage.java
+++ projectforge-wicket/src/main/java/org/projectforge/web/vacation/VacationListPage.java
@@ -128,8 +128,7 @@ public class VacationListPage extends AbstractListPage<VacationListForm, Vacatio
             cellItemListener));
 
     columns.add(new CellItemListenerLambdaColumn<>(new ResourceModel("vacation.workingdays"),
-        rowModel -> vacationService.getVacationDays(rowModel.getObject().getStartDate(), rowModel.getObject().getEndDate(), rowModel.getObject().getHalfDayBegin()),
-        cellItemListener)
+        rowModel -> vacationService.getVacationDays(rowModel.getObject()), cellItemListener)
     );
 
     columns
