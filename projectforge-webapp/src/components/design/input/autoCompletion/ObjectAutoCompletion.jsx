4355cd432495a80367f9089d772448fceec86c0c
Fin Reinhard <fin.reinhard@icloud.com>
2020-01-19 19:30:41 +0100

    remove unused import

--- projectforge-webapp/src/components/design/input/autoCompletion/ObjectAutoCompletion.jsx
+++ projectforge-webapp/src/components/design/input/autoCompletion/ObjectAutoCompletion.jsx
@@ -1,4 +1,4 @@
-import { faTimes, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import PropTypes from 'prop-types';
 import React from 'react';
