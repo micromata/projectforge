4056f0625dd32ef20adeb4560921ce70522d0b78
Kai Reinhard <k.reinhard@micromata.de>
2020-01-20 16:43:08 +0100

    Renaming stuff: *BaseRest -> *PagesRest.

---
+++ projectforge-rest/src/main/kotlin/org/projectforge/rest/fibu/EmployeeSalaryPagesRest.kt
@@ -0,0 +1,80 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2020 Micromata GmbH, Germany (www.micromata.com)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.rest.fibu
+
+import org.projectforge.business.fibu.EmployeeSalaryDO
+import org.projectforge.business.fibu.EmployeeSalaryDao
+import org.projectforge.rest.config.Rest
+import org.projectforge.rest.core.AbstractDTOPagesRest
+import org.projectforge.rest.dto.EmployeeSalary
+import org.projectforge.ui.*
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("${Rest.URL}/employeeSalary")
+class EmployeeSalaryPagesRest
+    : AbstractDTOPagesRest<EmployeeSalaryDO, EmployeeSalary, EmployeeSalaryDao>(
+        EmployeeSalaryDao::class.java,
+        "fibu.employee.salary.title") {
+
+    override fun transformFromDB(obj: EmployeeSalaryDO, editMode: Boolean): EmployeeSalary {
+        val employeeSalary = EmployeeSalary()
+        employeeSalary.copyFrom(obj)
+        return employeeSalary
+    }
+
+    override fun transformForDB(dto: EmployeeSalary): EmployeeSalaryDO {
+        val employeeSalaryDO = EmployeeSalaryDO()
+        dto.copyTo(employeeSalaryDO)
+        return employeeSalaryDO
+    }
+
+    /**
+     * LAYOUT List page
+     */
+    override fun createListLayout(): UILayout {
+        val layout = super.createListLayout()
+                .add(UITable.UIResultSetTable()
+                        .add(lc, "month")
+                        .add(UITableColumn("fibu.employee.user.lastname", "name"))
+                        .add(UITableColumn("fibu.employee.user.firstname", "firstName"))
+                        .add(UITableColumn("fibu.employee.staffNumber", "fibu.employee.staffNumber"))
+                        .add(lc, "type", "bruttoMitAgAnteil", "comment"))
+        return LayoutUtils.processListPage(layout, this)
+    }
+
+    /**
+     * LAYOUT Edit page
+     */
+    override fun createEditLayout(dto: EmployeeSalary, userAccess: UILayout.UserAccess): UILayout {
+        val layout = super.createEditLayout(dto, userAccess)
+                .add(UIRow()
+                        .add(UICol()
+                                .add(lc, "fibu.employee.user.lastname", "fibu.employee.user.firstname")
+                                .add(UILabel("TODO: Implement selector for year/month"))
+                                .add(lc, "type", "bruttoMitAgAnteil", "comment")))
+        return LayoutUtils.processEditPage(layout, dto, this)
+    }
+}
---
+++ projectforge-rest/src/main/kotlin/org/projectforge/rest/orga/PostausgangPagesRest.kt
@@ -0,0 +1,93 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2020 Micromata GmbH, Germany (www.micromata.com)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.rest.orga
+
+import org.projectforge.business.orga.PostFilter
+import org.projectforge.business.orga.PostType
+import org.projectforge.business.orga.PostausgangDO
+import org.projectforge.business.orga.PostausgangDao
+import org.projectforge.framework.i18n.translate
+import org.projectforge.framework.time.PFDay
+import org.projectforge.rest.config.Rest
+import org.projectforge.rest.core.AbstractDOPagesRest
+import org.projectforge.ui.*
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import javax.servlet.http.HttpServletRequest
+
+@RestController
+@RequestMapping("${Rest.URL}/outgoingMail")
+class PostausgangPagesRest() : AbstractDOPagesRest<PostausgangDO, PostausgangDao>(PostausgangDao::class.java, "orga.postausgang.title") {
+    /**
+     * Initializes new outbox mails for adding.
+     */
+    override fun newBaseDO(request: HttpServletRequest?): PostausgangDO {
+        val outbox = super.newBaseDO(request)
+        outbox.datum = PFDay.now().sqlDate
+        outbox.type = PostType.BRIEF
+        return outbox
+    }
+
+    override fun validate(validationErrors: MutableList<ValidationError>, dto: PostausgangDO) {
+        val date = PFDay.from(dto.datum)
+        if (date != null && PFDay.now().isBefore(date)) { // No dates in the future accepted.
+            validationErrors.add(ValidationError(translate("error.dateInFuture"), fieldId = "datum"))
+        }
+    }
+
+    /**
+     * LAYOUT List page
+     */
+    override fun createListLayout(): UILayout {
+        val layout = super.createListLayout()
+                .add(UITable.UIResultSetTable()
+                        .add(lc, "datum", "empfaenger", "person", "inhalt", "bemerkung", "type"))
+        layout.getTableColumnById("datum").formatter = Formatter.DATE
+        LayoutUtils.addListFilterContainer(layout, UILabel("'TODO: date range"),
+                filterClass = PostFilter::class.java)
+        return LayoutUtils.processListPage(layout, this)
+    }
+
+    /**
+     * LAYOUT Edit page
+     */
+    override fun createEditLayout(dto: PostausgangDO, userAccess: UILayout.UserAccess): UILayout {
+        val receiver = UIInput("empfaenger", lc) // Input-field instead of text-area (length > 255)
+        receiver.focus = true
+        receiver.enableAutoCompletion(this)
+        val person = UIInput("person", lc).enableAutoCompletion(this)
+        val inhalt = UIInput("inhalt", lc).enableAutoCompletion(this)
+        val layout = super.createEditLayout(dto, userAccess)
+                .add(UIRow()
+                        .add(UICol(length = 2)
+                                .add(lc, "datum"))
+                        .add(UICol(length = 10)
+                                .add(lc, "type")))
+                .add(receiver)
+                .add(person)
+                .add(inhalt)
+                .add(lc, "bemerkung")
+        return LayoutUtils.processEditPage(layout, dto, this)
+    }
+}
