0b38bb6b616d72077cb3bad304ee8b5692c971b4
Kai Reinhard <k.reinhard@micromata.de>
2020-01-23 23:51:01 +0100

    React: VacationAccountPage WIP

--- projectforge-webapp/src/components/base/dynamicLayout/components/customized/components/VacationEntries.jsx
+++ projectforge-webapp/src/components/base/dynamicLayout/components/customized/components/VacationEntries.jsx
@@ -3,45 +3,90 @@ import React from 'react';
 import 'react-rrule-generator/build/styles.css';
 import { Col, Row, Table } from 'reactstrap';
 import { DynamicLayoutContext } from '../../../context';
+import style from './Vacation.module.scss';
 
 function VacationEntriesTable(vacationEntries, translations, year) {
     return (
         <React.Fragment>
-            <h4>
-                {translations['vacation.title.list']}
-                {' '}
-                {year}
-            </h4>
-            <Table striped>
-                <thead>
-                <tr>
-                    <th>{translations['vacation.startdate']}</th>
-                    <th>{translations['vacation.enddate']}</th>
-                    <th>{translations['vacation.status']}</th>
-                    <th>{translations['vacation.workingdays']}</th>
-                    <th>{translations['vacation.special']}</th>
-                    <th>{translations['vacation.replacement']}</th>
-                    <th>{translations['vacation.manager']}</th>
-                    <th>{translations['vacation.vacationmode']}</th>
-                    <th>{translations.comment}</th>
-                </tr>
-                </thead>
-                <tbody>
-                {vacationEntries.map(entry => (
-                    <tr key={entry.id}>
-                        <td>{entry.startDateFormatted}</td>
-                        <td>{entry.endDateFormatted}</td>
-                        <td>{entry.statusString}</td>
-                        <td>{entry.workingDaysFormatted}</td>
-                        <td>{entry.specialFormatted}</td>
-                        <td>{entry.replacement.displayName}</td>
-                        <td>{entry.manager.displayName}</td>
-                        <td>{entry.vacationModeString}</td>
-                        <td>{entry.comment}</td>
-                    </tr>
-                ))}
-                </tbody>
-            </Table>
+            {vacationEntries && vacationEntries.length > 0
+                ? (
+                    <Row>
+                        <Col sm={12}>
+                            <h4>
+                                {translations['vacation.title.list']}
+                                {' '}
+                                {year}
+                            </h4>
+                            <Table striped>
+                                <thead>
+                                <tr>
+                                    <th>{translations['vacation.startdate']}</th>
+                                    <th>{translations['vacation.enddate']}</th>
+                                    <th>{translations['vacation.status']}</th>
+                                    <th className={style.number}>{translations['vacation.Days']}</th>
+                                    <th>{translations['vacation.special']}</th>
+                                    <th>{translations['vacation.replacement']}</th>
+                                    <th>{translations['vacation.manager']}</th>
+                                    <th>{translations['vacation.vacationmode']}</th>
+                                    <th>{translations.comment}</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                {vacationEntries.map(entry => (
+                                    <tr key={entry.id}>
+                                        <td>{entry.startDateFormatted}</td>
+                                        <td>{entry.endDateFormatted}</td>
+                                        <td>{entry.statusString}</td>
+                                        <td className={style.number}>{entry.workingDaysFormatted}</td>
+                                        <td>{entry.specialFormatted}</td>
+                                        <td>{entry.replacement.displayName}</td>
+                                        <td>{entry.manager.displayName}</td>
+                                        <td>{entry.vacationModeString}</td>
+                                        <td>{entry.comment}</td>
+                                    </tr>
+                                ))}
+                                </tbody>
+                            </Table>
+                        </Col>
+                    </Row>
+                ) : undefined
+            }
+        </React.Fragment>
+    );
+}
+
+function LeaveAccountEntriesTable(leaveAccountEntries, translations) {
+    return (
+        <React.Fragment>
+            {leaveAccountEntries && leaveAccountEntries.length > 0
+                ? (
+                    <Row>
+                        <Col sm={12}>
+                            <h4>
+                                {translations['vacation.leaveAccountEntry.title.heading']}
+                            </h4>
+                            <Table striped>
+                                <thead>
+                                <tr>
+                                    <th>{translations.date}</th>
+                                    <th className={style.number}>{translations['vacation.leaveAccountEntry.amount']}</th>
+                                    <th className={style.fullWidth}>{translations.description}</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                {leaveAccountEntries.map(entry => (
+                                    <tr key={entry.id}>
+                                        <td>{entry.dateFormatted}</td>
+                                        <td className={style.number}>{entry.amount}</td>
+                                        <td>{entry.description}</td>
+                                    </tr>
+                                ))}
+                                </tbody>
+                            </Table>
+                        </Col>
+                    </Row>
+                ) : undefined
+            }
         </React.Fragment>
     );
 }
@@ -51,24 +96,18 @@ function VacationEntries({ values }) {
 
     const {
         vacationsCurrentYear,
-        vacationPreviousYear,
+        vacationsPreviousYear,
+        leaveAccountEntries,
         yearCurrent,
         yearPrevious,
     } = values;
-    const currentVacations = vacationsCurrentYear;
-    const prevVacations = vacationPreviousYear;
 
     return React.useMemo(
         () => (
             <React.Fragment>
-                <Row>
-                    <Col sm={12}>
-                        {!currentVacations || currentVacations.length > 0
-                            ? (VacationEntriesTable(currentVacations, ui.translations, yearCurrent)
-                            ) : undefined
-                        }
-                    </Col>
-                </Row>
+                {VacationEntriesTable(vacationsCurrentYear, ui.translations, yearCurrent)}
+                {VacationEntriesTable(vacationsPreviousYear, ui.translations, yearPrevious)}
+                {LeaveAccountEntriesTable(leaveAccountEntries, ui.translations)}
             </React.Fragment>
         ),
     );
