cd23149304d31d3ba1cd956d4c53ffe827a4a03d
Fin Reinhard <fin.reinhard@icloud.com>
2020-01-29 16:31:13 +0100

    adding second label to MagicCheckbox

--- projectforge-webapp/src/containers/page/list/searchFilter/magicFilter/inputs/MagicCheckboxInput.jsx
+++ projectforge-webapp/src/containers/page/list/searchFilter/magicFilter/inputs/MagicCheckboxInput.jsx
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { CheckBox } from '../../../../../../components/design';
-import MagicObjectInput from './MagicObjectInput';
 
 function MagicCheckboxInput(
     {
         id,
+        label,
         onChange,
         value,
     },
@@ -15,12 +15,14 @@ function MagicCheckboxInput(
             id={`magic-checkbox-${id}`}
             onChange={({ target }) => onChange({ value: target.checked })}
             checked={value.value === true}
+            label={label}
         />
     );
 }
 
 MagicCheckboxInput.propTypes = {
     id: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
     onChange: PropTypes.func.isRequired,
     value: PropTypes.shape({}).isRequired,
 };
