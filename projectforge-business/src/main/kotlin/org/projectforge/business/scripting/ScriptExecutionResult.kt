0c820b4fcfec547e34de95b64bc6a8d56fe7371a
Kai Reinhard <k.reinhard@micromata.de>
2020-02-04 22:21:27 +0100

    Source file header fixed.

--- projectforge-business/src/main/kotlin/org/projectforge/business/scripting/ScriptExecutionResult.kt
+++ projectforge-business/src/main/kotlin/org/projectforge/business/scripting/ScriptExecutionResult.kt
@@ -20,6 +20,7 @@
 // with this program; if not, see http://www.gnu.org/licenses/.
 //
 /////////////////////////////////////////////////////////////////////////////
+
 package org.projectforge.business.scripting
 
 import org.projectforge.business.utils.HtmlHelper
