03d1d7ab55d34f6ddf1eb1669d24a3f48a7d6b19
Fin Reinhard <fin.reinhard@icloud.com>
2020-02-19 00:27:27 +0100

    made changes after switch possible

--- projectforge-webapp/src/reducers/edit/currentCategory.js
+++ projectforge-webapp/src/reducers/edit/currentCategory.js
@@ -1,10 +1,11 @@
-import { EDIT_CALL_INITIAL_BEGIN } from '../../actions';
+import { EDIT_CALL_INITIAL_BEGIN, EDIT_SWITCH_CATEGORY } from '../../actions';
 
 const initialState = null;
 
 const reducer = (state = initialState, { type, payload }) => {
     switch (type) {
         case EDIT_CALL_INITIAL_BEGIN:
+        case EDIT_SWITCH_CATEGORY:
             return payload.category;
         default:
             return state;
