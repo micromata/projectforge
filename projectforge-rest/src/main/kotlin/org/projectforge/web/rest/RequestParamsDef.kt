102d76df7aeead1d7f369afcf30077ce127569a1
Kai Reinhard <k.reinhard@micromata.de>
2020-02-22 09:02:15 +0100

    Crypt: NPE fix in decrypt

--- projectforge-rest/src/main/kotlin/org/projectforge/web/rest/RequestParamsDef.kt
+++ projectforge-rest/src/main/kotlin/org/projectforge/web/rest/RequestParamsDef.kt
@@ -20,6 +20,7 @@
 // with this program; if not, see http://www.gnu.org/licenses/.
 //
 /////////////////////////////////////////////////////////////////////////////
+
 package org.projectforge.web.rest
 
 import org.projectforge.rest.Authentication
