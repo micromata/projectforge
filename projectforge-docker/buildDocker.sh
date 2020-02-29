ec22b00450d7fa19aba5d84ddf3fdc893a2c68c7
Kai Reinhard <k.reinhard@micromata.de>
2020-02-29 22:50:35 +0100

    Dockerfile -> app/Dockerfile

--- projectforge-docker/buildDocker.sh
+++ projectforge-docker/buildDocker.sh
@@ -3,11 +3,11 @@
 echo "Make sure, that you've run 'mvn clean install' on top directory first"
 
 echo "Unpacking spring boot fat jar to target/dependency..."
-rm -rf target/dependency
-mkdir -p target/dependency && (cd target/dependency; jar -xf ../../../projectforge-application/target/*.jar)
+rm -rf app/target/dependency
+mkdir -p app/target/dependency && (cd app/target/dependency; jar -xf ../../../../projectforge-application/target/*.jar)
 
 echo "Building docker file..."
-docker build -t micromata/projectforge .
+(cd app; docker build -t micromata/projectforge .)
 
 echo "Run with 'docker run -p 8080:8080 -v ~/ProjectForge:/home/projectforge/ProjectForge micromata/projectforge'"
 
