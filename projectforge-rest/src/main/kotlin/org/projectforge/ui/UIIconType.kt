881fccb8a3ee563ed4459844768cd32d60da9595
Kai Reinhard <k.reinhard@micromata.de>
2020-03-12 14:08:11 +0100

    Source file header fixed.

--- projectforge-rest/src/main/kotlin/org/projectforge/ui/UIIconType.kt
+++ projectforge-rest/src/main/kotlin/org/projectforge/ui/UIIconType.kt
@@ -11,7 +11,7 @@
 // modify it under the terms of the GNU General Public License as published
 // by the Free Software Foundation; version 3 of the License.
 //
-// This community edition is distributed in the hope that IT will be useful,
+// This community edition is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
 // Public License for more details.
