24e595b6c948a75e8a8a1d152f2bf74b9e312bd5
Kai Reinhard <k.reinhard@micromata.de>
2020-03-24 00:39:24 +0100

    WIP: MyAccount as React page...

--- projectforge-rest/src/main/kotlin/org/projectforge/rest/MyAccountPageRest.kt
+++ projectforge-rest/src/main/kotlin/org/projectforge/rest/MyAccountPageRest.kt
@@ -24,12 +24,16 @@
 package org.projectforge.rest
 
 import mu.KotlinLogging
+import org.projectforge.Const
 import org.projectforge.business.group.service.GroupService
 import org.projectforge.business.user.UserAuthenticationsService
+import org.projectforge.business.user.UserDao
 import org.projectforge.business.user.UserTokenType
+import org.projectforge.framework.i18n.translate
 import org.projectforge.framework.persistence.user.api.ThreadLocalUserContext
 import org.projectforge.framework.persistence.user.entities.PFUserDO
 import org.projectforge.framework.persistence.user.entities.UserAuthenticationsDO
+import org.projectforge.framework.time.DateTimeFormatter
 import org.projectforge.rest.config.Rest
 import org.projectforge.rest.dto.FormLayoutData
 import org.projectforge.ui.*
@@ -37,6 +41,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 private val log = KotlinLogging.logger {}
 
@@ -49,6 +54,9 @@ class MyAccountPageRest {
     @Autowired
     private lateinit var groupService: GroupService
 
+    @Autowired
+    private lateinit var userDao: UserDao
+
     class MyAccountData(
             var username: String? = null,
             var firstname: String? = null,
@@ -56,13 +64,16 @@ class MyAccountPageRest {
             var calendarExportToken: String? = null,
             var davToken: String? = null,
             var restClientToken: String? = null,
-            var groups: String? = null
+            var stayLoggedInKey: String? = null,
+            var groups: String? = null,
+            var lastLogin: String? = null,
+            var locale: Locale? = null
     )
 
     @GetMapping("dynamic")
     fun getForm(): FormLayoutData {
-        val user = ThreadLocalUserContext.getUser()
         val userId = ThreadLocalUserContext.getUserId()
+        val user = userDao.getById(userId)
         val data = MyAccountData(user.username, user.firstname, user.lastname)
 
         val layout = UILayout("user.myAccount.title.edit")
@@ -72,23 +83,67 @@ class MyAccountPageRest {
         data.calendarExportToken = authenticationsService.getToken(userId, UserTokenType.CALENDAR_REST)
         data.davToken = authenticationsService.getToken(userId, UserTokenType.DAV_TOKEN)
         data.restClientToken = authenticationsService.getToken(userId, UserTokenType.REST_CLIENT)
+        data.stayLoggedInKey = authenticationsService.getToken(userId, UserTokenType.STAY_LOGGED_IN_KEY)
         data.groups = groupService.getGroupnames(userId)
+        data.locale = ThreadLocalUserContext.getLocale() ?: Locale("DEFAULT")
 
-        layout.add(UIFieldset(UILength(md = 6))
-                        .add(UIReadOnlyField("username", userLC))
-                        .add(UIInput("firstname", userLC))
-                        .add(UIInput("lastname", userLC))
-                        .add(UIReadOnlyField("calendarExportToken", authenticationsLC, canCopy = true, coverUp = true, ignoreTooltip = true))
-                        .add(UIReadOnlyField("davToken", authenticationsLC, canCopy = true, coverUp = true, ignoreTooltip = true))
-                        .add(UIReadOnlyField("restClientToken", authenticationsLC, canCopy = true, coverUp = true, ignoreTooltip = true))
-                )
+        data.lastLogin = DateTimeFormatter.instance().getFormattedDateTime(user.lastLogin)
+
+        val locales = Const.LOCALIZATIONS.map { UISelectValue(Locale(it), translate("locale.$it")) }.toMutableList()
+        locales.add(0, UISelectValue(Locale("DEFAULT"), translate("user.defaultLocale")))
+
+        layout.add(UIRow()
+                        .add(UIFieldset(UILength(lg = 6))
+                                .add(UIReadOnlyField("username", userLC))
+                                .add(UIInput("firstname", userLC))
+                                .add(UIInput("lastname", userLC))
+                                .add(addAuthenticationToken(authenticationsLC, "stayLoggedInKey"))
+                                .add(addAuthenticationToken(authenticationsLC, "calendarExportToken"))
+                                .add(addAuthenticationToken(authenticationsLC, "davToken"))
+                                .add(addAuthenticationToken(authenticationsLC, "restClientToken"))
+                        )
+                        .add(UIFieldset(UILength(lg = 6))
+                                .add(UIReadOnlyField("lastLogin", dataLC, label = "login.lastLogin"))
+                                .add(UISelect("locale", dataLC, label = "user.locale", required = true, values = locales))
+                        ))
                 .add(UIFieldset(12)
                         .add(UIReadOnlyField("groups", dataLC, label = "user.assignedGroups"))
                 )
-        LayoutUtils.process(layout)
-        //layout.addTranslation("user.assignedGroups")
 
+        // Datumsformat
+        //Exceldatumsformat
+        //Uhrzeitformat
+        //Zeitzone
+        //Telefonkennungen
+        //MEB Mobilfunknummern
+
+        // Kalender Whitelist für Kalendersoftware
+
+        // Beschreibung
+        // SSH public key
+        LayoutUtils.process(layout)
+
+        // Passwort ändern
+        //WLAN/Samba Passwort ändern
 
         return FormLayoutData(data, layout, null)
+    }
+
+    private fun addAuthenticationToken(lc: LayoutContext, id: String): UIRow {
+        return UIRow()
+                .add(UICol(9)
+                        .add(UIReadOnlyField(id, lc, canCopy = true, coverUp = true, ignoreTooltip = true))
+                )
+                .add(UICol(3)
+                        .add(UIButton("renew",
+                                title = translate("user.authenticationToken.renew"),
+                                confirmMessage = translate("user.authenticationToken.renew.securityQuestion"),
+                                color = UIColor.DANGER,
+                                responseAction = ResponseAction("/rs/renew", targetType = TargetType.TOAST)))
+                        .add(UIButton("usage",
+                                title = translate("user.authenticationToken.button.showUsage"),
+                                color = UIColor.LINK,
+                                responseAction = ResponseAction("/rs/usage", targetType = TargetType.POST)))
+                )
     }
 }
