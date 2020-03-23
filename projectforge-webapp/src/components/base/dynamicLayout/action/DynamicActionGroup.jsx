ec4e2adbd034280942a5b18f4cd6295eb7afba53
Fin Reinhard <fin.reinhard@icloud.com>
2020-03-23 14:25:14 +0100

    fix DynamicActionGroup margin

--- projectforge-webapp/src/components/base/dynamicLayout/action/DynamicActionGroup.jsx
+++ projectforge-webapp/src/components/base/dynamicLayout/action/DynamicActionGroup.jsx
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { colorPropType } from '../../../../utilities/propTypes';
-import { ButtonGroup, Spinner } from '../../../design';
-import { DynamicLayoutContext } from '../context';
+import { ButtonGroup, Col, Row, Spinner } from '../../../design';
 import DynamicButton from '../components/DynamicButton';
+import { DynamicLayoutContext } from '../context';
 
 export const actionPropType = PropTypes.shape({
     id: PropTypes.string.isRequired,
@@ -21,17 +21,19 @@ function DynamicActionGroup({ actions }) {
     const { isFetching } = React.useContext(DynamicLayoutContext);
 
     return (
-        <React.Fragment>
-            <ButtonGroup>
-                {actions.map(action => (
-                    <DynamicButton
-                        key={`dynamic-action-button-${action.id}-${action.key}`}
-                        {...action}
-                    />
-                ))}
-            </ButtonGroup>
-            {isFetching && <Spinner color="primary" style={{ marginLeft: '1em' }} />}
-        </React.Fragment>
+        <Row>
+            <Col>
+                <ButtonGroup>
+                    {actions.map(action => (
+                        <DynamicButton
+                            key={`dynamic-action-button-${action.id}-${action.key}`}
+                            {...action}
+                        />
+                    ))}
+                </ButtonGroup>
+                {isFetching && <Spinner color="primary" style={{ marginLeft: '1em' }} />}
+            </Col>
+        </Row>
     );
 }
 
