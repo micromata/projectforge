4032c4f980cb377dadf82cf087eb7fba66bbf32e
Kai Reinhard <k.reinhard@micromata.de>
2020-03-27 23:30:56 +0100

    SetupPage -> SetupPageRest

--- projectforge-rest/src/main/kotlin/org/projectforge/rest/pub/SetupPageRest.kt
+++ projectforge-rest/src/main/kotlin/org/projectforge/rest/pub/SetupPageRest.kt
@@ -23,6 +23,7 @@
 
 package org.projectforge.rest.pub
 
+import mu.KotlinLogging
 import org.projectforge.framework.configuration.ConfigurationParam
 import org.projectforge.framework.configuration.GlobalConfiguration
 import org.projectforge.framework.persistence.database.DatabaseService
@@ -37,15 +38,16 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+private val log = KotlinLogging.logger {}
+
 /**
  * This rest service should be available without login (public).
+ * Under construction. Wicket version is used.
  */
 @RestController
 @RequestMapping("${Rest.PUBLIC_URL}/setup")
-open class SetupRest {
+open class SetupPageRest {
     data class LoginData(var username: String? = null, var password: String? = null, var stayLoggedIn: Boolean? = null)
-
-    private val log = org.slf4j.LoggerFactory.getLogger(SetupRest::class.java)
 
     @Autowired
     private lateinit var applicationContext: ApplicationContext
@@ -58,8 +60,7 @@ open class SetupRest {
         val layout = UILayout("administration.setup.title")
         if (databaseService.databaseTablesWithEntriesExists()) {
             log.error("Data-base isn't empty: SetupPage shouldn't be used...")
-            return FormLayoutData(null, layout, null)
-            // throw RestartResponseException(SetupPage::class.java!!)
+            //throw IllegalArgumentException("Can't setup system, it's not empty.")
         }
         layout
                 .addTranslations("username", "password", "login.stayLoggedIn", "login.stayLoggedIn.tooltip")
