c599ec7bdb20afe1ad145157210d6580b8ddfcbd
Jan Brümmer <j.bruemmer@micromata.de>
2020-04-02 15:22:21 +0200

    Konto: Fix attribute name and add classics link

--- projectforge-rest/src/main/kotlin/org/projectforge/rest/dto/Konto.kt
+++ projectforge-rest/src/main/kotlin/org/projectforge/rest/dto/Konto.kt
@@ -29,7 +29,7 @@ import org.projectforge.business.fibu.KontoStatus
 class Konto(var nummer: Int? = null,
             var bezeichnung: String? = null,
             var description: String? = null,
-            var kontoStatus: KontoStatus? = null
+            var status: KontoStatus? = null
 ) : BaseDTODisplayObject<KontoDO>() {
     override fun copyFromMinimal(src: KontoDO) {
         super.copyFromMinimal(src)
