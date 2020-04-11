0aa301e589170b973bb99c33f19da8e89f68d390
Kai Reinhard <k.reinhard@micromata.de>
2020-04-11 11:03:34 +0200

    wip AttachmentService

--- projectforge-business/src/main/kotlin/org/projectforge/framework/jcr/AttachmentsService.kt
+++ projectforge-business/src/main/kotlin/org/projectforge/framework/jcr/AttachmentsService.kt
@@ -24,35 +24,34 @@
 package org.projectforge.framework.jcr
 
 import mu.KotlinLogging
-import org.projectforge.framework.persistence.api.BaseDO
+import org.projectforge.framework.persistence.api.IdObject
 import org.projectforge.jcr.FileObject
 import org.projectforge.jcr.RepoService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import java.io.ByteArrayInputStream
 import java.io.InputStream
 
 private val log = KotlinLogging.logger {}
 
 /**
- * Service for handling attachments of DO's.
+ * Service for handling attachments of DO's. It's possible to attach files to each [IdObject].
  */
 @Service
 open class AttachmentsService {
     @Autowired
     private lateinit var repoService: RepoService
 
-    open fun getAttachments(baseDO: BaseDO<*>, subPath: String? = null): List<Attachment>? {
-        return repoService.getFileInfos(getPath(baseDO), subPath)?.map { Attachment(it) }
+    open fun getAttachments(idObject: IdObject<*>, subPath: String? = null): List<Attachment>? {
+        return repoService.getFileInfos(getPath(idObject), subPath)?.map { Attachment(it) }
     }
 
-    open fun getAttachmentInfo(baseDO: BaseDO<*>, subPath: String? = null, id: String? = null, name: String? = null): Attachment? {
-        val fileObject = repoService.getFileInfo(getPath(baseDO), subPath, id = id, fileName = name) ?: return null
+    open fun getAttachmentInfo(idObject: IdObject<*>, subPath: String? = null, id: String? = null, name: String? = null): Attachment? {
+        val fileObject = repoService.getFileInfo(getPath(idObject), subPath, id = id, fileName = name) ?: return null
         return Attachment(fileObject)
     }
 
-    open fun getAttachmentContent(baseDO: BaseDO<*>, subPath: String? = null, id: String? = null, name: String? = null): ByteArray? {
-        val fileObject = repoService.getFileInfo(getPath(baseDO), subPath, id = id, fileName = name) ?: return null
+    open fun getAttachmentContent(idObject: IdObject<*>, subPath: String? = null, id: String? = null, name: String? = null): ByteArray? {
+        val fileObject = repoService.getFileInfo(getPath(idObject), subPath, id = id, fileName = name) ?: return null
         return if (repoService.retrieveFile(fileObject)) {
             fileObject.content
         } else {
@@ -60,34 +59,34 @@ open class AttachmentsService {
         }
     }
 
-    open fun getAttachmentInputStream(baseDO: BaseDO<*>, subPath: String? = null, id: String? = null, name: String? = null): InputStream? {
-        val fileObject = repoService.getFileInfo(getPath(baseDO), subPath, id = id, fileName = name) ?: return null
+    open fun getAttachmentInputStream(idObject: IdObject<*>, subPath: String? = null, id: String? = null, name: String? = null): InputStream? {
+        val fileObject = repoService.getFileInfo(getPath(idObject), subPath, id = id, fileName = name) ?: return null
         return repoService.retrieveFileInputStream(fileObject)
     }
 
-    open fun addAttachment(baseDO: BaseDO<*>, subPath: String? = null, name: String, content: ByteArray): Attachment {
-        val fileObject = FileObject(getPath(baseDO), subPath, fileName = name)
+    open fun addAttachment(idObject: IdObject<*>, subPath: String? = null, name: String, content: ByteArray): Attachment {
+        val fileObject = FileObject(getPath(idObject), subPath, fileName = name)
         fileObject.content = content
         return Attachment(fileObject)
 
     }
 
-    open fun addAttachment(baseDO: BaseDO<*>, subPath: String? = null, name: String, content: InputStream): Attachment {
-        val fileObject = FileObject(getPath(baseDO), subPath, fileName = name)
+    open fun addAttachment(idObject: IdObject<*>, subPath: String? = null, name: String, content: InputStream): Attachment {
+        val fileObject = FileObject(getPath(idObject), subPath, fileName = name)
         repoService.storeFile(fileObject, content)
         return Attachment(fileObject)
     }
 
-    open fun deleteAttachment(baseDO: BaseDO<*>, subPath: String? = null, id: String?): Boolean {
-        val fileObject = FileObject(getPath(baseDO), subPath, id = id)
+    open fun deleteAttachment(idObject: IdObject<*>, subPath: String? = null, id: String?): Boolean {
+        val fileObject = FileObject(getPath(idObject), subPath, id = id)
         return repoService.deleteFile(fileObject)
     }
 
     /**
-     * Path will be baseDO.classname/id.
+     * Path will be idObject.classname/id.
      * @return path relative to main node ProjectForge.
      */
-    open fun getPath(baseDO: BaseDO<*>): String {
-        return "${baseDO::class.java.name}/${baseDO.id}"
+    open fun getPath(idObject: IdObject<*>): String {
+        return "${idObject::class.java.name}/${idObject.id}"
     }
 }
