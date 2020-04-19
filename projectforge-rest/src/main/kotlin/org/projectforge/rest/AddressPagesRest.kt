2294cd8380167255f568a3fa6c9ac3cb0fb2956a
Kai Reinhard <k.reinhard@micromata.de>
2020-04-19 23:05:06 +0200

    AddressPagesRest: uses now star as favorite icon in table.

--- projectforge-rest/src/main/kotlin/org/projectforge/rest/AddressPagesRest.kt
+++ projectforge-rest/src/main/kotlin/org/projectforge/rest/AddressPagesRest.kt
@@ -208,10 +208,11 @@ class AddressPagesRest
                         .add(lc, "address.addressbookList"))
         layout.getTableColumnById("address.lastUpdate").formatter = Formatter.DATE
         layout.getTableColumnById("address.addressbookList").set(formatter = Formatter.ADDRESS_BOOK, sortable = false)
-        layout.getTableColumnById("address.isFavoriteCard").setStandardBoolean().set(
+        layout.getTableColumnById("address.isFavoriteCard").set(
                 sortable = false,
                 title = "address.columnHead.myFavorites",
                 tooltip = "address.filter.myFavorites")
+                .valueIconMap = mapOf(true to UIIconType.STAR_REGULAR)
         var menuIndex = 0
         if (configurationService.isTelephoneSystemUrlConfigured) {
             layout.add(MenuItem("address.phoneCall", i18nKey = "menu.phoneCall", url = "wa/phoneCall"), menuIndex++)
