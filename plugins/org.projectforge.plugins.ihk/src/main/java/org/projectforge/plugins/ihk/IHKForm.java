2e45d4b456d1248666856da5e8ed9a6fb2ddb6af
mnuhn <m.nuhn@micromata.de>
2020-05-28 10:43:44 +0200

    ihk plugin added logging/handling for null-values

--- plugins/org.projectforge.plugins.ihk/src/main/java/org/projectforge/plugins/ihk/IHKForm.java
+++ plugins/org.projectforge.plugins.ihk/src/main/java/org/projectforge/plugins/ihk/IHKForm.java
@@ -35,7 +35,6 @@ import org.projectforge.business.address.AddressDao;
 import org.projectforge.business.timesheet.TimesheetDO;
 import org.projectforge.framework.persistence.api.BaseSearchFilter;
 import org.projectforge.framework.persistence.user.api.ThreadLocalUserContext;
-import org.projectforge.framework.persistence.user.api.UserContext;
 import org.projectforge.framework.time.PFDay;
 import org.projectforge.framework.time.TimePeriod;
 import org.projectforge.web.CSSColor;
@@ -47,6 +46,7 @@ import org.projectforge.web.wicket.components.LocalDateModel;
 import org.projectforge.web.wicket.components.LocalDatePanel;
 import org.projectforge.web.wicket.components.SingleButtonPanel;
 import org.projectforge.web.wicket.flowlayout.*;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
@@ -55,192 +55,177 @@ import java.util.List;
 /**
  * Created by mnuhn on 05.12.2019
  */
-public class IHKForm extends AbstractStandardForm<Object, IHKPage>
-{
-  private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(IHKForm.class);
+public class IHKForm extends AbstractStandardForm<Object, IHKPage> {
+    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(IHKForm.class);
 
-  private TimePeriod timePeriod = new TimePeriod();
+    private TimePeriod timePeriod = new TimePeriod();
 
-  protected LocalDatePanel startDate;
+    protected LocalDatePanel startDate;
 
-  protected LocalDatePanel stopDate;
+    protected LocalDatePanel stopDate;
 
-  protected LocalDate ausbildungsStartDate;
+    protected LocalDate ausbildungsStartDate;
 
-  protected String teamName;
+    protected String teamName;
 
-  protected int ausbildungsJahr;
+    protected int ausbildungsJahr;
 
-  public IHKForm(IHKPage parentPage)
-  {
-    super(parentPage);
-  }
-
-  @SpringBean
-  private AddressDao addressDao;
-
-  @Override
-  protected void init()
-  {
-    super.init();
-
-    String userComment = "";
-    List<AddressDO> addressDos = addressDao.getList(new BaseSearchFilter());
-
-
-    for(AddressDO addressDo : addressDos){
-      if(addressDo.getName().equals(ThreadLocalUserContext.getUser().getLastname())){
-        if(addressDo.getFirstName().equals(ThreadLocalUserContext.getUser().getFirstname())){
-
-          userComment = addressDo.getComment();
-          break;
-
-        }
-      }
+    public IHKForm(IHKPage parentPage) {
+        super(parentPage);
     }
 
+    @SpringBean
+    private AddressDao addressDao;
 
-    Gson gson = new Gson();
-    IHKCommentObject ihkCommentObject = gson.fromJson(userComment,IHKCommentObject.class);
+    @Override
+    protected void init() {
+        super.init();
 
-    ausbildungsJahr = ihkCommentObject.getAusbildungsJahr();
-    teamName = ihkCommentObject.getTeam();
-    ausbildungsStartDate = LocalDate.parse(ihkCommentObject.getAusbildungStart());
+        String userComment = "";
+        List<AddressDO> addressDos = addressDao.getList(new BaseSearchFilter());
 
-    gridBuilder.newSplitPanel(GridSize.COL66);
-    final FieldsetPanel fs = gridBuilder.newFieldset(getString("timePeriod"));
 
-    FieldProperties<LocalDate> props = getFromDayProperties();
-    startDate = new LocalDatePanel(fs.newChildId(), new LocalDateModel(props.getModel()));
-    fs.add(startDate);
-    fs.setLabelFor(startDate);
-    fs.add(new DivTextPanel(fs.newChildId(), " - "));
-    props = getToDayProperties();
-    stopDate = new LocalDatePanel(fs.newChildId(), new LocalDateModel(props.getModel()));
-    fs.add(stopDate);
+        for (AddressDO addressDo : addressDos) {
+            if (addressDo.getName().equals(ThreadLocalUserContext.getUser().getLastname())) {
+                if (addressDo.getFirstName().equals(ThreadLocalUserContext.getUser().getFirstname())) {
+                    userComment = addressDo.getComment();
+                    break;
+                }
+            }
+        }
 
-    {
-      final SubmitLink unselectPeriodLink = new SubmitLink(IconLinkPanel.LINK_ID)
-      {
-        @Override
-        public void onSubmit()
+        if (!userComment.isEmpty()) {
+            IHKCommentObject ihkCommentObject;
+            try {
+                Gson gson = new Gson();
+                ihkCommentObject = gson.fromJson(userComment, IHKCommentObject.class);
+                ausbildungsJahr = ihkCommentObject.getAusbildungsJahr();
+                teamName = ihkCommentObject.getTeamName();
+                ausbildungsStartDate = LocalDate.parse(ihkCommentObject.getAusbildungStartDate());
+            } catch (Exception e) {
+                log.warn("ihk plugin wasnt able to parse json from AddressDo.getComment()");
+            }
+        }
+
+        gridBuilder.newSplitPanel(GridSize.COL66);
+        final FieldsetPanel fs = gridBuilder.newFieldset(getString("timePeriod"));
+
+        FieldProperties<LocalDate> props = getFromDayProperties();
+        startDate = new LocalDatePanel(fs.newChildId(), new LocalDateModel(props.getModel()));
+        fs.add(startDate);
+        fs.setLabelFor(startDate);
+        fs.add(new DivTextPanel(fs.newChildId(), " - "));
+        props = getToDayProperties();
+        stopDate = new LocalDatePanel(fs.newChildId(), new LocalDateModel(props.getModel()));
+        fs.add(stopDate);
+
         {
-          timePeriod.setFromDate(null);
-          timePeriod.setToDate(null);
-          clearInput();
+            final SubmitLink unselectPeriodLink = new SubmitLink(IconLinkPanel.LINK_ID) {
+                @Override
+                public void onSubmit() {
+                    timePeriod.setFromDate(null);
+                    timePeriod.setToDate(null);
+                    clearInput();
+                }
+            };
+            unselectPeriodLink.setDefaultFormProcessing(false);
+            fs.add(new IconLinkPanel(fs.newChildId(), IconType.REMOVE_SIGN,
+                    new ResourceModel("calendar.tooltip.unselectPeriod"),
+                    unselectPeriodLink).setColor(CSSColor.RED));
         }
-      };
-      unselectPeriodLink.setDefaultFormProcessing(false);
-      fs.add(new IconLinkPanel(fs.newChildId(), IconType.REMOVE_SIGN,
-          new ResourceModel("calendar.tooltip.unselectPeriod"),
-          unselectPeriodLink).setColor(CSSColor.RED));
+
+        final QuickSelectWeekPanel quickSelectWeekPanel = new QuickSelectWeekPanel(fs.newChildId(), new Model<LocalDate>() {
+            @Override
+            public LocalDate getObject() {
+                startDate.getDateField().validate();
+                return PFDay.fromOrNow(startDate.getDateField().getConvertedInput()).getLocalDate();
+            }
+        }, parentPage, "quickSelect" + ".week");
+        fs.add(quickSelectWeekPanel);
+        quickSelectWeekPanel.init();
+
+        fs.add(new DivTextPanel(fs.newChildId(), new Model<String>() {
+            @Override
+            public String getObject() {
+                return WicketUtils.getCalendarWeeks(IHKForm.this, timePeriod.getFromDate(), timePeriod.getToDate());
+            }
+        }));
+
+        Button downloadButton = new Button(SingleButtonPanel.WICKET_ID, new Model("download")) {
+            @Override
+            public void onSubmit() {
+                parentPage.export(false);
+            }
+        };
+        fs.add(new SingleButtonPanel(fs.newChildId(), downloadButton,
+                getString("plugins.ihk.download"), SingleButtonPanel.DEFAULT_SUBMIT));
     }
 
-    final QuickSelectWeekPanel quickSelectWeekPanel = new QuickSelectWeekPanel(fs.newChildId(), new Model<LocalDate>()
-    {
-      @Override
-      public LocalDate getObject()
-      {
-        startDate.getDateField().validate();
-        return PFDay.fromOrNow(startDate.getDateField().getConvertedInput()).getLocalDate();
-      }
-    }, parentPage, "quickSelect" + ".week");
-    fs.add(quickSelectWeekPanel);
-    quickSelectWeekPanel.init();
+    void showMissingDescriptionList(List<TimesheetDO> missingDescriptionList) {
+        gridBuilder.newSplitPanel(GridSize.COL66);
 
-    fs.add(new DivTextPanel(fs.newChildId(), new Model<String>()
-    {
-      @Override
-      public String getObject()
-      {
-        return WicketUtils.getCalendarWeeks(IHKForm.this, timePeriod.getFromDate(), timePeriod.getToDate());
-      }
-    }));
+        DateFormat date = new SimpleDateFormat("dd.MM.yyyy");
+        DateFormat time = new SimpleDateFormat("HH:mm");
 
-    Button downloadButton = new Button(SingleButtonPanel.WICKET_ID, new Model("download"))
-    {
-      @Override
-      public void onSubmit()
-      {
-        parentPage.export(false);
-      }
-    };
-    fs.add(new SingleButtonPanel(fs.newChildId(), downloadButton,
-        getString("plugins.ihk.download"), SingleButtonPanel.DEFAULT_SUBMIT));
-  }
+        for (TimesheetDO ts : missingDescriptionList) {
 
-  void showMissingDescriptionList(List<TimesheetDO> missingDescriptionList) {
-    gridBuilder.newSplitPanel(GridSize.COL66);
+            String label = ts.getKost2().getDescription() != null ? ts.getKost2().getDescription() : ts.getKost2().getDisplayName();
+            final FieldsetPanel fs = gridBuilder.newFieldset(label);
+            fs.add(new DivTextPanel(fs.newChildId(),
+                    date.format(ts.getStartTime().getTime()) + " " +
+                            time.format(ts.getStartTime().getTime()) + " - " +
+                            time.format(ts.getStopTime().getTime())));
 
-    DateFormat date = new SimpleDateFormat("dd.MM.yyyy");
-    DateFormat time = new SimpleDateFormat("HH:mm");
-
-    for (TimesheetDO ts : missingDescriptionList) {
-
-      String label = ts.getKost2().getDescription() != null ? ts.getKost2().getDescription() : ts.getKost2().getDisplayName();
-      final FieldsetPanel fs = gridBuilder.newFieldset(label);
-      fs.add(new DivTextPanel(fs.newChildId(),
-              date.format(ts.getStartTime().getTime()) + " " +
-              time.format(ts.getStartTime().getTime()) + " - " +
-              time.format(ts.getStopTime().getTime())));
-
-      Button editButton = new Button(SingleButtonPanel.WICKET_ID, new Model("edit")) {
-        @Override
-        public void onSubmit() {
-          parentPage.edit(ts);
+            Button editButton = new Button(SingleButtonPanel.WICKET_ID, new Model("edit")) {
+                @Override
+                public void onSubmit() {
+                    parentPage.edit(ts);
+                }
+            };
+            fs.add(new SingleButtonPanel(fs.newChildId(), editButton,
+                    getString("plugins.ihk.edit"), SingleButtonPanel.CANCEL));
         }
-      };
-      fs.add(new SingleButtonPanel(fs.newChildId(), editButton,
-              getString("plugins.ihk.edit"), SingleButtonPanel.CANCEL));
+
+        // Download anyway button
+        final FieldsetPanel downloadPanel = gridBuilder.newFieldset("");
+        Button downloadButton = new Button(SingleButtonPanel.WICKET_ID, new Model("download")) {
+            @Override
+            public void onSubmit() {
+                parentPage.export(true);
+            }
+        };
+        downloadPanel.add(new SingleButtonPanel(downloadPanel.newChildId(), downloadButton,
+                getString("plugins.ihk.downloadAnyway"), SingleButtonPanel.DEFAULT_SUBMIT));
+
+
     }
 
-    // Download anyway button
-    final FieldsetPanel downloadPanel = gridBuilder.newFieldset("");
-    Button downloadButton = new Button(SingleButtonPanel.WICKET_ID, new Model("download"))
-    {
-      @Override
-      public void onSubmit()
-      {
-        parentPage.export(true);
-      }
-    };
-    downloadPanel.add(new SingleButtonPanel(downloadPanel.newChildId(), downloadButton,
-            getString("plugins.ihk.downloadAnyway"), SingleButtonPanel.DEFAULT_SUBMIT));
+    private FieldProperties<LocalDate> getToDayProperties() {
+        return new FieldProperties<>("", new PropertyModel<>(timePeriod, "fromDay"));
+    }
 
+    private FieldProperties<LocalDate> getFromDayProperties() {
+        return new FieldProperties<>("", new PropertyModel<>(timePeriod, "toDay"));
+    }
 
-  }
+    public TimePeriod getTimePeriod() {
+        return timePeriod;
+    }
 
-  private FieldProperties<LocalDate> getToDayProperties() {
-    return new FieldProperties<>("", new PropertyModel<>(timePeriod, "fromDay"));
-  }
+    public LocalDatePanel getStartDate() {
+        return startDate;
+    }
 
-  private FieldProperties<LocalDate> getFromDayProperties() {
-    return new FieldProperties<>("", new PropertyModel<>(timePeriod, "toDay"));
-  }
+    public LocalDate getAusbildungsStartDate() {
+        return ausbildungsStartDate;
+    }
 
-  public TimePeriod getTimePeriod()
-  {
-    return timePeriod;
-  }
+    public int getAusbildungsJahr() {
+        return ausbildungsJahr;
+    }
 
-  public LocalDatePanel getStartDate()
-  {
-    return startDate;
-  }
-
-  public LocalDate getAusbildungsStartDate()
-  {
-    return ausbildungsStartDate;
-  }
-
-  public int getAusbildungsJahr()
-  {
-    return ausbildungsJahr;
-  }
-
-  public String getTeamName()
-  {
-    return teamName;
-  }
+    public String getTeamName() {
+        return teamName;
+    }
 
 }
