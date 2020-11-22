b910d5bce7ba6f38c821fd24922180b6647f336f
Kai Reinhard <k.reinhard@micromata.de>
2020-11-22 21:59:26 +0100

    AuthenticationPublicServicesRest: First version.

---
+++ projectforge-rest/src/test/kotlin/org/projectforge/rest/pub/AuthenticationPublicServicesRestTest.kt
@@ -0,0 +1,107 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2020 Micromata GmbH, Germany (www.micromata.com)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.rest.pub
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.fail
+import org.projectforge.business.user.UserAuthenticationsDao
+import org.projectforge.business.user.UserTokenType
+import org.projectforge.framework.utils.Crypt
+import org.projectforge.test.AbstractTestBase
+import org.springframework.beans.factory.annotation.Autowired
+
+class AuthenticationPublicServicesRestTest : AbstractTestBase() {
+    @Autowired
+    private lateinit var authenticationPublicServicesRest: AuthenticationPublicServicesRest
+
+    @Autowired
+    private lateinit var userAuthenticationsDao: UserAuthenticationsDao
+
+    @Test
+    fun getAuthenticationCredentialsTest() {
+        val user = logon(TEST_USER)
+        val q = authenticationPublicServicesRest.createQueryParam(user.id)
+        val credentials = authenticationPublicServicesRest.getAuthenticationCredentials(q, user.id)
+        assertEquals(user.username, credentials.username)
+        assertEquals(user.id, credentials.uid)
+        val token = userAuthenticationsDao.getToken(user.id, UserTokenType.REST_CLIENT)
+        assertEquals(token, credentials.authenticationToken)
+        assertEquals("http://localhost:8080", credentials.url)
+
+        try {
+            authenticationPublicServicesRest.getAuthenticationCredentials(q, 671237617)
+            fail("Failure expected (unknown uid)")
+        } catch (ex: Exception) {
+            // expected
+        }
+
+        try {
+            authenticationPublicServicesRest.getAuthenticationCredentials(q, getUserId(TEST_ADMIN_USER))
+            fail("Failure expected (different uid)")
+        } catch (ex: Exception) {
+            // expected
+        }
+
+        try {
+            authenticationPublicServicesRest.getAuthenticationCredentials("OXn1Nq1T7qZUkOCHEdp3LB", user.id)
+            fail("Failure expected, invalid q")
+        } catch (ex: Exception) {
+            // expected
+        }
+    }
+
+    @Test
+    fun paramCheckTest() {
+        logon(TEST_USER)
+        val uid = getUserId(TEST_USER)
+        paramCheck(uid, -1000, true, "1 second ago should be valid.")
+        paramCheck(uid, -50 * 1000, true, "1 minute ago should be valid.")
+        paramCheck(uid, -61 * 1000, false, "more than 1 minute ago should be invalid.")
+        paramCheck(uid, +1000, false, "Any time in the future should be invalid.")
+        try {
+            authenticationPublicServicesRest.checkQuery("OXn1Nq1T7qZUkOCHEdp3LB", uid)
+            fail("Invalid param q. Failure was expected.")
+        } catch (ex: Exception) {
+            // excepted
+        }
+        val token = userAuthenticationsDao.getToken(uid, UserTokenType.REST_CLIENT)
+        try {
+            authenticationPublicServicesRest.checkQuery(Crypt.encrypt(token, "nonumber"), uid)
+            fail("Invalid system time im millis. Failure was expected.")
+        } catch (ex: Exception) {
+            // excepted
+        }
+    }
+
+    private fun paramCheck(uid: Int, timeOffset: Long, expectedResult: Boolean, msg: String) {
+        val q = authenticationPublicServicesRest.createQueryParam(uid, System.currentTimeMillis() + timeOffset)
+        try {
+            authenticationPublicServicesRest.checkQuery(q, uid)
+            assertTrue(expectedResult, "Oups, check failure expected: $msg")
+        } catch (ex: Exception) {
+            assertFalse(expectedResult, "Oups, check failure not expected: $msg")
+        }
+    }
+}
