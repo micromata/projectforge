e4ff698b90fb0cf4d2ff8d7b59331e4c7adec583
Kai Reinhard <k.reinhard@micromata.de>
2021-04-20 22:08:59 +0200

    Configuration params implemented: projectforge.jcr.cron.purgeBackupKeepDailyBackups=10, projectforge.cron.purgeBackupKeepDailyBackups=20

--- projectforge-business/src/main/kotlin/org/projectforge/framework/persistence/database/DatabaseBackupPurgeJob.kt
+++ projectforge-business/src/main/kotlin/org/projectforge/framework/persistence/database/DatabaseBackupPurgeJob.kt
@@ -43,6 +43,9 @@ class DatabaseBackupPurgeJob {
     @Value("\${projectforge.cron.purgeBackupFilesPrefix}")
     private val dbBackupFilesPrefix: String? = null
 
+    @Value("\${projectforge.cron.purgeBackupKeepDailyBackups}")
+    private val dbBackupKeepDailyBackups: Long? = null
+
     // projectforge.cron.dbBackupCleanup=0 40 0 * * *
     @Scheduled(cron = "\${projectforge.cron.purgeBackup}")
     fun execute() {
@@ -56,6 +59,6 @@ class DatabaseBackupPurgeJob {
             return
         }
         log.info { "Starting job for cleaning daily backup files older than 30 days, but monthly backups will be kept." }
-        BackupFilesPurging.purgeDirectory(backupDir, filePrefix = dbBackupFilesPrefix)
+        BackupFilesPurging.purgeDirectory(backupDir, filePrefix = dbBackupFilesPrefix, keepDailyBackups = dbBackupKeepDailyBackups ?: 30)
     }
 }
