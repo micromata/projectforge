a1b2deb4b0c72b8fcc3ccc74a0a28ec5209bc727
Kai Reinhard <k.reinhard@micromata.de>
2021-05-03 08:06:21 +0200

    stopProjectForge.sh: fine-tuning.

--- doc/misc/stopProjectForge.sh
+++ doc/misc/stopProjectForge.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 
-JAVA_MAIN="projectforge-application"
+PROCESS_IDENTIFIER="projectforge-application"
 APP_NAME="ProjectForge"
 
 checkStopped() {
-  pid=$(pgrep -f $JAVA_MAIN)
+  pid=$(pgrep -f $PROCESS_IDENTIFIER)
   if [[ -z $pid ]]; then
     echo "${APP_NAME} $1"
     exit 0
   fi
+  if [[ -n $2 ]]; then
+    echo "${APP_NAME} $2"
+  fi
 }
 
-checkStopped "process not found (already terminated?)."
+checkStopped "process not found (already terminated?)"
 
 echo "Sending shutdown signal to $APP_NAME..."
 kill $pid
 
-# Loop 10 times a 3 seconds to wait for ProjectForge's shutdown:
+# Loop 20 times a 3 seconds to wait for ProjectForge's shutdown:
 for run in {1..20}; do
   echo "waiting 3 sec for termination of pid $pid..."
   sleep 3
   checkStopped "successfully stopped."
 done
 
-echo "${APP_NAME} not stopped, sending sigkill now..."
+checkStopped "successfully stopped." "not stopped, sending sigkill now..."
 kill -9 $pid
 
 sleep 2
 
-checkStopped "killed."
+checkStopped "killed." "cannot be killed?!"
