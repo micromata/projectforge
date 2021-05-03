9f3c11aae77efec0972e6be5c45dfe0d4799043e
Kai Reinhard <k.reinhard@micromata.de>
2021-05-03 03:13:33 +0200

    linting disabled for files in progress.

--- projectforge-webapp/src/components/base/dynamicLayout/components/customized/components/OutgoingInvoicePositionsComponent.jsx
+++ projectforge-webapp/src/components/base/dynamicLayout/components/customized/components/OutgoingInvoicePositionsComponent.jsx
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import { Button, UncontrolledCollapse } from 'reactstrap';
 import { DynamicLayoutContext } from '../../../context';
 
+/* eslint-disable */
+
 function OutgoingInvoicePositionsComponent() {
     const { data, callAction } = React.useContext(DynamicLayoutContext);
 
