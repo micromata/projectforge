2e3806fd5bf6d1e5fee14948897bf828c0677754
Kai Reinhard <k.reinhard@micromata.de>
2021-05-13 13:01:56 +0200

    TimeAgo: Code clean up.

--- projectforge-business/src/main/kotlin/org/projectforge/framework/i18n/TimeAgo.kt
+++ projectforge-business/src/main/kotlin/org/projectforge/framework/i18n/TimeAgo.kt
@@ -27,51 +27,57 @@ import java.util.*
 
 /**
  * Time ago builds human readable localized strings for time ago events, such as: a few seconds ago, a minute ago, 19 minutes ago, an hour ago, ...
+ *
+ * Used i18n properties:
+ * * timeago.afewseconds=a few seconds ago
+ * * timeago.aminute=a minute ago
+ * * timeago.amonth=a month ago
+ * * timeago.anhour=an hour ago
+ * * timeago.aweek=a week ago
+ * * timeago.ayear=a year ago
+ * * timeago.days={0} days ago
+ * * timeago.hours={0} hours ago
+ * * timeago.inthefuture=in the future!
+ * * timeago.minutes={0} minutes ago
+ * * timeago.months={0} months ago
+ * * timeago.weeks={0} weeks ago
+ * * timeago.years={0} years ago
+ * * timeago.yesterday=yesterday
  */
 object TimeAgo {
-    const val MINUTE = 60
-    const val HOUR = 60 * MINUTE
-    const val DAY = 24 * HOUR
-    const val WEEK = 7 * DAY
-    const val MONTH = 30 * DAY
-    const val YEAR = 365 * DAY
+  const val MINUTE = 60
+  const val HOUR = 60 * MINUTE
+  const val DAY = 24 * HOUR
+  const val WEEK = 7 * DAY
+  const val MONTH = 30 * DAY
+  const val YEAR = 365 * DAY
 
-    fun getI18nKey(date: Date): Pair<String, Long> {
-        val seconds = (System.currentTimeMillis() - date.time) / 1000;
-        if (seconds < 0)
-            return Pair("timeago.inthefuture", -1)
-        else if (seconds > 2 * YEAR)
-            return Pair("timeago.years", seconds / YEAR)
-        else if (seconds > YEAR)
-            return Pair("timeago.ayear", -1)
-        else if (seconds > 40 * DAY)
-            return Pair("timeago.months", seconds / MONTH)
-        else if (seconds > MONTH)
-            return Pair("timeago.amonth", -1)
-        else if (seconds > 2 * WEEK)
-            return Pair("timeago.weeks", seconds / WEEK)
-        else if (seconds > WEEK)
-            return Pair("timeago.aweek", -1)
-        else if (seconds > 2 * DAY)
-            return Pair("timeago.days", seconds / DAY)
-        else if (seconds > DAY)
-            return Pair("timeago.yesterday", -1)
-        else if (seconds > 2 * HOUR)
-            return Pair("timeago.hours", seconds / HOUR)
-        else if (seconds > HOUR)
-            return Pair("timeago.anhour", -1)
-        else if (seconds > 2 * MINUTE)
-            return Pair("timeago.minutes", seconds / MINUTE)
-        else if (seconds > MINUTE)
-            return Pair("timeago.aminute", -1)
-        return Pair("timeago.afewseconds", -1)
+  fun getI18nKey(date: Date): Pair<String, Long> {
+    val seconds = (System.currentTimeMillis() - date.time) / 1000
+    when {
+      seconds < 0 -> return Pair("timeago.inthefuture", -1)
+      seconds > 2 * YEAR -> return Pair("timeago.years", seconds / YEAR)
+      seconds > YEAR -> return Pair("timeago.ayear", -1)
+      seconds > 40 * DAY -> return Pair("timeago.months", seconds / MONTH)
+      seconds > MONTH -> return Pair("timeago.amonth", -1)
+      seconds > 2 * WEEK -> return Pair("timeago.weeks", seconds / WEEK)
+      seconds > WEEK -> return Pair("timeago.aweek", -1)
+      seconds > 2 * DAY -> return Pair("timeago.days", seconds / DAY)
+      seconds > DAY -> return Pair("timeago.yesterday", -1)
+      seconds > 2 * HOUR -> return Pair("timeago.hours", seconds / HOUR)
+      seconds > HOUR -> return Pair("timeago.anhour", -1)
+      seconds > 2 * MINUTE -> return Pair("timeago.minutes", seconds / MINUTE)
+      seconds > MINUTE -> return Pair("timeago.aminute", -1)
+      else -> return Pair("timeago.afewseconds", -1)
     }
+  }
 
-    fun getMessage(date: Date?, locale: Locale? = null): String {
-        date ?: return ""
-        val pair = getI18nKey(date)
-        if (pair.second < 0)
-            return translate(locale, pair.first)
-        return translateMsg(locale, pair.first, pair.second)
-    }
+  fun getMessage(date: Date?, locale: Locale? = null): String {
+    date ?: return ""
+    val pair = getI18nKey(date)
+    return if (pair.second < 0)
+      translate(locale, pair.first)
+    else
+      translateMsg(locale, pair.first, pair.second)
+  }
 }
