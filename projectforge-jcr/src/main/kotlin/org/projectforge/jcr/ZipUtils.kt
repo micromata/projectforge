1576f6b64bea30c5311c78f2b96cdc8c2a87ef27
Kai Reinhard <k.reinhard@micromata.de>
2021-05-21 00:00:56 +0200

    ZipUtils prepared for encrypted zips.

---
+++ projectforge-jcr/src/main/kotlin/org/projectforge/jcr/ZipUtils.kt
@@ -0,0 +1,80 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2021 Micromata GmbH, Germany (www.micromata.com)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.jcr
+
+import net.lingala.zip4j.exception.ZipException
+import net.lingala.zip4j.io.inputstream.ZipInputStream
+import net.lingala.zip4j.io.outputstream.ZipOutputStream
+import net.lingala.zip4j.model.ZipParameters
+import net.lingala.zip4j.model.enums.AesKeyStrength
+import net.lingala.zip4j.model.enums.EncryptionMethod
+import java.io.InputStream
+import java.io.OutputStream
+
+object ZipUtils {
+  @Suppress("unused")
+  enum class EncryptionMode { ZIP_STANDARD, AES128, AES256 }
+
+  @JvmStatic
+  fun isEncrypted(inputStream: InputStream): Boolean {
+    try {
+      ZipInputStream(inputStream).nextEntry // nextEntry.isEncrypted doesn't work.
+      return false
+    } catch (ex: ZipException) {
+      return ex.type == ZipException.Type.WRONG_PASSWORD
+    }
+  }
+
+  @JvmStatic
+  @JvmOverloads
+  fun encryptZipFile(
+    fileName: String,
+    password: String,
+    inputStream: InputStream,
+    outputStream: OutputStream,
+    mode: EncryptionMode = EncryptionMode.ZIP_STANDARD,
+  ) {
+    val zipParameters = ZipParameters()
+    zipParameters.isEncryptFiles = true
+    if (mode == EncryptionMode.ZIP_STANDARD) {
+      zipParameters.encryptionMethod = EncryptionMethod.ZIP_STANDARD
+    } else {
+      zipParameters.encryptionMethod = EncryptionMethod.AES
+      zipParameters.aesKeyStrength = if (mode == EncryptionMode.AES128) {
+        AesKeyStrength.KEY_STRENGTH_128
+      } else {
+        AesKeyStrength.KEY_STRENGTH_256
+      }
+    }
+    val zipOutputStream = ZipOutputStream(outputStream, password.toCharArray())
+    zipOutputStream.use { zipOut ->
+      zipParameters.fileNameInZip = fileName
+      zipOut.putNextEntry(zipParameters)
+      inputStream.use {
+        it.copyTo(zipOut)
+      }
+      zipOut.closeEntry()
+    }
+  }
+}
---
+++ projectforge-jcr/src/test/kotlin/org/projectforge/jcr/ZipUtilsTest.kt
@@ -0,0 +1,65 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2021 Micromata GmbH, Germany (www.micromata.com)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.jcr
+
+import net.lingala.zip4j.io.inputstream.ZipInputStream
+import net.lingala.zip4j.model.LocalFileHeader
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.projectforge.test.TestUtils
+import java.io.*
+
+class ZipUtilsTest {
+  private var testUtils = TestUtils(MODULE_NAME)
+
+  @Test
+  fun encryptionTest() {
+    val testDir = testUtils.deleteAndCreateTestFile("zipTests")
+    testDir.mkdirs()
+    val istream = FileInputStream("pom.xml")
+    val zipFile = File(testDir, "pom.zip")
+    val outputStream = FileOutputStream(zipFile)
+    outputStream.use {
+      ZipUtils.encryptZipFile("pom.xml", "test123", istream, it)
+    }
+    val content = File("pom.xml").readBytes()
+    Assertions.assertTrue(ZipUtils.isEncrypted(FileInputStream(zipFile)))
+    decryptZipFile("test123", FileInputStream(zipFile), "pom.xml", content)
+  }
+
+  internal fun decryptZipFile(password: String, inputStream: InputStream, expectedFileName: String, expectedContent: ByteArray) {
+    var localFileHeader: LocalFileHeader?
+    ZipInputStream(inputStream, password.toCharArray()).use { zipInputStream ->
+      while (zipInputStream.nextEntry.also { localFileHeader = it } != null) {
+        Assertions.assertEquals(expectedFileName, localFileHeader!!.fileName)
+        val baos = ByteArrayOutputStream()
+        baos.use { out ->
+          zipInputStream.copyTo(out)
+        }
+        Assertions.assertArrayEquals(expectedContent, baos.toByteArray())
+      }
+    }
+  }
+
+}
