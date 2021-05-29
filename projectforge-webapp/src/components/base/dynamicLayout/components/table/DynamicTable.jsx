e4972cd468b32896bd26d640f942a035f40563a6
Kai Reinhard <k.reinhard@micromata.de>
2021-05-29 13:34:14 +0200

    LogViewer finished. DynamicTable supports now auto-refresh.

--- projectforge-webapp/src/components/base/dynamicLayout/components/table/DynamicTable.jsx
+++ projectforge-webapp/src/components/base/dynamicLayout/components/table/DynamicTable.jsx
@@ -1,14 +1,49 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import useInterval from '@use-hooks/interval';
 import { Table } from '../../../../design';
 import { DynamicLayoutContext } from '../../context';
+import { fetchJsonPost } from '../../../../../utilities/rest';
 import DynamicTableHead from './DynamicTableHead';
 import DynamicTableRow from './DynamicTableRow';
 
-function DynamicTable({ columns, id }) {
-    const { data, ui, variables } = React.useContext(DynamicLayoutContext);
+function DynamicTable(
+    {
+        columns,
+        id,
+        refreshUrl,
+        refreshIntervalSeconds,
+        autoRefreshFlag,
+    },
+) {
+    const {
+        data,
+        ui,
+        variables,
+        setVariables,
+    } = React.useContext(DynamicLayoutContext);
 
     const entries = Object.getByString(data, id) || Object.getByString(variables, id) || '';
+
+    /*
+      If automatic refresh is required, the table content must be given as variable in variables.
+      Reason: If given in data, the table content would be posted by every refresh.
+     */
+    if (refreshUrl) {
+        useInterval(() => {
+            if (!autoRefreshFlag || Object.getByString(data, autoRefreshFlag)) {
+                fetchJsonPost(
+                    refreshUrl,
+                    {
+                        data,
+                    },
+                    (json) => {
+                        setVariables({ [id]: json });
+                    },
+                );
+            }
+        }, refreshIntervalSeconds * 1000);
+    }
 
     return React.useMemo(() => (
         <div
@@ -55,10 +90,16 @@ DynamicTable.propTypes = {
         titleIcon: PropTypes.arrayOf(PropTypes.string),
     })).isRequired,
     id: PropTypes.string,
+    refreshUrl: PropTypes.string,
+    refreshIntervalSeconds: PropTypes.number,
+    autoRefreshFlag: PropTypes.string,
 };
 
 DynamicTable.defaultProps = {
     id: undefined,
+    refreshUrl: undefined,
+    refreshIntervalSeconds: 10, // 10 seconds as default intervall.
+    autoRefreshFlag: undefined,
 };
 
 export default DynamicTable;
