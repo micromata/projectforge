3cb8f7f9cf4918d8c1180d672a0e426803aa3011
Kai Reinhard <k.reinhard@micromata.de>
2021-05-31 08:51:40 +0200

    TimeLeft added (uses TimeAgo and vice versa).

--- projectforge-business/src/test/kotlin/org/projectforge/framework/i18n/TimeAgoLeftTest.kt
+++ projectforge-business/src/test/kotlin/org/projectforge/framework/i18n/TimeAgoLeftTest.kt
@@ -26,40 +26,50 @@ package org.projectforge.web
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.projectforge.framework.i18n.TimeAgo
+import org.projectforge.framework.i18n.TimeLeft
 import java.util.*
 
 private const val HOUR = 3600L
 private const val DAY = 24 * HOUR
 private const val MONTH = 30 * DAY
 
-class TimeAgoTest {
+class TimeAgoLeftTest {
 
-    @Test
-    fun i18nTest() {
-        timeAgo(-100, "negative", -1)
-        timeAgo(-125, "minutes", 2, true)
-        timeAgo(20, "afewseconds", -1)
-        timeAgo(59, "afewseconds", -1)
-        timeAgo(65, "aminute", -1)
-        timeAgo(125, "minutes", 2)
+  @Test
+  fun i18nTest() {
+    timeAgoLeft(-100, "negative", -1)
+    timeAgoLeft(-125, "minutes", 2, true)
+    timeAgoLeft(20, "afewseconds", -1)
+    timeAgoLeft(59, "afewseconds", -1)
+    timeAgoLeft(65, "aminute", -1)
+    timeAgoLeft(125, "minutes", 2)
 
-        timeAgo(HOUR, "anhour", -1)
-        timeAgo(2 * HOUR, "hours", 2)
-        timeAgo(DAY, "day", -1)
-        timeAgo(2 * DAY, "days", 2)
-        timeAgo(7 * DAY, "aweek", -1)
-        timeAgo(14 * DAY, "weeks", 2)
-        timeAgo(31 * DAY, "amonth", -1)
+    timeAgoLeft(HOUR, "anhour", -1)
+    timeAgoLeft(2 * HOUR, "hours", 2)
+    timeAgoLeft(DAY, "day", -1)
+    timeAgoLeft(2 * DAY, "days", 2)
+    timeAgoLeft(7 * DAY, "aweek", -1)
+    timeAgoLeft(14 * DAY, "weeks", 2)
+    timeAgoLeft(31 * DAY, "amonth", -1)
 
-        timeAgo(2 *  MONTH, "months", 2)
-        timeAgo(11 * MONTH, "months", 11)
-        timeAgo(365 * DAY, "ayear", -1)
-        timeAgo(25 * MONTH, "years", 2)
-    }
+    timeAgoLeft(2 * MONTH, "months", 2)
+    timeAgoLeft(11 * MONTH, "months", 11)
+    timeAgoLeft(365 * DAY, "ayear", -1)
+    timeAgoLeft(25 * MONTH, "years", 2)
+  }
 
-    private fun timeAgo(secondsOffset: Long, expectedI18nKey: String, expectedCounter: Long, allowFutureTimes: Boolean = false) {
-        val pair = TimeAgo.getI18nKey(Date(System.currentTimeMillis() - secondsOffset * 1000 - 1000), allowFutureTimes)
-        Assertions.assertEquals(expectedI18nKey, pair.first)
-        Assertions.assertEquals(expectedCounter, pair.second)
-    }
+  private fun timeAgoLeft(
+    secondsOffset: Long,
+    expectedI18nKey: String,
+    expectedCounter: Long,
+    allowNegativeTimes: Boolean = false
+  ) {
+    var pair = TimeAgo.getI18nKey(Date(System.currentTimeMillis() - secondsOffset * 1000 - 1000), allowNegativeTimes)
+    Assertions.assertEquals(expectedI18nKey, pair.first)
+    Assertions.assertEquals(expectedCounter, pair.second)
+
+    pair = TimeLeft.getI18nKey(Date(System.currentTimeMillis() + secondsOffset * 1000 + 1000), allowNegativeTimes)
+    Assertions.assertEquals(expectedI18nKey, pair.first)
+    Assertions.assertEquals(expectedCounter, pair.second)
+  }
 }
