77d22d685339c4449505bf7ed568ae1bf6bf349e
Kai Reinhard <k.reinhard@micromata.de>
2021-08-18 10:59:46 +0200

    Security: LoggingFilter logs now suspicious urls at default in logs/access.log (using Rolling file appender).

--- projectforge-caldav/src/main/kotlin/org/projectforge/caldav/config/DAVMethodsInterceptor.kt
+++ projectforge-caldav/src/main/kotlin/org/projectforge/caldav/config/DAVMethodsInterceptor.kt
@@ -24,8 +24,6 @@
 package org.projectforge.caldav.config
 
 import mu.KotlinLogging
-import org.projectforge.business.user.UserGroupCache
-import org.slf4j.LoggerFactory
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
@@ -38,36 +36,38 @@ private val log = KotlinLogging.logger {}
  * Enables http methods, required by DAV functionality such as "PROPFIND" etc.
  */
 class DAVMethodsInterceptor : HandlerInterceptorAdapter() {
-    /**
-     * Needed to add support for DAV methods, such as "PROPFIND" etc. Otherwise Spring reject such http methods.
-     */
-    override fun preHandle(request: HttpServletRequest,
-                           response: HttpServletResponse, handler: Any): Boolean {
-        return !handledByMiltonFilter(request) // Support PROPFIND, PROPPATCH etc.
-    }
+  /**
+   * Needed to add support for DAV methods, such as "PROPFIND" etc. Otherwise Spring reject such http methods.
+   */
+  override fun preHandle(
+    request: HttpServletRequest,
+    response: HttpServletResponse, handler: Any
+  ): Boolean {
+    return !handledByMiltonFilter(request) // Support PROPFIND, PROPPATCH etc.
+  }
 
-    companion object {
-        /**
-         * @return true if [PFMiltonInit.available] and given method is in list "PROPFIND", "PROPPATCH", "OPTIONS" (with /users/...), "REPORT".
-         * Otherwise false.
-         */
-        internal fun handledByMiltonFilter(request: HttpServletRequest): Boolean {
-            if (!PFMiltonInit.available) {
-                return false
-            }
-            val method = request.method
-            if (method == "PROPFIND") {
-                val uri = request.requestURI
-                log.info("PROPFIND call detected: $uri")
-                // All PROPFIND's will be handled by Milton.
-                return true
-            }
-            if (METHODS.contains(method)) {
-                val uri = request.requestURI
-                // We may add uri pattern later ("/", "/principals/*", "/users/*").
-                return uri.matches("/+users.*".toRegex())
-            }
-            return false
-        }
+  companion object {
+    /**
+     * @return true if [PFMiltonInit.available] and given method is in list "PROPFIND", "PROPPATCH", "OPTIONS" (with /users/...), "REPORT".
+     * Otherwise false.
+     */
+    fun handledByMiltonFilter(request: HttpServletRequest): Boolean {
+      if (!PFMiltonInit.available) {
+        return false
+      }
+      val method = request.method
+      if (method == "PROPFIND") {
+        val uri = request.requestURI
+        log.info("PROPFIND call detected: $uri")
+        // All PROPFIND's will be handled by Milton.
+        return true
+      }
+      if (METHODS.contains(method)) {
+        val uri = request.requestURI
+        // We may add uri pattern later ("/", "/principals/*", "/users/*").
+        return uri.matches("/+users.*".toRegex())
+      }
+      return false
     }
+  }
 }
