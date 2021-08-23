a87c9550b00b749ec128ac0e0dd37676e58a85f4
Kai Reinhard <k.reinhard@micromata.de>
2021-08-23 23:20:51 +0200

    WIP: 2FA

--- projectforge-business/src/main/kotlin/org/projectforge/security/TwoFactorAuthenticationConfiguration.kt
+++ projectforge-business/src/main/kotlin/org/projectforge/security/TwoFactorAuthenticationConfiguration.kt
@@ -3,7 +3,7 @@
 // Project ProjectForge Community Edition
 //         www.projectforge.org
 //
-// Copyright (C) 2001-2021 Micromata GmbH; Germany (www.micromata.com)
+// Copyright (C) 2001-2021 Micromata GmbH, Germany (www.micromata.com)
 //
 // ProjectForge is dual-licensed.
 //
@@ -11,13 +11,13 @@
 // modify it under the terms of the GNU General Public License as published
 // by the Free Software Foundation; version 3 of the License.
 //
-// This community edition is distributed in the hope that it will be useful;
+// This community edition is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
 // Public License for more details.
 //
 // You should have received a copy of the GNU General Public License along
-// with this program; if not; see http://www.gnu.org/licenses/.
+// with this program; if not, see http://www.gnu.org/licenses/.
 //
 /////////////////////////////////////////////////////////////////////////////
 
