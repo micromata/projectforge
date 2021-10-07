27f8f831555558adae0a9970fc9dd440a0d17703
Fin Reinhard <f.reinhard@micromata.de>
2021-10-07 16:20:56 +0000

    fix category navigation safari 15 bug

--- projectforge-webapp/src/components/base/navigation/categories-dropdown/index.jsx
+++ projectforge-webapp/src/components/base/navigation/categories-dropdown/index.jsx
@@ -2,8 +2,11 @@ import { faListUl } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import PropTypes from 'prop-types';
 import React from 'react';
+import { NavItem, NavLink, Nav, Button } from 'reactstrap';
+import { Manager, Reference, Popper } from 'react-popper';
+import classNames from 'classnames';
 import { menuItemPropType } from '../../../../utilities/propTypes';
-import { Container, Dropdown, DropdownMenu, DropdownToggle } from '../../../design';
+import { Container } from '../../../design';
 import style from '../Navigation.module.scss';
 import Category from './Category';
 import MenuBadge from './MenuBadge';
@@ -41,41 +44,69 @@ function CategoriesDropdown({ badge, categories }) {
             .push(category));
 
     return (
-        <Dropdown isOpen={open} toggle={() => setOpen(!open)}>
-            <DropdownToggle nav caret>
-                <FontAwesomeIcon icon={faListUl} />
-                {badge && (
-                    <MenuBadge
-                        elementKey="DROPDOWN_TOGGLE"
-                        isFlying
-                        color={badge.style}
-                    >
-                        {badge.counter}
-                    </MenuBadge>
-                )}
-            </DropdownToggle>
-            <DropdownMenu className={style.categoryListDropdownMenu}>
-                <Container>
-                    <div className={style.categories}>
-                        {columns.map((column) => (
-                            <div
-                                key={`menu-column-${column.map(({ id }) => id)
-                                    .join('-')}`}
-                                className={style.categoryColumn}
-                            >
-                                {column.map((category) => (
-                                    <Category
-                                        category={category}
-                                        key={`category-${category.id}`}
-                                        closeMenu={() => setOpen(false)}
-                                    />
-                                ))}
-                            </div>
-                        ))}
-                    </div>
-                </Container>
-            </DropdownMenu>
-        </Dropdown>
+        <Nav>
+            <Manager>
+                <Reference>
+                    {({ ref }) => (
+                        <NavItem>
+                            <NavLink onClick={() => setOpen(!open)} tag={Button} color="link" ref={ref}>
+                                <FontAwesomeIcon icon={faListUl} />
+                                {badge && (
+                                    <MenuBadge
+                                        elementKey="DROPDOWN_TOGGLE"
+                                        isFlying
+                                        color={badge.style}
+                                    >
+                                        {badge.counter}
+                                    </MenuBadge>
+                                )}
+                            </NavLink>
+                        </NavItem>
+                    )}
+                </Reference>
+                <Popper placement="bottom-start">
+                    {(
+                        {
+                            ref,
+                            style: popperStyle,
+                            placement,
+                            arrowProps,
+                        },
+                    ) => (
+                        <div
+                            ref={ref}
+                            style={popperStyle}
+                            data-placement={placement}
+                            className={classNames(
+                                style.categoryListDropdownMenu,
+                                open && style.open,
+                            )}
+                        >
+                            <Container>
+                                <div className={style.categories}>
+                                    {columns.map((column) => (
+                                        <div
+                                            key={`menu-column-${column.map(({ id }) => id)
+                                                .join('-')}`}
+                                            className={style.categoryColumn}
+                                        >
+                                            {column.map((category) => (
+                                                <Category
+                                                    category={category}
+                                                    key={`category-${category.id}`}
+                                                    closeMenu={() => setOpen(false)}
+                                                />
+                                            ))}
+                                        </div>
+                                    ))}
+                                </div>
+                            </Container>
+                            <div ref={arrowProps.ref} style={arrowProps.style} />
+                        </div>
+                    )}
+                </Popper>
+            </Manager>
+        </Nav>
     );
 }
 
