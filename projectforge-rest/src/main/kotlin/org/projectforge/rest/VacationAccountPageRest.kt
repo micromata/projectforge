0d7ae5f5a34a1d7920540b110c218da368d3ef7d
Kai Reinhard <k.reinhard@micromata.de>
2021-12-01 19:33:39 +0100

    Vacation account page: average working hours added.

--- projectforge-rest/src/main/kotlin/org/projectforge/rest/VacationAccountPageRest.kt
+++ projectforge-rest/src/main/kotlin/org/projectforge/rest/VacationAccountPageRest.kt
@@ -58,180 +58,229 @@ private val log = KotlinLogging.logger {}
 @RestController
 @RequestMapping("${Rest.URL}/vacationAccount")
 class VacationAccountPageRest {
-    class Data(
-            var employee: EmployeeDO? = null,
-            var statistics: MutableMap<String, Any>? = null,
-            var vacations: MutableMap<String, Any>? = null
+  class Data(
+    var employee: EmployeeDO? = null,
+    var statistics: MutableMap<String, Any>? = null,
+    var vacations: MutableMap<String, Any>? = null,
+    /**
+     * booked hours (time sheets) of last 3 months divided by working days as localized text message.
+     */
+    var workingHoursStatistics: String? = null,
+  )
+
+  @Autowired
+  private lateinit var employeeDao: EmployeeDao
+
+  @Autowired
+  private lateinit var leaveAccountEntryDao: LeaveAccountEntryDao
+
+  @Autowired
+  private lateinit var userPrefService: UserPrefService
+
+  @Autowired
+  private lateinit var vacationService: VacationService
+
+  @Autowired
+  private lateinit var remainingLeaveDao: RemainingLeaveDao
+
+  @GetMapping("dynamic")
+  fun getForm(@RequestParam("id") searchEmployeeId: Int? = null): FormLayoutData {
+    val layout = UILayout("vacation.leaveaccount.title")
+    val lc = LayoutContext(VacationDO::class.java)
+    layout.addTranslations(
+      "employee",
+      "fibu.common.workingDays",
+      "vacation.leaveaccount.title",
+      "vacation.annualleave",
+      "vacation.previousyearleave",
+      "vacation.subtotal",
+      "menu.vacation.leaveAccountEntry",
+      "vacation.vacationApproved",
+      "vacation.vacationInProgress",
+      "vacation.availablevacation",
+      "vacation.specialApproved",
+      "vacation.specialInProgress",
+      "vacation.title.list",
+      "vacation.startdate",
+      "vacation.enddate",
+      "vacation.Days",
+      "vacation.replacement",
+      "vacation.manager",
+      "vacation.status",
+      "vacation.vacationmode",
+      "vacation.special",
+      "comment",
+      "vacation.leaveAccountEntry.title.heading",
+      "date",
+      "vacation.leaveAccountEntry.amount",
+      "description"
+    )
+    val endOfYear = vacationService.getEndOfCarryVacationOfPreviousYear(Year.now().value)
+    val endOfYearString = PFDateTimeUtils.ensureUsersDateTimeFormat(DateFormatType.DATE_WITHOUT_YEAR).format(endOfYear)
+    layout.addTranslation(
+      "vacation.previousyearleaveunused",
+      translateMsg("vacation.previousyearleaveunused", endOfYearString)
     )
 
-    @Autowired
-    private lateinit var employeeDao: EmployeeDao
+    val isHRMember = vacationService.hasLoggedInUserHRVacationAccess()
 
-    @Autowired
-    private lateinit var leaveAccountEntryDao: LeaveAccountEntryDao
+    val employeeId: Int? = if (isHRMember) {
+      // If, and only if the current logged-in user is a member of HR staff, other employees may be chosen:
+      // 1st any given user by request param is used,
+      // 2nd the last chosen user from the user's preferences or, if none given:
+      // 3rd the current loggedin user himself.
+      searchEmployeeId ?: getUserPref().employeeId ?: ThreadLocalUserContext.getUserContext().employeeId
+    } else {
+      // For non HR users, only the user himself is assumed.
+      ThreadLocalUserContext.getUserContext().employeeId;
+    }
+    val employee = employeeDao.internalGetById(employeeId)
 
-    @Autowired
-    private lateinit var userPrefService: UserPrefService
-
-    @Autowired
-    private lateinit var vacationService: VacationService
-
-    @Autowired
-    private lateinit var remainingLeaveDao: RemainingLeaveDao
-
-    @GetMapping("dynamic")
-    fun getForm(@RequestParam("id") searchEmployeeId: Int? = null): FormLayoutData {
-        val layout = UILayout("vacation.leaveaccount.title")
-        val lc = LayoutContext(VacationDO::class.java)
-        layout.addTranslations("employee",
-                "vacation.leaveaccount.title",
-                "vacation.annualleave",
-                "vacation.previousyearleave",
-                "vacation.subtotal",
-                "menu.vacation.leaveAccountEntry",
-                "vacation.vacationApproved",
-                "vacation.vacationInProgress",
-                "vacation.availablevacation",
-                "vacation.specialApproved",
-                "vacation.specialInProgress",
-                "vacation.title.list",
-                "vacation.startdate",
-                "vacation.enddate",
-                "vacation.Days",
-                "vacation.replacement",
-                "vacation.manager",
-                "vacation.status",
-                "vacation.vacationmode",
-                "vacation.special",
-                "comment",
-                "vacation.leaveAccountEntry.title.heading",
-                "date",
-                "vacation.leaveAccountEntry.amount",
-                "description"
+    val statistics = mutableMapOf<String, Any>()
+    val currentStats = vacationService.getVacationStats(employee, Year.now().value)
+    val prevStats = vacationService.getVacationStats(employee, Year.now().value - 1)
+    val nextStats = vacationService.getVacationStats(employee, Year.now().value + 1)
+    val vacations = mutableMapOf<String, Any>()
+    if (employeeId != null) {
+      statistics["statisticsCurrentYear"] = VacationStatsFormatted(currentStats)
+      statistics["statisticsPreviousYear"] = VacationStatsFormatted(prevStats)
+      readVacations(vacations, "Current", employeeId, currentStats.year)
+      readVacations(vacations, "Previous", employeeId, prevStats.year)
+      readVacations(vacations, "Next", employeeId, nextStats.year)
+      val periodBegin = PFDay.of(prevStats.year, Month.JANUARY, 1).localDate
+      val periodEnd = PFDay.of(currentStats.year, Month.JANUARY, 1).endOfYear.localDate
+      leaveAccountEntryDao.getList(employeeId, periodBegin, periodEnd)?.let { list ->
+        vacations["leaveAccountEntries"] = list.map { LeaveAccountEntry(it) }.sortedByDescending { it.date }
+      }
+    }
+    val buttonCol = UICol(6)
+    val responseAction = ResponseAction(
+      PagesResolver.getEditPageUrl(
+        VacationPagesRest::class.java,
+        params = mapOf("employee" to employeeId),
+        absolute = true
+      ), targetType = TargetType.REDIRECT
+    )
+    responseAction.addVariable("returnToCaller", "account")
+    // TODO: Add employee for preselecting edit form
+    buttonCol.add(UIButton("add", translate("add"), UIColor.SUCCESS, responseAction = responseAction))
+    if (this.vacationService.hasLoggedInUserHRVacationAccess() &&
+      !NumberHelper.isEqual(currentStats.remainingLeaveFromPreviousYear, prevStats.vacationDaysLeftInYear)
+    ) {
+      buttonCol.add(
+        UIButton(
+          "recalculate", translate("vacation.recalculateRemainingLeave"), UIColor.DANGER,
+          responseAction = ResponseAction("vacationAccount/recalculate", targetType = TargetType.POST)
         )
-        val endOfYear = vacationService.getEndOfCarryVacationOfPreviousYear(Year.now().value)
-        val endOfYearString = PFDateTimeUtils.ensureUsersDateTimeFormat(DateFormatType.DATE_WITHOUT_YEAR).format(endOfYear)
-        layout.addTranslation("vacation.previousyearleaveunused", translateMsg("vacation.previousyearleaveunused", endOfYearString))
-
-        val isHRMember = vacationService.hasLoggedInUserHRVacationAccess()
-
-        val employeeId: Int? = if (isHRMember) {
-            // If, and only if the current logged-in user is a member of HR staff, other employees may be chosen:
-            // 1st any given user by request param is used,
-            // 2nd the last chosen user from the user's preferences or, if none given:
-            // 3rd the current loggedin user himself.
-            searchEmployeeId ?: getUserPref().employeeId ?: ThreadLocalUserContext.getUserContext().employeeId
-        } else {
-            // For non HR users, only the user himself is assumed.
-            ThreadLocalUserContext.getUserContext().employeeId;
-        }
-        val employee = employeeDao.internalGetById(employeeId)
-
-        val statistics = mutableMapOf<String, Any>()
-        val currentStats = vacationService.getVacationStats(employee, Year.now().value)
-        val prevStats = vacationService.getVacationStats(employee, Year.now().value - 1)
-        val nextStats = vacationService.getVacationStats(employee, Year.now().value + 1)
-        val vacations = mutableMapOf<String, Any>()
-        if (employeeId != null) {
-            statistics["statisticsCurrentYear"] = VacationStatsFormatted(currentStats)
-            statistics["statisticsPreviousYear"] = VacationStatsFormatted(prevStats)
-            readVacations(vacations, "Current", employeeId, currentStats.year)
-            readVacations(vacations, "Previous", employeeId, prevStats.year)
-            readVacations(vacations, "Next", employeeId, nextStats.year)
-            val periodBegin = PFDay.of(prevStats.year, Month.JANUARY, 1).localDate
-            val periodEnd = PFDay.of(currentStats.year, Month.JANUARY, 1).endOfYear.localDate
-            leaveAccountEntryDao.getList(employeeId, periodBegin, periodEnd)?.let { list ->
-                vacations["leaveAccountEntries"] = list.map { LeaveAccountEntry(it) }.sortedByDescending { it.date }
-            }
-        }
-        val buttonCol = UICol(6)
-        val responseAction = ResponseAction(PagesResolver.getEditPageUrl(VacationPagesRest::class.java, params = mapOf("employee" to employeeId), absolute = true), targetType = TargetType.REDIRECT)
-        responseAction.addVariable("returnToCaller", "account")
-        // TODO: Add employee for preselecting edit form
-        buttonCol.add(UIButton("add", translate("add"), UIColor.SUCCESS, responseAction = responseAction))
-        if (this.vacationService.hasLoggedInUserHRVacationAccess() &&
-                !NumberHelper.isEqual(currentStats.remainingLeaveFromPreviousYear, prevStats.vacationDaysLeftInYear)) {
-            buttonCol.add(UIButton("recalculate", translate("vacation.recalculateRemainingLeave"), UIColor.DANGER,
-                    responseAction = ResponseAction("vacationAccount/recalculate", targetType = TargetType.POST)))
-        }
-
-        val statisticRow = UIRow()
-
-        if (isHRMember) {
-            statisticRow
-                    .add(UICol(UILength(md = 4, sm = 12))
-                            .add(lc, "employee"))
-                    .add(UICol(UILength(md = 8, sm = 12))
-                            .add(UICustomized("vacation.statistics")))
-        } else {
-            statisticRow.add(UICol(UILength(md = 12, sm = 12))
-                    .add(UICustomized("vacation.statistics")))
-        }
-
-
-        layout.add(UIFieldset(12)
-                .add(statisticRow)
-                .add(UIRow().add(buttonCol))
-        ).add(UIFieldset(12)
-                .add(UIRow()
-                        .add(UICol(12)
-                                .add(UICustomized("vacation.entries")))))
-
-        layout.add(UIAlert("vacation.subscription.info", title = "vacation.subscription"))
-
-        layout.watchFields.add("employee")
-        LayoutUtils.process(layout)
-
-        val data = Data(employee = employee, statistics = statistics, vacations = vacations)
-
-        return FormLayoutData(data, layout, null)
+      )
     }
 
-    @PostMapping(RestPaths.WATCH_FIELDS)
-    fun watchFields(@Valid @RequestBody postData: PostData<Data>): ResponseAction {
-        val employeeId = postData.data.employee?.id
-        if (postData.watchFieldsTriggered?.contains("employee") == false || employeeId == null) {
-            return ResponseAction(targetType = TargetType.NOTHING)
-        }
-        getUserPref().employeeId = employeeId
-        return buildResponseAction(employeeId)
-    }
+    val statisticRow = UIRow()
 
-    @PostMapping("recalculate")
-    fun recalculateRemainingLeave(@Valid @RequestBody postData: PostData<Data>): ResponseAction {
-        if (!this.vacationService.hasLoggedInUserHRVacationAccess() || postData.data.employee == null) {
-            log.warn { "User has now HR vacation access. Recalculating of remaining leaves ignored." }
-            return ResponseAction(targetType = TargetType.NOTHING)
-        }
-        val employeeId = postData.data.employee!!.id
-
-        remainingLeaveDao.internalMarkAsDeleted(employeeId, Year.now().value)
-
-        return buildResponseAction(employeeId)
-    }
-
-    private fun buildResponseAction(employeeId: Int): ResponseAction {
-        val layoutData = getForm(employeeId)
-
-        return ResponseAction(
-                url = PagesResolver.getDynamicPageUrl(this::class.java, id = employeeId, absolute = true),
-                targetType = TargetType.UPDATE
+    val workingStatistics = UIReadOnlyField("workingHoursStatistics", label = "fibu.common.workingDays")
+    if (isHRMember) {
+      statisticRow
+        .add(
+          UICol(UILength(md = 4, sm = 12))
+            .add(lc, "employee")
         )
-                .addVariable("data", layoutData.data)
-                .addVariable("ui", layoutData.ui)
+        .add(
+          UICol(UILength(md = 8, sm = 12))
+            .add(UICustomized("vacation.statistics"))
+            .add(workingStatistics)
+        )
+    } else {
+      statisticRow.add(
+        UICol(UILength(md = 12, sm = 12))
+          .add(UICustomized("vacation.statistics"))
+          .add(workingStatistics)
+      )
     }
 
-    private fun readVacations(variables: MutableMap<String, Any>, id: String, employeeId: Int, year: Int) {
-        val yearDate = PFDay.of(year, Month.JANUARY, 1)
-        val dbList = vacationService.getVacationsListForPeriod(employeeId, yearDate.localDate, yearDate.endOfYear.localDate, true, true)
-        val list = dbList.map { Vacation(it) }
-        variables["vacations${id}Year"] = list
-        variables["year$id"] = year
-    }
 
-    class VacationAccountUserPref(var employeeId: Int? = null)
+    layout.add(
+      UIFieldset(12)
+        .add(statisticRow)
+        .add(UIRow().add(buttonCol))
+    ).add(
+      UIFieldset(12)
+        .add(
+          UIRow()
+            .add(
+              UICol(12)
+                .add(UICustomized("vacation.entries"))
+            )
+        )
+    )
 
-    private fun getUserPref(): VacationAccountUserPref {
-        return userPrefService.ensureEntry("vacation", "account", VacationAccountUserPref())
+    layout.add(UIAlert("vacation.subscription.info", title = "vacation.subscription"))
+
+    layout.watchFields.add("employee")
+    LayoutUtils.process(layout)
+
+    val data = Data(
+      employee = employee,
+      statistics = statistics,
+      vacations = vacations,
+      workingHoursStatistics = vacationService.getAverageWorkingTimeStats(
+        employee.user ?: ThreadLocalUserContext.getUser(), PFDay.fromOrNull(employee.eintrittsDatum)
+      ).localizedMessage
+    )
+
+    return FormLayoutData(data, layout, null)
+  }
+
+  @PostMapping(RestPaths.WATCH_FIELDS)
+  fun watchFields(@Valid @RequestBody postData: PostData<Data>): ResponseAction {
+    val employeeId = postData.data.employee?.id
+    if (postData.watchFieldsTriggered?.contains("employee") == false || employeeId == null) {
+      return ResponseAction(targetType = TargetType.NOTHING)
     }
+    getUserPref().employeeId = employeeId
+    return buildResponseAction(employeeId)
+  }
+
+  @PostMapping("recalculate")
+  fun recalculateRemainingLeave(@Valid @RequestBody postData: PostData<Data>): ResponseAction {
+    if (!this.vacationService.hasLoggedInUserHRVacationAccess() || postData.data.employee == null) {
+      log.warn { "User has now HR vacation access. Recalculating of remaining leaves ignored." }
+      return ResponseAction(targetType = TargetType.NOTHING)
+    }
+    val employeeId = postData.data.employee!!.id
+
+    remainingLeaveDao.internalMarkAsDeleted(employeeId, Year.now().value)
+
+    return buildResponseAction(employeeId)
+  }
+
+  private fun buildResponseAction(employeeId: Int): ResponseAction {
+    val layoutData = getForm(employeeId)
+
+    return ResponseAction(
+      url = PagesResolver.getDynamicPageUrl(this::class.java, id = employeeId, absolute = true),
+      targetType = TargetType.UPDATE
+    )
+      .addVariable("data", layoutData.data)
+      .addVariable("ui", layoutData.ui)
+  }
+
+  private fun readVacations(variables: MutableMap<String, Any>, id: String, employeeId: Int, year: Int) {
+    val yearDate = PFDay.of(year, Month.JANUARY, 1)
+    val dbList = vacationService.getVacationsListForPeriod(
+      employeeId,
+      yearDate.localDate,
+      yearDate.endOfYear.localDate,
+      true,
+      true
+    )
+    val list = dbList.map { Vacation(it) }
+    variables["vacations${id}Year"] = list
+    variables["year$id"] = year
+  }
+
+  class VacationAccountUserPref(var employeeId: Int? = null)
+
+  private fun getUserPref(): VacationAccountUserPref {
+    return userPrefService.ensureEntry("vacation", "account", VacationAccountUserPref())
+  }
 }
