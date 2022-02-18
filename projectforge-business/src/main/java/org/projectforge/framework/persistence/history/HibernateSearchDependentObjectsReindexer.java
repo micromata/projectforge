5f7ef41b8cbbf29b1bff094f0c0b708dcaecb19e
Kai Reinhard <k.reinhard@micromata.de>
2022-02-18 19:11:53 +0100

    Copyright 2021 -> 2022

--- projectforge-business/src/main/java/org/projectforge/framework/persistence/history/HibernateSearchDependentObjectsReindexer.java
+++ projectforge-business/src/main/java/org/projectforge/framework/persistence/history/HibernateSearchDependentObjectsReindexer.java
@@ -3,7 +3,7 @@
 // Project ProjectForge Community Edition
 //         www.projectforge.org
 //
-// Copyright (C) 2001-2021 Micromata GmbH, Germany (www.micromata.com)
+// Copyright (C) 2001-2022 Micromata GmbH, Germany (www.micromata.com)
 //
 // ProjectForge is dual-licensed.
 //
