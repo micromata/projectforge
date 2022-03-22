5f1a8c9d36022d9ba9cbc606aa99cc899571d167
Kai Reinhard <k.reinhard@micromata.de>
2022-03-22 08:59:00 +0100

    WIP: Password reset with 2FA

---
+++ projectforge-business/src/main/java/org/projectforge/framework/persistence/user/service/PasswordResetService.kt
@@ -0,0 +1,119 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2022 Micromata GmbH, Germany (www.micromata.com)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.framework.persistence.user.service
+
+import mu.KotlinLogging
+import org.projectforge.SystemStatus
+import org.projectforge.mail.SendMail
+import org.projectforge.business.user.service.UserService
+import org.projectforge.framework.i18n.translate
+import org.projectforge.framework.persistence.user.entities.PFUserDO
+import org.projectforge.mail.Mail
+import org.projectforge.security.SecurityLogging
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import kotlin.concurrent.thread
+
+private val log = KotlinLogging.logger {}
+
+/**
+ * After requesting a password reset by the user (username or e-mail must match a single existing user and user must
+ * have system access), an e-mail is sent to the user with a link including a token, which is valid for a short period
+ * (see [PasswordResetTokenStore]).
+ * On the web page for password reset, the existence of the token will be checked.
+ */
+@Service
+class PasswordResetService {
+  @Autowired
+  private lateinit var sendMail: SendMail
+
+  @Autowired
+  private lateinit var userService: UserService
+
+  /**
+   * @param Checks the existence of the given token.
+   * @return The user assigned to the token or null, if no such user is found.
+   */
+  fun checkToken(token: String): PFUserDO? {
+    val userId = PasswordResetTokenStore.checkAndDeleteToken(token) ?: return null
+    return userService.internalGetById(userId)
+  }
+
+  /**
+   * @param Link in e-mail for password reset. "<token>" will be reset by the generated token.
+   *        E. g. projectforge.acme.com/react/public/passwordReset?token=<token>
+   */
+  fun sendMail(usernameEmail: String, link: String) {
+    /**
+     * Start the following procedure as thread to prevent, that an attacker
+     * will get any information of the user existence regarding the duration of the request
+     */
+    thread(start = true) {
+      if (usernameEmail.contains("@")) {
+        userService.findUserByMail(usernameEmail)?.let { users ->
+          val size = users.size
+          if (size > 1) {
+            log.warn { "Can't reset user password by e-mail, because the mail address '$usernameEmail' is used by ${size} users." }
+          } else if (size == 1) {
+            sendPasswordReset(usernameEmail, users[0], link)
+            return@thread
+          }
+        }
+      }
+      // User may contain '@'-char, so do not user else branch:
+      val user = userService.getInternalByUsername(usernameEmail)
+      if (user == null) {
+        SecurityLogging.logSecurityWarn(
+          PasswordResetService::class.java,
+          "User with e-mail '$usernameEmail' not found for password reset."
+        )
+      } else {
+        sendPasswordReset(usernameEmail, user, link)
+      }
+    }
+  }
+
+  private fun sendPasswordReset(usernameEmail: String, user: PFUserDO, link: String) {
+    // User found by mail
+    log.info { "Password reset requested by '$usernameEmail' for user ${user.username}." }
+    if (!user.hasSystemAccess()) {
+      log.warn { "A user without system access required a password reset by '$usernameEmail': ${user}." }
+      return
+    }
+    val mail = Mail()
+    mail.subject = translate("password.forgotten.mail.subject")
+    mail.setTo(user)
+    mail.contentType = Mail.CONTENTTYPE_HTML
+    val token = PasswordResetTokenStore.createToken(user.id)
+    val data = mutableMapOf<String, Any>("link" to link.replace("<token>", token))
+    mail.content = sendMail.renderGroovyTemplate(
+      mail, "mail/passwordResetMail.html", data,
+      mail.subject, user
+    )
+    if (SystemStatus.isDevelopmentMode()) {
+      log.info { "Development mode: Mail with password reset will be sent to '${mail.to}'. Link is '$link'." }
+    }
+    sendMail.send(mail)
+  }
+}
---
+++ projectforge-rest/src/main/kotlin/org/projectforge/rest/pub/PasswordResetPageRest.kt
@@ -0,0 +1,131 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2022 Micromata GmbH, Germany (www.micromata.com)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.rest.pub
+
+import org.projectforge.framework.i18n.translate
+import org.projectforge.framework.persistence.user.api.UserContext
+import org.projectforge.framework.persistence.user.service.PasswordResetService
+import org.projectforge.rest.My2FAServicesRest
+import org.projectforge.rest.config.Rest
+import org.projectforge.rest.core.AbstractDynamicPageRest
+import org.projectforge.rest.core.RestResolver
+import org.projectforge.rest.dto.FormLayoutData
+import org.projectforge.rest.dto.PostData
+import org.projectforge.rest.dto.ServerData
+import org.projectforge.security.My2FAData
+import org.projectforge.security.My2FAService
+import org.projectforge.ui.*
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+
+/**
+ * This rest service should be available without login (public).
+ * On this page, the user may enter his login name or e-mail and request a password reset.
+ */
+@RestController
+@RequestMapping("${Rest.PUBLIC_URL}/passwordReset")
+open class PasswordResetPageRest : AbstractDynamicPageRest() {
+  @Autowired
+  private lateinit var passwordResetService: PasswordResetService
+
+  @Autowired
+  private lateinit var my2FAService: My2FAService
+
+  @Autowired
+  private lateinit var my2FAServicesRest: My2FAServicesRest
+
+  /**
+   * @param token The token sent by mail (is mandatory for getting and checking the user).
+   * @see [PasswordResetService.checkToken]
+   */
+  @GetMapping("dynamic")
+  fun getForm(@RequestParam("token") token: String): FormLayoutData {
+    val user = passwordResetService.checkToken(token)
+    val layout = UILayout("password.reset.title")
+    if (user == null) {
+      UIAlert(
+        message = "password.reset.error",
+        color = UIColor.DANGER
+      )
+      LayoutUtils.process(layout)
+      return FormLayoutData(null, layout, ServerData())
+    }
+    val userContext = UserContext(user)
+    my2FAServicesRest.fillLayout4PublicPage(layout, userContext)
+    layout
+      .add(
+        UIInput(
+          "newPassword",
+          label = "user.changePassword.newPassword",
+          dataType = UIDataType.PASSWORD,
+          required = true
+        )
+      )
+      .add(
+        UIInput(
+          "newPasswordRepeat",
+          label = "passwordRepeat",
+          dataType = UIDataType.PASSWORD,
+          required = true
+        )
+      )
+      .add(
+        UIButton(
+          "cancel",
+          translate("cancel"),
+          UIColor.DANGER,
+          responseAction = ResponseAction("/", targetType = TargetType.REDIRECT),
+        )
+      )
+      .add(
+        UIButton(
+          "reset",
+          translate("password.forgotten.request"),
+          UIColor.SUCCESS,
+          responseAction = ResponseAction(RestResolver.getRestUrl(this::class.java), targetType = TargetType.POST),
+          default = true
+        )
+      )
+    LayoutUtils.process(layout)
+    return FormLayoutData(null, layout, ServerData())
+  }
+
+  @PostMapping
+  fun post(@RequestBody postData: PostData<PasswordResetData>)
+      : ResponseEntity<ResponseAction> {
+    val newPassword = postData.data.newPassword
+    val newPasswordRepeat = postData.data.newPasswordRepeat
+    val code = postData.data.code
+    return ResponseEntity.ok(
+      ResponseAction(targetType = TargetType.UPDATE)
+    )
+  }
+
+  class PasswordResetData : My2FAData() {
+    var newPassword: CharArray? = null
+    var newPasswordRepeat: CharArray? = null
+  }
+}
