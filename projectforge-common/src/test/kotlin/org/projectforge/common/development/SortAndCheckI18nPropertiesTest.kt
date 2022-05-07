a8c0c2ce69d132736d2bcd3414bef6cfb83c084c
Kai Reinhard <k.reinhard@micromata.de>
2022-05-07 09:52:17 +0200

    I18n*Resources*.properties: Using now UTF8 instead of ISO-8859-1. Java 9 supports now UTF-8 resource property files.

--- projectforge-common/src/test/kotlin/org/projectforge/common/development/SortAndCheckI18nPropertiesTest.kt
+++ projectforge-common/src/test/kotlin/org/projectforge/common/development/SortAndCheckI18nPropertiesTest.kt
@@ -39,7 +39,7 @@ class SortAndCheckI18nPropertiesTest {
     check("Field ''\${label}'' is", "Field ''\${label}'' is")
     val germanText = "Dies ist ein gültiger Deutscher Text mit allen Umlauten: äöüÄÖÜß"
     check(
-      "Dies ist ein g\\u00FCltiger Deutscher Text mit allen Umlauten: \\u00E4\\u00F6\\u00FC\\u00C4\\u00D6\\u00DC\\u00DF",
+      "Dies ist ein gültiger Deutscher Text mit allen Umlauten: äöüÄÖÜß",
       germanText
     )
   }
