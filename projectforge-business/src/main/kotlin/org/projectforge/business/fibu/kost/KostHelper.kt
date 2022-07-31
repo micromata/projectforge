3a1b35707cbd4ec4d6c3cec2776066526d2bb644
Kai Reinhard <k.reinhard@micromata.de>
2022-07-31 22:02:32 +0200

    KostHelper -> Kotlin

--- projectforge-business/src/main/kotlin/org/projectforge/business/fibu/kost/KostHelper.kt
+++ projectforge-business/src/main/kotlin/org/projectforge/business/fibu/kost/KostHelper.kt
@@ -20,39 +20,37 @@
 // with this program; if not, see http://www.gnu.org/licenses/.
 //
 /////////////////////////////////////////////////////////////////////////////
+package org.projectforge.business.fibu.kost
 
-package org.projectforge.business.fibu.kost;
+import org.projectforge.framework.utils.NumberHelper.parseInteger
 
-import org.projectforge.framework.utils.NumberHelper;
-
-public class KostHelper
-{
+object KostHelper {
   /**
    * @param kostString Format ######## or #.###.##.## is supported.
    * @return int[4] or null if string is not parse-able.
    */
-  public static int[] parseKostString(final String kostString)
-  {
+  @JvmStatic
+  fun parseKostString(kostString: String?): IntArray? {
     if (kostString == null) {
-      return null;
+      return null
     }
-    final String str = kostString.trim();
-    if (str.matches("\\d{8}")) {
+    val str = kostString.trim { it <= ' ' }
+    if (str.matches("\\d{8}".toRegex())) {
       // 12345678
-      final int[] result = new int[4];
-      result[0] = NumberHelper.parseInteger(kostString.substring(0, 1));
-      result[1] = NumberHelper.parseInteger(kostString.substring(1, 4));
-      result[2] = NumberHelper.parseInteger(kostString.substring(4, 6));
-      result[3] = NumberHelper.parseInteger(kostString.substring(6, 8));
-      return result;
-    } else if (str.matches("\\d{1}\\.\\d{3}\\.\\d{2}\\.\\d{2}")) {
-      final int[] result = new int[4];
-      result[0] = NumberHelper.parseInteger(kostString.substring(0, 1));
-      result[1] = NumberHelper.parseInteger(kostString.substring(2, 5));
-      result[2] = NumberHelper.parseInteger(kostString.substring(6, 8));
-      result[3] = NumberHelper.parseInteger(kostString.substring(9, 11));
-      return result;
+      val result = IntArray(4)
+      result[0] = parseInteger(kostString.substring(0, 1))!!
+      result[1] = parseInteger(kostString.substring(1, 4))!!
+      result[2] = parseInteger(kostString.substring(4, 6))!!
+      result[3] = parseInteger(kostString.substring(6, 8))!!
+      return result
+    } else if (str.matches("\\d{1}\\.\\d{3}\\.\\d{2}\\.\\d{2}".toRegex())) {
+      val result = IntArray(4)
+      result[0] = parseInteger(kostString.substring(0, 1))!!
+      result[1] = parseInteger(kostString.substring(2, 5))!!
+      result[2] = parseInteger(kostString.substring(6, 8))!!
+      result[3] = parseInteger(kostString.substring(9, 11))!!
+      return result
     }
-    return null;
+    return null
   }
 }
