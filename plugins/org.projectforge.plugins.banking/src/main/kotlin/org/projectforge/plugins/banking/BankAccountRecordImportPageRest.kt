9ccbfdfc2d3f45f6b0a81b3fdc1524b85a9250db
Kai Reinhard <k.reinhard@micromata.de>
2022-08-20 08:29:05 +0200

    WIP: Banking plugin (and new import module).

---
+++ plugins/org.projectforge.plugins.banking/src/main/kotlin/org/projectforge/plugins/banking/BankAccountRecordImportPageRest.kt
@@ -0,0 +1,45 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2022 Micromata GmbH, Germany (www.micromata.com)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.plugins.banking
+
+import org.projectforge.rest.config.Rest
+import org.projectforge.rest.dto.FormLayoutData
+import org.projectforge.rest.importer.AbstractImportPageRest
+import org.projectforge.ui.UILayout
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import javax.servlet.http.HttpServletRequest
+
+@RestController
+@RequestMapping("${Rest.URL}/importBankAccountRecords")
+open class BankAccountRecordImportPageRest : AbstractImportPageRest() {
+
+  @GetMapping("dynamic")
+  fun getForm(request: HttpServletRequest): FormLayoutData {
+    val layout = UILayout("plugins.banking.account.record.import.title")
+    return FormLayoutData(BankAccountRecord(), layout, createServerData(request))
+  }
+
+}
