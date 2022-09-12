f4312d858a82c507034af031a4b1a8201a0d7b98
Kai Reinhard <k.reinhard@micromata.de>
2022-09-13 00:12:22 +0200

    Importing

--- projectforge-webapp/src/components/design/DiffText.jsx
+++ projectforge-webapp/src/components/design/DiffText.jsx
@@ -2,12 +2,13 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import DiffViewer from 'react-diff-viewer';
 import { DiffMethod } from 'react-diff-viewer/lib/compute-lines';
+import _ from 'lodash';
 
 function DiffText({ oldValue, newValue, compareMethod }) {
     return (
         <DiffViewer
-            oldValue={oldValue}
-            newValue={newValue}
+            oldValue={_.toString(oldValue)}
+            newValue={_.toString(newValue)}
             showDiffOnly
             splitView={false}
             compareMethod={compareMethod}
@@ -17,8 +18,10 @@ function DiffText({ oldValue, newValue, compareMethod }) {
 }
 
 DiffText.propTypes = {
-    newValue: PropTypes.string.isRequired,
-    oldValue: PropTypes.string.isRequired,
+    // eslint-disable-next-line react/forbid-prop-types
+    newValue: PropTypes.any.isRequired, // string, number, boolean, array, ...
+    // eslint-disable-next-line react/forbid-prop-types
+    oldValue: PropTypes.any.isRequired, // string, number, boolean, array, ...
     compareMethod: PropTypes.string,
 };
 
