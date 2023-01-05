bf2e51373408bb190b23569cc84e7f971c2698d4
Kai Reinhard <k.reinhard@micromata.de>
2023-01-06 00:41:47 +0100

    WIP D.velop

--- projectforge-rest/src/main/kotlin/org/projectforge/rest/dvelop/TradingPartnerService.kt
+++ projectforge-rest/src/main/kotlin/org/projectforge/rest/dvelop/TradingPartnerService.kt
@@ -18,6 +18,38 @@ import org.springframework.stereotype.Service
 class TradingPartnerService :
   AbstractService<TradingPartner>("/alphaflow-tradingpartner/tradingpartnerservice/tradingpartners", "TradingPartner") {
 
+  override fun buildUpdateEntity(localState: TradingPartner, remoteState: TradingPartner): TradingPartner? {
+    if (localState.number != remoteState.number) {
+      throw IllegalArgumentException("Number of existing TradingPartner (oldState) doesn't match new state!")
+    }
+    val updateEntity = TradingPartner()
+    val updateContext = UpdateContext()
+    updateEntity.id = remoteState.id
+    updateEntity.company = getPrioritizedValue(localState.company, remoteState.company, updateContext)
+    updateEntity.shortName = getPrioritizedValue(localState.shortName, remoteState.shortName, updateContext)
+    updateEntity.importCode = getPrioritizedValue(localState.importCode, remoteState.importCode, updateContext)
+    updateEntity.datevKonto = getPrioritizedValue(localState.datevKonto, remoteState.datevKonto, updateContext)
+    if (remoteState.organization == null || remoteState.organization?.id.isNullOrBlank()) {
+      remoteState.organization = localState.organization
+      updateContext.modified = true
+    }
+    if (remoteState.isBillToAddressEmpty && !localState.isBillToAddressEmpty) {
+      // Update remote address only, if remote address is empty.
+      updateEntity.billToStreet = localState.billToStreet
+      updateEntity.billToZip = localState.billToZip
+      updateEntity.billToCity = localState.billToCity
+      updateEntity.billToCountry = localState.billToCountry
+      updateEntity.billToRegion = localState.billToRegion
+      updateEntity.billToAddressAdditional = localState.billToAddressAdditional
+      updateContext.modified = true
+    }
+    return if (updateContext.modified) {
+      updateEntity
+    } else {
+      null
+    }
+  }
+
   override fun fromJson(response: String): ListData<TradingPartner>? {
     return JsonUtils.fromJson(response, TradingPartnerListData::class.java, false)
   }
