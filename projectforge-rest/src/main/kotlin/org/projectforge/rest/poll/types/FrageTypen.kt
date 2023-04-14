e3d6fff234f5b83c5f2e51f1b5ef310b1d45f3fc
Jonas Bernst <j.bernst@micromata.de>
2023-04-14 09:59:08 +0200

    es ist jetzt möglich fragefelder zu erstellen

--- projectforge-rest/src/main/kotlin/org/projectforge/rest/poll/types/FrageTypen.kt
+++ projectforge-rest/src/main/kotlin/org/projectforge/rest/poll/types/FrageTypen.kt
@@ -1,15 +1,25 @@
 package org.projectforge.rest.poll.types
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+import org.projectforge.framework.time.PFDateTime
+import org.projectforge.framework.time.PFDateTimeUtils
+import org.projectforge.framework.time.PFDay
+import org.projectforge.framework.time.PFDayUtils
+import java.time.LocalDate
+import java.time.LocalDateTime
+
 
 class Frage(
-    val uid: String,
+    val uid: String?,
     val question: String? = "",
     val type: BaseType = BaseType.FreiTextFrage,
-    var antworten: List<String> = mutableListOf(),
+    var antworten: MutableList<String>? = mutableListOf(""),
     var parent: String? = null,
-){
+    var isRequired: Boolean? = false,
+    var numberOfSelect: Int? = 1,
 
-}
+)
+
 enum class BaseType {
     JaNeinFrage,
     DatumsAbfrage,
