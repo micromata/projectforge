8afc82402e4dcfd5175c5139ba50ceeda7d0400d
Léon Spohr <l.spohr@micromata.de>
2023-04-19 11:12:58 +0200

    Created PremadeQuestions and added functionality for adding them

--- projectforge-rest/src/main/kotlin/org/projectforge/rest/poll/types/Question.kt
+++ projectforge-rest/src/main/kotlin/org/projectforge/rest/poll/types/Question.kt
@@ -3,17 +3,17 @@ package org.projectforge.rest.poll.types
 import com.fasterxml.jackson.databind.ObjectMapper
 
 
-class Frage(
+class Question(
     val uid: String? = null,
     val question: String? = "",
-    val type: BaseType? = BaseType.FreiTextFrage,
-    var antworten: MutableList<String>? = mutableListOf(""),
+    val type: BaseType? = BaseType.TextQuestion,
+    var answers: MutableList<String>? = mutableListOf(""),
     var parent: String? = null,
     var isRequired: Boolean? = false,
     var numberOfSelect: Int? = 1,
 ){
-    fun toObject(string:String): Frage {
-        return ObjectMapper().readValue(string, Frage::class.java)
+    fun toObject(string:String): Question {
+        return ObjectMapper().readValue(string, Question::class.java)
     }
     fun toJson(): String {
         return ObjectMapper().writeValueAsString(this)
@@ -21,9 +21,10 @@ class Frage(
 }
 
 enum class BaseType {
-    JaNeinFrage,
-    DatumsAbfrage,
+    YesNoQuestion,
+    DateQuestion,
     MultipleChoices,
-    FreiTextFrage,
-    DropDownFrage
+    TextQuestion,
+    DropDownQuestion,
+    PremadeQuestion
 }
