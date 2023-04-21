fc30afaf76806dd31a6773618e163451abd437af
NikitaMic <91180248+NikitaMic@users.noreply.github.com>
2023-04-21 12:54:26 +0200

    removed explanation

--- projectforge-business/src/main/kotlin/org/projectforge/business/poll/PollDO.kt
+++ projectforge-business/src/main/kotlin/org/projectforge/business/poll/PollDO.kt
@@ -26,7 +26,7 @@ open class PollDO : DefaultBaseDO() {
     @get:Column(name = "description", length = 10000)
     open var description: String? = null
 
-    @get:PropertyInfo(i18nKey = "poll.owner", additionalI18nKey = "poll.owner.explanation")
+    @get:PropertyInfo(i18nKey = "poll.owner")
     @get:ManyToOne(fetch = FetchType.LAZY)
     @get:JoinColumn(name = "owner_pk", nullable = false)
     open var owner: PFUserDO? = null
