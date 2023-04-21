d46d34345123073fe2d3cb36bfd226b6c2eefdad
Léon Spohr <l.spohr@micromata.de>
2023-04-21 09:25:54 +0200

    Copied the changes from feature/singleAndMutlipleResponseQuestion branch

--- projectforge-business/src/main/resources/flyway/migrate/common/V7.5.1.2__7.5.1.0-POLL.sql
+++ projectforge-business/src/main/resources/flyway/migrate/common/V7.5.1.2__7.5.1.0-POLL.sql
@@ -24,21 +24,3 @@ ALTER TABLE T_POLL
     ADD CONSTRAINT t_poll_pkey PRIMARY KEY (pk);
 ALTER TABLE T_POLL
     ADD CONSTRAINT fk_t_poll_pf_user FOREIGN KEY (owner_pk) REFERENCES t_pf_user (pk);
-
-CREATE TABLE T_POLL_RESPONSE
-(
-    pk          INTEGER NOT NULL,
-    deleted     BOOLEAN NOT NULL,
-    created     TIMESTAMP WITHOUT TIME ZONE,
-    last_update TIMESTAMP WITHOUT TIME ZONE,
-    poll_pk     INTEGER NOT NULL,
-    owner_pk     INTEGER NOT NULL,
-    responses   CHARACTER Varying(10000)
-);
-
-ALTER TABLE T_POLL_RESPONSE
-    ADD CONSTRAINT t_poll_response_pkey PRIMARY KEY (pk);
-ALTER TABLE T_POLL_RESPONSE
-    ADD CONSTRAINT fk_t_poll_response_pf_user FOREIGN KEY (owner_pk) REFERENCES t_pf_user (pk);
-ALTER TABLE T_POLL_RESPONSE
-    ADD CONSTRAINT fk_t_poll_response_poll FOREIGN KEY (poll_pk) REFERENCES t_poll (pk);
