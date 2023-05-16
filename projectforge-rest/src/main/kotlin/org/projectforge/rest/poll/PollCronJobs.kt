369c0c552d728efa50c24df5c5bae30160a8db8f
Léon Spohr <l.spohr@micromata.de>
2023-05-16 11:24:11 +0200

    Added more mailing

--- projectforge-rest/src/main/kotlin/org/projectforge/rest/poll/PollCronJobs.kt
+++ projectforge-rest/src/main/kotlin/org/projectforge/rest/poll/PollCronJobs.kt
@@ -12,15 +12,13 @@ import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
 import kotlin.collections.ArrayList
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 
 @RestController
-class CronJobs {
+class PollCronJobs {
 
-    private val log: Logger = LoggerFactory.getLogger(CronJobs::class.java)
 
     @Autowired
     private lateinit var pollDao: PollDao
@@ -34,7 +32,7 @@ class CronJobs {
     /**
      * Cron job for daily stuff
      */
-//    @Scheduled(cron = "0 0 1 * * *") // 1am everyday
+// todo   @Scheduled(cron = "0 0 1 * * *") // 1am everyday
     @Scheduled(cron = "0 * * * * *") // 1am everyday
     fun dailyCronJobs() {
         cronDeletePolls()
@@ -46,69 +44,60 @@ class CronJobs {
      * Method to end polls after deadline
      */
     private fun cronEndPolls() {
-        var mailContent = ""
-        var mailHeader = ""
-        val mailAttachments = ArrayList<MailAttachment>()
-        val mailTo = "l.spohr@micromata.de"
 
-        val polls = pollDao.internalLoadAll()
+        val pollDOs = pollDao.internalLoadAll()
         // set State.FINISHED for all old polls and export excel
-        polls.forEach {
-            if (it.deadline?.isBefore(LocalDate.now()) == true) {
-                it.state = PollDO.State.FINISHED
+        pollDOs.forEach { pollDO ->
+            if (pollDO.deadline?.isBefore(LocalDate.now()) == true) {
+                pollDO.state = PollDO.State.FINISHED
 
                 val poll = Poll()
-                poll.copyFrom(it)
+                poll.copyFrom(pollDO)
 
                 val excel = exporter.getExcel(poll)
 
                 val mailAttachment = object : MailAttachment {
                     override fun getFilename(): String {
-                        return "${it.title}_${LocalDateTime.now().year}_Result.xlsx"
+                        return "${pollDO.title}_${LocalDateTime.now().year}_Result.xlsx"
                     }
 
                     override fun getContent(): ByteArray? {
                         return excel
                     }
                 }
-                mailAttachments.add(mailAttachment)
+                // add all attendees mails
+                var mailTo: ArrayList<String> = poll.attendees?.map { it.email } as ArrayList<String>
+                val mailFrom = pollDO.owner?.email.toString()
+                val mailSubject = translateMsg("poll.mail.ended.subject")
+                val mailContent = translateMsg("poll.mail.ended.content", pollDO.title, pollDO.owner?.displayName)
 
-                mailHeader = translateMsg("poll.mail.ended.header")
-                mailContent = translateMsg(
-                    "poll.mail.ended.content", it.title, it.owner?.displayName
-                )
-
-                pollDao.internalSaveOrUpdate(it)
-
-                sendMail(mailTo, mailContent, mailHeader, mailAttachments)
+                pollDao.internalSaveOrUpdate(pollDO)
+                pollMailService.sendMail(mailFrom, mailTo, mailContent, mailSubject, listOf(mailAttachment))
             }
         }
 
-        val pollsInFuture = polls.filter { it.deadline?.isAfter(LocalDate.now()) ?: false }
-        pollsInFuture.forEach {
-            val daysDifference = ChronoUnit.DAYS.between(LocalDate.now(), it.deadline)
+        val pollsInFuture = pollDOs.filter { it.deadline?.isAfter(LocalDate.now()) ?: false }
+        pollsInFuture.forEach { pollDO ->
+            val poll = Poll()
+            poll.copyFrom(pollDO)
+            val daysDifference = ChronoUnit.DAYS.between(LocalDate.now(), pollDO.deadline)
             if (daysDifference == 1L || daysDifference == 7L) {
-                mailHeader = translateMsg("poll.mail.endingSoon.header", daysDifference)
-                mailContent = translateMsg(
+                // add all attendees mails
+                var mailTo: ArrayList<String> = poll.attendees?.map { it.email } as ArrayList<String>
+                val mailFrom = pollDO.owner?.email.toString()
+                val mailSubject = translateMsg("poll.mail.endingSoon.subject", daysDifference)
+                val mailContent = translateMsg(
                     "poll.mail.endingSoon.content",
-                    it.title,
-                    it.owner?.displayName,
-                    it.deadline.toString(),
-                    "https://projectforge.micromata.de/react/response/dynamic/${it.id}"
+                    pollDO.title,
+                    pollDO.owner?.displayName,
+                    pollDO.deadline?.format(DateTimeFormatter.ofPattern("dd.MM.yyyy")).toString(),
+                    "https://projectforge.micromata.de/react/response/dynamic/${pollDO.id}"
                 )
+                pollMailService.sendMail(mailFrom, mailTo, mailSubject, mailContent)
             }
         }
     }
 
-    private fun sendMail(to: String, subject: String, content: String, mailAttachments: List<MailAttachment>? = null) {
-        try {
-            if (content.isNotEmpty()) {
-                pollMailService.sendMail(subject = subject, content = content, mailAttachments = mailAttachments, to = to)
-            }
-        } catch (e: Exception) {
-            log.error(e.toString())
-        }
-    }
 
     /**
      * Method to delete old polls
