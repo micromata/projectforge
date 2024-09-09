8734b106e2b3f07d6106099fc48fa95c5a6b3f14
Kai Reinhard <k.reinhard@micromata.de>
2024-09-09 12:43:37 +0200

    Migration stuff in progress...

---
+++ projectforge-business/src/main/kotlin/org/projectforge/business/orga/VisitorbookService.kt
@@ -0,0 +1,65 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2024 Micromata GmbH, Germany (www.micromata.com)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+package org.projectforge.business.orga
+
+import org.projectforge.framework.persistence.api.BaseSearchFilter
+import org.projectforge.framework.persistence.api.IDao
+import org.projectforge.framework.persistence.user.entities.PFUserDO
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class VisitorbookService : IDao<VisitorbookDO?> {
+    @Autowired
+    private val visitorbookDao: VisitorbookDao? = null
+
+    override fun getList(filter: BaseSearchFilter): List<VisitorbookDO> {
+        return visitorbookDao!!.getList(filter)
+    }
+
+    override fun isHistorizable(): Boolean {
+        return true
+    }
+
+    override fun hasInsertAccess(user: PFUserDO): Boolean {
+        return visitorbookDao!!.hasInsertAccess(user)
+    }
+
+    fun getAssignedContactPersonsIds(data: VisitorbookDO?): List<Int?> {
+        val assignedContactPersons: MutableList<Int?> = ArrayList()
+        if (data?.contactPersons != null) {
+            for (employee in data.contactPersons!!) {
+                assignedContactPersons.add(employee.id)
+            }
+        }
+        return assignedContactPersons
+    }
+
+    fun addNewEntry(visitor: VisitorbookDO, groupName: String?): VisitorbookEntryDO {
+        val nw: VisitorbookTimedDO = VisitorbookTimedDO()
+        nw.setVisitor(visitor)
+        nw.setGroupName(groupName)
+        visitor.getTimeableAttributes().add(nw)
+        return nw
+    }
+}
