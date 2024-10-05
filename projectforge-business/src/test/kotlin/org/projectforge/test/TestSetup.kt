da56cdd6e7d01b62005b4974037f87b71712fd71
Kai Reinhard <k.reinhard@micromata.de>
2024-10-05 13:30:57 +0200

    TestSetup initialized test version of Configuration.

--- projectforge-business/src/test/kotlin/org/projectforge/test/TestSetup.kt
+++ projectforge-business/src/test/kotlin/org/projectforge/test/TestSetup.kt
@@ -24,9 +24,13 @@
 package org.projectforge.test
 
 import mu.KotlinLogging
+import org.mockito.Mockito
 import org.projectforge.Constants
+import org.projectforge.business.configuration.ConfigurationService
 import org.projectforge.business.configuration.ConfigurationServiceAccessor
 import org.projectforge.framework.configuration.ConfigXmlTest
+import org.projectforge.framework.configuration.Configuration
+import org.projectforge.framework.configuration.entities.ConfigurationDO
 import org.projectforge.framework.i18n.I18nHelper.addBundleName
 import org.projectforge.framework.persistence.user.api.ThreadLocalUserContext
 import org.projectforge.framework.persistence.user.api.UserContext.Companion.createTestInstance
@@ -40,25 +44,29 @@ private val log = KotlinLogging.logger {}
  * @author Kai Reinhard (k.reinhard@micromata.de)
  */
 object TestSetup {
-  /**
-   * Puts a context user in ThreadLocaleUserContext and creates a minimal ConfigXml configuration.
-   * This is use-full for tests needing the user's locale, timezone etc, but not the time consuming
-   * database and Spring setup.
-   * @return created user in ThreadLocalUserContext.
-   */
-  @JvmStatic
-  fun init(): PFUserDO {
-    addBundleName(Constants.RESOURCE_BUNDLE_NAME)
-    TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
-    val user = PFUserDO()
-    user.timeZone = TimeZone.getTimeZone("Europe/Berlin")
-    user.excelDateFormat = "YYYY-MM-DD"
-    user.dateFormat = "dd.MM.yyyy"
-    user.locale = Locale("de", "DE")
-    user.firstDayOfWeek = DayOfWeek.MONDAY
-    ThreadLocalUserContext.userContext = createTestInstance(user)
-    ConfigXmlTest.createTestConfiguration()
-    ConfigurationServiceAccessor.internalInitJunitTestMode()
-    return user
-  }
+    /**
+     * Puts a context user in ThreadLocaleUserContext and creates a minimal ConfigXml configuration.
+     * This is use-full for tests needing the user's locale, timezone etc, but not the time consuming
+     * database and Spring setup.
+     * @return created user in ThreadLocalUserContext.
+     */
+    @JvmStatic
+    fun init(): PFUserDO {
+        addBundleName(Constants.RESOURCE_BUNDLE_NAME)
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
+        val user = PFUserDO()
+        user.timeZone = TimeZone.getTimeZone("Europe/Berlin")
+        user.excelDateFormat = "YYYY-MM-DD"
+        user.dateFormat = "dd.MM.yyyy"
+        user.locale = Locale("de", "DE")
+        user.firstDayOfWeek = DayOfWeek.MONDAY
+        ThreadLocalUserContext.userContext = createTestInstance(user)
+        ConfigXmlTest.createTestConfiguration()
+        ConfigurationServiceAccessor.internalInitJunitTestMode()
+        val configurationService = Mockito.mock(ConfigurationService::class.java)
+        val configList = mutableListOf<ConfigurationDO>()
+        Mockito.`when`(configurationService.daoInternalLoadAll()).thenReturn(configList)
+        Configuration.initializeForTestOnly(configurationService)
+        return user
+    }
 }
