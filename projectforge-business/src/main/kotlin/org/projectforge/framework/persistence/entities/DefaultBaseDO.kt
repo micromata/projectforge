44253baee2448d0c7b5c60bb672e3594e57f8340
Kai Reinhard <k.reinhard@micromata.de>
2024-12-04 02:07:35 +0100

    WIP: Setup of ProjectForge...

--- projectforge-business/src/main/kotlin/org/projectforge/framework/persistence/entities/DefaultBaseDO.kt
+++ projectforge-business/src/main/kotlin/org/projectforge/framework/persistence/entities/DefaultBaseDO.kt
@@ -28,7 +28,6 @@ import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.MappedSuperclass
-import jakarta.persistence.SequenceGenerator
 import org.projectforge.common.anots.PropertyInfo
 
 /**
