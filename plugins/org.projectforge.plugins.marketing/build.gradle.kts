e21feaa61e66e09b312b8dfd9fc870aa0a820cf9
Kai Reinhard <k.reinhard@micromata.de>
2024-12-07 11:58:38 +0100

    Gradle games...

--- plugins/org.projectforge.plugins.marketing/build.gradle.kts
+++ plugins/org.projectforge.plugins.marketing/build.gradle.kts
@@ -1,5 +1,14 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     id("buildlogic.pf-module-conventions")
+    id("org.jetbrains.kotlin.jvm")
+}
+
+tasks.withType<KotlinCompile> {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    }
 }
 
 dependencies {
