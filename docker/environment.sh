7e982485601e3d533b475e267ee3bc9981149be2
Kai Reinhard <k.reinhard@micromata.de>
2024-12-26 22:55:22 +0100

    Docker works now with version 8.0, open jdk 17 and up-to-date debian.

---
+++ docker/environment.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Set the java options and arguments here for Your docker installation only.
+
+# Increase ProjectForge's memory settings:
+#export JAVA_OPTS=-Xms1g -Xmx1g
+export JAVA_OPTS=
+
+# Define your optional program arguments here
+export JAVA_ARGS=
