9675aaf18111cdc60f1fbb88e8182eb2fde3869f
Kai Reinhard <k.reinhard@micromata.de>
2024-12-29 22:56:59 +0100

    Script execution: SseEmitterTool implemented for re-use.

--- projectforge-business/src/main/kotlin/org/projectforge/business/admin/SystemStatisticsService.kt
+++ projectforge-business/src/main/kotlin/org/projectforge/business/admin/SystemStatisticsService.kt
@@ -23,59 +23,65 @@
 
 package org.projectforge.business.admin
 
+import jakarta.annotation.PostConstruct
 import mu.KotlinLogging
 import org.projectforge.framework.ToStringUtil
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import jakarta.annotation.PostConstruct
 
 private val log = KotlinLogging.logger {}
 
 @Service
 class SystemStatistics {
-  class DatabasePoolStatistics(
-    val total: Int,
-    val idle: Int,
-    val active: Int,
-    val threadsAwaitingConnection: Int
-  )
+    class DatabasePoolStatistics(
+        val total: Int,
+        val idle: Int,
+        val active: Int,
+        val threadsAwaitingConnection: Int
+    )
 
-  @Autowired
-  private lateinit var databaseStatisticsBuilder: DatabaseStatisticsBuilder
+    @Autowired
+    private lateinit var databaseStatisticsBuilder: DatabaseStatisticsBuilder
 
-  @Autowired
-  private lateinit var diskUsageStatisticsBuilder: DiskUsageStatisticsBuilder
+    @Autowired
+    private lateinit var diskUsageStatisticsBuilder: DiskUsageStatisticsBuilder
 
-  private var statisticsBuilderRegistry = mutableSetOf<SystemsStatisticsBuilderInterface>()
+    private var statisticsBuilderRegistry = mutableSetOf<SystemsStatisticsBuilderInterface>()
 
-  @PostConstruct
-  private fun postConstruct() {
-    // First get System statistics (for getting current CPU loads, not CPU loads after collecting all statistics ;-)
-    registerStatisticsBuilder(SystemsStatisticsBuilder())
-    registerStatisticsBuilder(databaseStatisticsBuilder)
-    registerStatisticsBuilder(MemoryStatisticsBuilder())
-    registerStatisticsBuilder(diskUsageStatisticsBuilder)
-  }
-
-  /**
-   * Plugins may register statistics builder for appending there own statistics.
-   */
-  @Suppress("unused")
-  fun registerStatisticsBuilder(statisticsBuilder: SystemsStatisticsBuilderInterface) {
-    statisticsBuilderRegistry.add(statisticsBuilder)
-  }
-
-  /**
-   * Builds all system statistics of registered builders, logs and returns the result.
-   */
-  fun getSystemStatistics(): SystemStatisticsData {
-    val stats = SystemStatisticsData()
-
-    statisticsBuilderRegistry.forEach {
-      it.addStatisticsEntries(stats)
+    @PostConstruct
+    private fun postConstruct() {
+        instance = this
+        // First get System statistics (for getting current CPU loads, not CPU loads after collecting all statistics ;-)
+        registerStatisticsBuilder(SystemsStatisticsBuilder())
+        registerStatisticsBuilder(databaseStatisticsBuilder)
+        registerStatisticsBuilder(MemoryStatisticsBuilder())
+        registerStatisticsBuilder(diskUsageStatisticsBuilder)
     }
 
-    log.info("Statistics: ${ToStringUtil.toJsonString(stats)}")
-    return stats
-  }
+    /**
+     * Plugins may register statistics builder for appending there own statistics.
+     */
+    @Suppress("unused")
+    fun registerStatisticsBuilder(statisticsBuilder: SystemsStatisticsBuilderInterface) {
+        statisticsBuilderRegistry.add(statisticsBuilder)
+    }
+
+    /**
+     * Builds all system statistics of registered builders, logs and returns the result.
+     */
+    fun getSystemStatistics(): SystemStatisticsData {
+        val stats = SystemStatisticsData()
+
+        statisticsBuilderRegistry.forEach {
+            it.addStatisticsEntries(stats)
+        }
+
+        log.info("Statistics: ${ToStringUtil.toJsonString(stats)}")
+        return stats
+    }
+
+    companion object {
+        lateinit var instance: SystemStatistics
+            private set
+    }
 }
