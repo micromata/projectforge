c58cc1025f074bb555a4c6d26170c5ab0931ac2f
Kai Reinhard <k.reinhard@micromata.de>
2025-02-01 08:54:49 +0100

    User, Group access tightend (also in UI).

--- projectforge-wicket/src/main/java/org/projectforge/web/user/GroupListPage.java
+++ projectforge-wicket/src/main/java/org/projectforge/web/user/GroupListPage.java
@@ -34,6 +34,8 @@ import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.projectforge.business.ldap.LdapUserDao;
 import org.projectforge.business.user.GroupDao;
+import org.projectforge.business.user.UserGroupCache;
+import org.projectforge.framework.access.AccessException;
 import org.projectforge.framework.access.OperationType;
 import org.projectforge.framework.persistence.user.entities.GroupDO;
 import org.projectforge.web.WicketSupport;
@@ -52,11 +54,17 @@ public class GroupListPage extends AbstractListPage<GroupListForm, GroupDao, Gro
   public GroupListPage(final PageParameters parameters)
   {
     super(parameters, "group");
+    if (!UserGroupCache.getInstance().isUserMemberOfAdminGroup()) {
+      throw new AccessException("You are not allowed to access this page.");
+    }
   }
 
   public GroupListPage(final ISelectCallerPage caller, final String selectProperty)
   {
     super(caller, selectProperty, "group");
+    if (!UserGroupCache.getInstance().isUserMemberOfAdminGroup()) {
+      throw new AccessException("You are not allowed to access this page.");
+    }
   }
 
   @SuppressWarnings("serial")
