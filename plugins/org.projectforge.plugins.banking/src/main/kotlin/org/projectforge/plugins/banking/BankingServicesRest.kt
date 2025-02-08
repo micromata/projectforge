bf998d85f66c279ed2a7a13c2c6f84d915356413
Kai Reinhard <k.reinhard@micromata.de>
2025-02-08 13:14:33 +0100

    BankingServicesRest, DataTransferPublicServicesRest, MerlinExecutionPageRest, ButlerServicet: stream.use (streams weren't closed before?)

--- plugins/org.projectforge.plugins.banking/src/main/kotlin/org/projectforge/plugins/banking/BankingServicesRest.kt
+++ plugins/org.projectforge.plugins.banking/src/main/kotlin/org/projectforge/plugins/banking/BankingServicesRest.kt
@@ -23,6 +23,7 @@
 
 package org.projectforge.plugins.banking
 
+import jakarta.servlet.http.HttpServletRequest
 import mu.KotlinLogging
 import org.projectforge.Constants
 import org.projectforge.common.FormatterUtils
@@ -37,63 +38,64 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
-import jakarta.servlet.http.HttpServletRequest
 
 private val log = KotlinLogging.logger {}
 
 @RestController
 @RequestMapping(BankingServicesRest.REST_PATH)
 class BankingServicesRest {
-  @Autowired
-  private lateinit var bankAccountDao: BankAccountDao
+    @Autowired
+    private lateinit var bankAccountDao: BankAccountDao
 
-  @Autowired
-  private lateinit var transactionsImporter: TransactionsImporter
+    @Autowired
+    private lateinit var transactionsImporter: TransactionsImporter
 
-  @PostMapping("import/{id}")
-  fun import(
-    request: HttpServletRequest,
-    @PathVariable("id", required = true) id: Int,
-    @RequestParam("file") file: MultipartFile
-  ): ResponseEntity<*> {
-    val filename = file.originalFilename ?: "unknown"
-    log.info {
-      "User tries to upload serial execution file: id='$id', filename='$filename', size=${
-        FormatterUtils.formatBytes(
-          file.size
+    @PostMapping("import/{id}")
+    fun import(
+        request: HttpServletRequest,
+        @PathVariable("id", required = true) id: Int,
+        @RequestParam("file") file: MultipartFile
+    ): ResponseEntity<*> {
+        val filename = file.originalFilename ?: "unknown"
+        log.info {
+            "User tries to upload serial execution file: id='$id', filename='$filename', size=${
+                FormatterUtils.formatBytes(
+                    file.size
+                )
+            }."
+        }
+        if (file.size > 100 * Constants.MB) {
+            log.warn("Upload file size to big: ${file.size} > 100MB")
+            throw IllegalArgumentException("Upload file size to big: ${file.size} > 100MB")
+        }
+        val bankAccountDO = bankAccountDao.find(id)
+        if (bankAccountDO == null) {
+            log.warn("Bank account with id #$id not found.")
+            throw IllegalArgumentException()
+        }
+        val bankAccount = BankAccount()
+        bankAccount.copyFrom(bankAccountDO)
+        log.info("Importing transactions for bank account #$id, iban=${bankAccount.iban}")
+        var importStorage: BankingImportStorage? = null
+        if (filename.endsWith("xls", ignoreCase = true) || filename.endsWith("xlsx", ignoreCase = true)) {
+            throw IllegalArgumentException("Excel not yet supported.")
+        } else {
+            importStorage = BankingImportStorage(bankAccount.importSettings, bankAccount)
+            // Try to import CSV
+            file.inputStream.use {
+                CsvImporter.parse(it, importStorage, importStorage.importSettings.charSet)
+            }
+        }
+        transactionsImporter.import(request, bankAccountDO, importStorage)
+        return ResponseEntity(
+            ResponseAction(
+                PagesResolver.getDynamicPageUrl(BankAccountRecordImportPageRest::class.java, absolute = true),
+                targetType = TargetType.REDIRECT
+            ), HttpStatus.OK
         )
-      }."
     }
-    if (file.size > 100 * Constants.MB) {
-      log.warn("Upload file size to big: ${file.size} > 100MB")
-      throw IllegalArgumentException("Upload file size to big: ${file.size} > 100MB")
-    }
-    val bankAccountDO = bankAccountDao.find(id)
-    if (bankAccountDO == null) {
-      log.warn("Bank account with id #$id not found.")
-      throw IllegalArgumentException()
-    }
-    val bankAccount = BankAccount()
-    bankAccount.copyFrom(bankAccountDO)
-    log.info("Importing transactions for bank account #$id, iban=${bankAccount.iban}")
-    var importStorage: BankingImportStorage? = null
-    if (filename.endsWith("xls", ignoreCase = true) || filename.endsWith("xlsx", ignoreCase = true)) {
-      throw IllegalArgumentException("Excel not yet supported.")
-    } else {
-      importStorage = BankingImportStorage(bankAccount.importSettings, bankAccount)
-      // Try to import CSV
-      CsvImporter.parse(file.inputStream, importStorage, importStorage.importSettings.charSet)
-    }
-    transactionsImporter.import(request, bankAccountDO, importStorage)
-    return ResponseEntity(
-      ResponseAction(
-        PagesResolver.getDynamicPageUrl(BankAccountRecordImportPageRest::class.java, absolute = true),
-        targetType = TargetType.REDIRECT
-      ), HttpStatus.OK
-    )
-  }
 
-  companion object {
-    const val REST_PATH = "${Rest.URL}/banking"
-  }
+    companion object {
+        const val REST_PATH = "${Rest.URL}/banking"
+    }
 }
