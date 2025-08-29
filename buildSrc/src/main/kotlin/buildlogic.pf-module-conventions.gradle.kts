d4e3ca941c82e2dcaea6b24c6470d1e9bcd68c37
Nils Schmidt <n.schmidt@micromata.de>
2025-08-29 10:10:05 +0200

    Upgrade Java version to 21
    - Change target vm in build configuration
    - Change java version in Dockerfile
    - Update documentation

--- buildSrc/src/main/kotlin/buildlogic.pf-module-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/buildlogic.pf-module-conventions.gradle.kts
@@ -18,8 +18,8 @@ group = "org.projectforge"
 version = libs.findVersion("org.projectforge").get().requiredVersion
 
 extensions.configure<JavaPluginExtension> {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 tasks.withType<JavaCompile> {
