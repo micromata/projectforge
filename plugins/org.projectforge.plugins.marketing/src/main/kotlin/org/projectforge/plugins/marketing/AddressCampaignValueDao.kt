8271a4353614883ebddd2c6677032e1658f92dd5
Kai Reinhard <k.reinhard@micromata.de>
2025-10-20 01:27:26 +0200

    WIP: Address campaign values

--- plugins/org.projectforge.plugins.marketing/src/main/kotlin/org/projectforge/plugins/marketing/AddressCampaignValueDao.kt
+++ plugins/org.projectforge.plugins.marketing/src/main/kotlin/org/projectforge/plugins/marketing/AddressCampaignValueDao.kt
@@ -111,7 +111,9 @@ open class AddressCampaignValueDao : BaseDao<AddressCampaignValueDO>(AddressCamp
                 existingValue
             } else {
                 // Create transient campaign value (not persisted)
+                // Assign synthetic ID using negative addressId to enable multi-selection tracking
                 val newValue = AddressCampaignValueDO()
+                newValue.id = -address.id!! // Synthetic ID: negative addressId
                 newValue.address = address
                 newValue.addressCampaign = campaign
                 newValue.value = null
