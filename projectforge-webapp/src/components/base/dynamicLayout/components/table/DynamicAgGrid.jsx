cef62038acc7ec0b0deaeee2c2e33c60a61c454d
Kai Reinhard <k.reinhard@micromata.de>
2025-12-31 22:49:20 +0100

    Breaking changes ag grid 34 fixed

--- projectforge-webapp/src/components/base/dynamicLayout/components/table/DynamicAgGrid.jsx
+++ projectforge-webapp/src/components/base/dynamicLayout/components/table/DynamicAgGrid.jsx
@@ -2,7 +2,8 @@ import AwesomeDebouncePromise from 'awesome-debounce-promise';
 import PropTypes from 'prop-types';
 import React, { useMemo, useRef, useEffect, useState, lazy, Suspense } from 'react';
 import { AgGridReact } from 'ag-grid-react';
-import { LicenseManager, ModuleRegistry, AllEnterpriseModule, themeBalham } from 'ag-grid-enterprise';
+import { ModuleRegistry, AllCommunityModule, themeBalham } from 'ag-grid-community';
+import { LicenseManager, AllEnterpriseModule } from 'ag-grid-enterprise';
 import { connect } from 'react-redux';
 import { useNavigate, useLocation } from 'react-router';
 import { DynamicLayoutContext } from '../../context';
@@ -28,7 +29,7 @@ function CustomizedCellWrapper(props) {
 }
 
 LicenseManager.setLicenseKey('Using_this_{AG_Grid}_Enterprise_key_{AG-089620}_in_excess_of_the_licence_granted_is_not_permitted___Please_report_misuse_to_legal@ag-grid.com___For_help_with_changing_this_key_please_contact_info@ag-grid.com___{Micromata_GmbH}_is_granted_a_{Single_Application}_Developer_License_for_the_application_{ProjectForge}_only_for_{1}_Front-End_JavaScript_developer___All_Front-End_JavaScript_developers_working_on_{ProjectForge}_need_to_be_licensed___{ProjectForge}_has_not_been_granted_a_Deployment_License_Add-on___This_key_works_with_{AG_Grid}_Enterprise_versions_released_before_{14_July_2026}____[v3]_[01]_MTc4Mzk4MzYwMDAwMA==932cdb76359c2b516156ac62da0bd954');
-ModuleRegistry.registerModules([AllEnterpriseModule]);
+ModuleRegistry.registerModules([AllCommunityModule, AllEnterpriseModule]);
 
 const agTheme = themeBalham
     .withParams({
